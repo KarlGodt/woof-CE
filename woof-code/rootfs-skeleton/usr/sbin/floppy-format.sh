42ffb03eb8fbf47bb35cacd817a38bd3ea8fa4f8
KarlGodt <karlgodt@excite.de>
2013-11-23 17:43:11 -0100

    this_branch : Replaced the scripts found in Macpup-Opera2.sfs as #!/bin/bash
    GOAL='/root/GitHub.d/KarlGodt_ForkWoof/woof-code/rootfs-skeleton'
    
    rm -f /root/PUPPYscripts.list
    for i in ./bin ./sbin ./usr/bin ./usr/sbin ./usr/local/bin ./usr/X11/bin;
    do grep -I -H -m1 -i -E 'BK|Kauler|Barry|B\.K\.|T2 |t\-2|rxvt' /* | grep '#' >>/root/PUPPYscripts.list;
    done
    
    OldIFS="
    "
    IFS=':'
    while read filename rest
    do
    
    dn=""
    
    echo "" "//"
    cp -a --remove-destination "" "//"
    
    done</root/PUPPYscripts.list
    
    IFS=""
    exit
    
    Bash was 3.2, bb ash was 1.18.3 .
    Dunno why IFS=':' while did not work.

--- woof-code/rootfs-skeleton/usr/sbin/floppy-format.sh
+++ woof-code/rootfs-skeleton/usr/sbin/floppy-format.sh
@@ -2,71 +2,58 @@
 #Format floppy disks
 #Copyright (c) Barry Kauler 2004 www.goosee.com/puppy
 #2007 Lesser GPL licence v2 (http://www.fsf.org/licensing/licenses/lgpl.html)
-#130517 BK: code improved and internationalized.
-
-export TEXTDOMAIN=floppy-format
-export OUTPUT_CHARSET=UTF-8
 
 zapfloppy()
 {
- # Puppy will only allow 1440, 1680K and 1760K capacities.
- ERR0=1
- while [ $ERR0 -ne 0 ];do
-  #xmessage -bg "#c0ffff" -center -title "$(gettext 'Please wait...')" -buttons "" "$(gettext 'Low-level formatting disk with this capacity:') ${1} Kbyte" &
-  pwMSG="$(gettext 'Low-level formatting disk with this capacity:') ${1} Kbyte
-$(gettext 'Please wait...')"
-  yaf-splash -close never -bg '#c0ffff' -fontsize large -text "${pwMSG}" &
-  pwID=$!
-  fdformat /dev/fd0u$1
-  ERR0=$?
-  sync
-  #killall xmessage
-  pupkill $pwID
-  if [ $ERR0 -ne 0 ];then
-   xmessage -bg "#ffe0e0" -name "loformat" -title "$(gettext 'Puppy Low-level Formatter')" -center -buttons "$(gettext 'Try again')":20,"$(gettext 'QUIT')":10 -file -<<XMSG
-$(gettext 'ERROR low-level formatting disk.')
-$(gettext 'Is the write-protect tab closed?')
+# Puppy will only allow 1440, 1680K and 1760K capacities.
+ERR0=1
+while [ ! $ERR0 -eq 0 ];do
+xmessage -bg "#c0ffff" -center -title "Please wait..." -buttons "" "Low-level formatting disk with $1 Kbyte capacity" &
+ fdformat /dev/fd0u$1
+ ERR0=$?
+ killall xmessage
+ if [ ! $ERR0 -eq 0 ];then
+  xmessage -bg "#ffe0e0" -name "loformat" -title "Puppy Lo-level Formatter" -center \
+  -buttons "Try again":20,"QUIT":10 -file -<<XMSG
+ERROR low-level formatting disk.
+Is the write-protect tab closed?
 XMSG
 
-   AN0=$?
-   if [ $AN0 -eq 10 ];then
-    ERR0=0
-   fi
-   if [ $AN0 -eq 0 ];then
-    ERR0=0
-   fi
-   if [ $AN0 -eq 1 ];then
-    ERR0=0
-   fi
-  else
-   INTROMSG="
-$(gettext '\ZbSUCCESS!\ZB')
-$(gettext 'Now you should press the \ZbMsdos/vfat filesystem\ZB button.')"
+  AN0=$?
+  if [ $AN0 -eq 10 ];then
+   ERR0=0
   fi
- done
+  if [ $AN0 -eq 0 ];then
+   ERR0=0
+  fi
+  if [ $AN0 -eq 1 ];then
+   ERR0=0
+  fi
+ else
+  INTROMSG="`
+echo "SUCCESS!"
+echo -e "Now you should press the \"Msdos/vfat filesystem\" button."
+`"
+ fi
+done
 }
 
 fsfloppy()
 {
-echo "$(gettext 'Creating msdos filesystem on the disk...')"
+echo "Creating msdos filesystem on the disk..."
 ERR1=1
 while [ ! $ERR1 -eq 0 ];do
- #xmessage -bg "#c0ffff" -center -title "$(gettext 'Please wait...')" -buttons "" "$(gettext 'Creating msdos/vfat filesystem on floppy disk')" &
- pwMSG="$(gettext 'Creating msdos/vfat filesystem on floppy disk')
-$(gettext 'Please wait...')"
- yaf-splash -close never -bg '#c0ffff' -fontsize large -text "${pwMSG}" &
- pwID=$!
+xmessage -bg "#c0ffff" -center -title "Please wait..." -buttons "" "Creating msdos/vfat filesystem on floppy disk" &
  mkfs.msdos -c /dev/fd0u$1
  #mformat -f $1 a:
  #mbadblocks a:
  ERR1=$?
- #killall xmessage
- pupkill $pwID
- if [ $ERR1 -ne 0 ];then
-  xmessage -bg "#ffe0e0" -name "msdosvfat" -title "$(gettext 'Floppy msdos/vfat filesystem')" -center \
-  -buttons "$(gettext 'Try again')":20,"$(gettext 'QUIT')":10 -file -<<XMSG
-$(gettext 'ERROR creating msdos/vfat filesystem on the floppy disk.')
-$(gettext 'Is the write-protect tab closed?')
+ killall xmessage
+ if [ ! $ERR1 -eq 0 ];then
+  xmessage -bg "#ffe0e0" -name "msdosvfat" -title "Floppy msdos/vfat filesystem" -center \
+  -buttons "Try again":20,"QUIT":10 -file -<<XMSG
+ERROR creating msdos/vfat filesystem on the floppy disk.
+Is the write-protect tab closed?
 XMSG
 
   AN0=$?
@@ -80,73 +67,94 @@ XMSG
    ERR1=0
   fi
  else
-  INTROMSG="
-$(gettext '\ZbSUCCESS!\ZB')
-$(gettext 'The floppy disk is now ready to be used. Use the Puppy Drive Mounter to mount it. Or, click the floppy-disk icon on the desktop.')
-$(gettext 'First though, press \ZbEXIT\ZB to get out of here...')"
+  INTROMSG="`
+echo "SUCCESS!"
+echo "The floppy disk is now ready to be used."
+echo "Use the Puppy Drive Mounter to mount it."
+echo "(NOTE: if you use the MToolsFM floppy file"
+echo " manager, the floppy drive is accessed directly,"
+echo " so do NOT use the Puppy Drive Mounter)"
+echo "First though, press EXIT to get out of here..."
+`"
  fi
 done
 sync
-echo "$(gettext '...done.')"
+echo "...done."
 echo " "
 }
 
-INTROMSG="$(gettext '\ZbWELCOME!\ZB')
-$(gettext 'The Puppy Floppy Formatter only formats floppies with 1440 Kbyte capacity and with the msdos/vfat filesystem, for interchangeability with Windows.')
-
-$(gettext 'You only need to low-level format if the disk is formatted with some other capacity, or it is corrupted. You do not have to low-level format a new disk, but may do so to check its integrity.')
-$(gettext 'A disk is NOT usable if it is only low-level formatted: it also must have a filesystem, so this must always be the second step.')
-$(gettext 'Doing step-2 only, that is, creating a filesystem on a disk, is also a method for wiping any existing files.')"
+INTROMSG="`
+echo "WELCOME!"
+echo "My Puppy Floppy Formatter only formats floppies with"
+echo "1440 Kbyte capacity and with the msdos/vfat filesystem,"
+echo "for interchangeability with Windows."
+echo " "
+echo "You only need to lo-level format if the disk is formatted"
+echo "with some other capacity, or it is corrupted. You do not"
+echo "have to lo-level format a new disk, but may do so to"
+echo "check its integrity."
+echo "A disk is NOT usable if it is only lo-level formatted: it"
+echo "also must have a filesystem, so this must always be the"
+echo "second step."
+echo "Doing step-2 only, that is, creating a filesystem on a"
+echo "disk, is also a method for wiping any existing files."
+`"
 
 #big loop...
 while :; do
 
- MNTDMSG=" "
- mount | grep "/dev/fd0" > /dev/null 2>&1
- if [ $? -eq 0 ];then #=0 if string found
-  CURRENTMNT="`mount | grep "/dev/fd0" | cut -f 3 -d ' '`"
-  #this tells Rox to close any window with this directory and subdirectories open...
-  rox -D "$CURRENTMNT"
-  sync
-  umount "$CURRENTMNT" #/mnt/floppy
-  if [ $? -ne 0 ];then
-   MNTDMSG="
-$(gettext 'Puppy found a floppy disk already mounted in the drive, but is not able to unmount it. The disk must be unmounted now. Please click the \Zbclose box\ZB on the floppy-disk icon on the desktop, or use the Puppy Drive Mounter (click \Zbmount\ZB icon at top of screen) to unmount the floppy disk. DO THIS FIRST!')"
-  else
-   MNTDMSG="
-$(gettext 'Puppy found that the floppy disk was mounted, but has now unmounted it. Now ok to format disk.')"
-  fi
+MNTDMSG=" "
+mount | grep "/dev/fd0" > /dev/null 2>&1
+if [ $? -eq 0 ];then #=0 if string found
+ CURRENTMNT="`mount | grep "/dev/fd0" | cut -f 3 -d ' '`"
+ #this tells Rox to close any window with this directory and subdirectories open...
+ rox -D "$CURRENTMNT"
+ sync
+ umount "$CURRENTMNT" #/mnt/floppy
+ if [ ! $? -eq 0 ];then
+  MNTDMSG="`
+echo " "
+echo "Puppy found a floppy disk already mounted in the drive, but"
+echo "is not able to unmount it. The disk must be unmounted now."
+echo "Please use the Puppy Drive Mounter (in the File Manager menu)"
+echo "to unmount the floppy disk. DO THIS FIRST!"
+echo " "
+`"
+ else
+  MNTDMSG="`
+echo " "
+echo "Puppy found that the floppy disk was mounted, but has now"
+echo "unmounted it. Now ok to format disk."
+echo " "
+`"
  fi
+fi
 
- #xmessage -bg "#e0ffe0" -name "pformat" -title "Puppy Floppy Formatter" -center \
- #-buttons "Lo-level Format":20,"Msdos/vfat filesystem":30,"EXIT":10 -file -<<XMSG
- #$INTROMSG
- #$MNTDMSG
- #Press a button:
- #XMSG
+xmessage -bg "#e0ffe0" -name "pformat" -title "Puppy Floppy Formatter" -center \
+-buttons "Lo-level Format":20,"Msdos/vfat filesystem":30,"EXIT":10 -file -<<XMSG
+$INTROMSG
+$MNTDMSG
+Press a button:
+XMSG
 
- pressMSG="$(gettext 'Press a button:')"
- pupdialog --colors --background '#e0ffe0' --title "$(gettext 'Puppy Floppy Formatter')" --extra-button --yes-label "$(gettext 'Low-level Format')" --no-label "$(gettext 'EXIT')" --extra-label "$(gettext 'Msdos/vfat filesystem')" --yesno "${INTROMSG}
-${MNTDMSG}
-${pressMSG}" 0 0
+ANS=$?
 
- ANS=$?
- 
- case $ANS in
-  0) #low-level format
-   zapfloppy 1440
-  ;;
-  3) #vfat
-   fsfloppy 1440
-  ;;
-  1) #exit
-   break
-  ;;
-  *)
-   break
-  ;; 
- esac
+if [ $ANS -eq 0 ]; then
+ break
+fi
+if [ $ANS -eq 1 ]; then
+ break
+fi
+if [ $ANS -eq 10 ]; then
+ break
+fi
+
+if [ $ANS -eq 20 ];then #format
+ zapfloppy 1440
+fi
+
+if [ $ANS -eq 30 ];then #vfat
+ fsfloppy 1440
+fi
 
 done
-
-###END###
--- woof-code/rootfs-skeleton/usr/sbin/indexgen.sh
+++ woof-code/rootfs-skeleton/usr/sbin/indexgen.sh
@@ -8,35 +8,12 @@
 #w019 now have /root/.packages/PKGS_HOMEPAGES
 #w464 reintroduce dropdown help for all builtin packages.
 #v423 file PKGS_HOMEPAGES is now a db of all known pkgs, not just in puppy.
-#120225 copy from raw doc files.
 
 export LANG=C
-. /etc/DISTRO_SPECS #has DISTRO_BINARY_COMPAT, DISTRO_COMPAT_VERSION, DISTRO_PUPPYDATE
+. /etc/DISTRO_SPECS #has DISTRO_BINARY_COMPAT, DISTRO_COMPAT_VERSION
 . /root/.packages/DISTRO_PKGS_SPECS
 
 WKGDIR="`pwd`"
-
-#120225 this is done in Woof by rootfs-skeleton/pinstall.sh, but do need to do it
-#here to support language translations (see /usr/share/sss/doc_strings)...
-if [ -f /usr/share/doc/index.html.top-raw ];then #see Woof rootfs-skeleton/pinstall.sh, also /usr/share/sss/doc_strings
- cp -f /usr/share/doc/index.html.top-raw /usr/share/doc/index.html.top
- cp -f /usr/share/doc/index.html.bottom-raw /usr/share/doc/index.html.bottom
- cp -f /usr/share/doc/home-raw.htm /usr/share/doc/home.htm
-
- cutDISTRONAME="`echo -n "$DISTRO_NAME" | cut -f 1 -d ' '`"
- cPATTERN="s/cutDISTRONAME/${cutDISTRONAME}/g"
- RIGHTVER="$DISTRO_VERSION"
- dPATTERN="s/PUPPYDATE/${DISTRO_PUPPYDATE}/g"
- PATTERN1="s/RIGHTVER/${RIGHTVER}/g"
- PATTERN2="s/DISTRO_VERSION/${DISTRO_VERSION}/g"
- nPATTERN="s/DISTRO_NAME/${DISTRO_NAME}/g"
- 
- sed -i -e "$PATTERN1" -e "$PATTERN2" -e "$nPATTERN" -e "$dPATTERN" -e "$cPATTERN" /usr/share/doc/index.html.top
- sed -i -e "$PATTERN1" -e "$PATTERN2" -e "$nPATTERN" -e "$dPATTERN" /usr/share/doc/index.html.bottom
- #...note, /usr/sbin/indexgen.sh puts these together as index.html (normally via rc.update and 3builddistro).
- 
- sed -i -e "$nPATTERN" /usr/share/doc/home.htm
-fi
 
 #search for installed pkgs with descriptions...
 
