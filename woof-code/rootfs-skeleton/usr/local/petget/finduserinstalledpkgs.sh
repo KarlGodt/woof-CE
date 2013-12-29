67e67e0e88bbc5ce15fae9ab13a1a451a818d660
KarlGodt <karlgodt@excite.de>
2013-12-29 02:20:59 -0100

    /usr/local/petget/finduserinstalledpkgs.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/usr/local/petget/finduserinstalledpkgs.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/finduserinstalledpkgs.sh
@@ -1,30 +1,49 @@
 #!/bin/sh
 #(c) Copyright Barry Kauler 2009, puppylinux.com
 #2009 Lesser GPL licence v2 (http://www.fsf.org/licensing/licenses/lgpl.html).
-#called from /usr/local/petget/pkg_chooser.sh (actually, from ui_Classic and ui_Ziggy, which are embedded in pkg_chooser.sh)
+#called from /usr/local/petget/pkg_chooser.sh
 #find all pkgs that have been user-installed, format for display.
-#120813 no longer using /var/local/petget/flg_appicons
-#120908 fix for icons (derived from Category field #5 in db).
+
+
+###KRG Fr 31. Aug 23:34:58 GMT+1 2012
+
+
+
+trap "exit 1" HUP INT QUIT KILL TERM
+
+
+OUT=/dev/null;ERR=$OUT
+[ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
+[ "$DEBUG" = "2" ] && set -x
+
+
+Version='1.1'
+
+
+usage(){
+USAGE_MSG="
+$0 [ PARAMETERS ]
+
+-V|--version : showing version information
+-H|--help : show this usage information
+
+*******  *******  *******  *******  *******  *******  *******  *******  *******
+$2
+"
+exit $1
+}
+
+[ "`echo "$1" | grep -wiE "help|\-H"`" ] && usage 0
+[ "`echo "$1" | grep -wiE "\-version|\-V"`" ] && { echo "$0 -version $Version";exit 0; }
+
+echo "$0:$*" >&2
+
+###KRG Fr 31. Aug 23:34:58 GMT+1 2012
+
 
 #/root/.packages/usr-installed-packages has the list of installed pkgs...
 touch /root/.packages/user-installed-packages
-cut -f 1,5,10 -d '|' /root/.packages/user-installed-packages > /tmp/petget/installedpkgs.results
+cut -f 1,10 -d '|' /root/.packages/user-installed-packages | sed '/^$/d' | sort -u > /tmp/installedpkgs.results
 
-#120529 may have app icons displayed in main window...
-#cp -f /tmp/petget/installedpkgs.results /tmp/petget/installedpkgs.results.post-noicons
-cut -f 1,3 -d '|' /tmp/petget/installedpkgs.results > /tmp/petget/installedpkgs.results.post-noicons #120908
-#120813 remove...
-#FLG_APPICONS="`cat /var/local/petget/flg_appicons`"
-#if [ "$FLG_APPICONS" = "true" ];then
-# #note, for main tree, this is done in postfilterpkgs.sh.
-# #ex: 'abiword0-1.2.3|description of abiword|stuff' becomes 'abiword|abiword0-1.2.3|description of abiword|stuff'
-# sed -r -e 's%(^[a-zA-Z]*)%\1|\1%' /tmp/petget/installedpkgs.results > /tmp/petget/installedpkgs.results.post
-#else
-# cp -f /tmp/petget/installedpkgs.results /tmp/petget/installedpkgs.results.post
-#fi
-#120908 category field: Document;edit becomes mini-Document-edit... (see also postfilterpkgs.sh)
-sed -e 's%|%|mini-%' -e 's%;%-%' /tmp/petget/installedpkgs.results > /tmp/petget/installedpkgs.results.post
-#120908 probably doesn't matter, but take icon out for prior compatibility...
-cp -f /tmp/petget/installedpkgs.results.post-noicons /tmp/petget/installedpkgs.results
 
 ###END###
