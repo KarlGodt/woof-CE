e6abe9a72298b221075a019b2ad64f6d1d7512dd
KarlGodt <karlgodt@excite.de>
2013-12-29 15:00:40 -0100

    /usr/local/petget/finduserinstalledpkgs.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/usr/local/petget/finduserinstalledpkgs.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/finduserinstalledpkgs.sh
@@ -5,45 +5,47 @@
 #find all pkgs that have been user-installed, format for display.
 
 
-###KRG Fr 31. Aug 23:34:58 GMT+1 2012
 
-
-
-trap "exit 1" HUP INT QUIT KILL TERM
+#************
+#KRG
 
 
 OUT=/dev/null;ERR=$OUT
 [ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
-[ "$DEBUG" = "2" ] && set -x
+[ "$DEBUG" = 2 ] && set -x
 
 
-Version='1.1'
-
+Version=1.1-KRG-MacPup_O2
 
 usage(){
-USAGE_MSG="
-$0 [ PARAMETERS ]
-
--V|--version : showing version information
--H|--help : show this usage information
-
-*******  *******  *******  *******  *******  *******  *******  *******  *******
-$2
+MSG="
+$0 [ help | version ]
 "
+echo "$MSG
+$2"
 exit $1
 }
+[ "`echo "$1" | grep -Ei "help|\-h"`" ] && usage 0
+[ "`echo "$1" | grep -Ei "version|\-V"`" ] && { echo "$0: $Version";exit 0; }
 
-[ "`echo "$1" | grep -wiE "help|\-H"`" ] && usage 0
-[ "`echo "$1" | grep -wiE "\-version|\-V"`" ] && { echo "$0 -version $Version";exit 0; }
 
-echo "$0:$*" >&2
+trap "exit" HUP INT QUIT ABRT KILL TERM
 
-###KRG Fr 31. Aug 23:34:58 GMT+1 2012
 
+#KRG
+#************
+
+
+echo "$0: START" >&2
+
+OUT=/dev/null;ERR=$OUT
+[ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
 
 #/root/.packages/usr-installed-packages has the list of installed pkgs...
 touch /root/.packages/user-installed-packages
-cut -f 1,10 -d '|' /root/.packages/user-installed-packages | sed '/^$/d' | sort -u > /tmp/installedpkgs.results
-
-
+sort -u /root/.packages/user-installed-packages >/tmp/installedpkgs.db
+cp --remove-destination /tmp/installedpkgs.db /root/.packages/user-installed-packages
+cut -f 1,10 -d '|' /root/.packages/user-installed-packages |sed '/^$/d' |sort -u > /tmp/installedpkgs.results
+#cp -f /tmp/installedpkgs.results /root/.packages/user-installed-packages
+echo "$0: END" >&2
 ###END###
