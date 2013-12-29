37c7d3cc3ed7bdafebed323ef27c78dc2e25ab78
KarlGodt <karlgodt@excite.de>
2013-12-29 02:24:24 -0100

    /usr/sbin/gaim-autosetup.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/usr/sbin/gaim-autosetup.sh
+++ woof-code/rootfs-skeleton/usr/sbin/gaim-autosetup.sh
@@ -18,6 +18,42 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+
+###KRG Fr 31. Aug 23:34:58 GMT+1 2012
+
+
+
+trap "exit 1" HUP INT QUIT KILL TERM
+
+
+OUT=/dev/null;ERR=$OUT
+[ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
+[ "$DEBUG" = "2" ] && set -x
+
+
+Version='1.1'
+
+
+usage(){
+USAGE_MSG="
+$0 [ PARAMETERS ]
+
+-V|--version : showing version information
+-H|--help : show this usage information
+
+*******  *******  *******  *******  *******  *******  *******  *******  *******
+$2
+"
+exit $1
+}
+
+[ "`echo "$1" | grep -wE "\-help|\-H"`" ] && usage 0
+[ "`echo "$1" | grep -wE "\-version|\-V"`" ] && { echo "$0 -version $Version";exit 0; }
+
+
+
+###KRG Fr 31. Aug 23:34:58 GMT+1 2012
+
 # bugfix bk 2006
 
 IRCHOST='irc.freenode.net'
