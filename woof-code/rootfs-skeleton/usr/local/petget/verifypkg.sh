ccf230b2b021451dcad78976fe3eb35da2dbc8fa
KarlGodt <karlgodt@excite.de>
2013-12-30 02:54:05 -0100

    /usr/local/petget/verifypkg.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/usr/local/petget/verifypkg.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/verifypkg.sh
@@ -5,12 +5,10 @@
 #passed param is the path and name of the downloaded package.
 #100116 add support for .tar.bz2 T2 pkgs.
 #100616 add support for .txz slackware pkgs.
-#101225 bug fix, .pet was converted to .tar.gz, restore to .pet.
 
 export LANG=C
 . /etc/rc.d/PUPSTATE  #this has PUPMODE and SAVE_LAYER.
 . /etc/DISTRO_SPECS #has DISTRO_BINARY_COMPAT, DISTRO_COMPAT_VERSION
-mkdir -p /tmp/petget #101225
 
 DLPKG="$1"
 DLPKG_BASE="`basename $DLPKG`" #ex: scite-1.77-i686-2as.tgz
@@ -21,19 +19,9 @@ cd $DLPKG_PATH
 
 case $DLPKG_BASE in
  *.pet)
-  #101225 bug fix, .pet was converted to .tar.gz, restore to .pet...
   DLPKG_MAIN="`basename $DLPKG_BASE .pet`"
-  FULLSIZE=`stat --format=%s "${DLPKG_BASE}"`
-  ORIGSIZE=`expr $FULLSIZE - 32`
-  dd if="${DLPKG_BASE}" of=/tmp/petget/petmd5sum bs=1 skip=${ORIGSIZE} 2>/dev/null
-  sync
-  MD5SUM="`cat /tmp/petget/petmd5sum`"
   pet2tgz $DLPKG_BASE
   [ $? -ne 0 ] && FLAG="bad"
-  if [ -f ${DLPKG_PATH}/${DLPKG_MAIN}.tar.gz ];then
-   mv -f ${DLPKG_PATH}/${DLPKG_MAIN}.tar.gz ${DLPKG_PATH}/${DLPKG_MAIN}.pet
-   echo -n "$MD5SUM" >> ${DLPKG_PATH}/${DLPKG_MAIN}.pet
-  fi
  ;;
  *.deb)
   DLPKG_MAIN="`basename $DLPKG_BASE .deb`"
