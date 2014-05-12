c394cc0fc1758b470131cf5907a8778e41dc5374
KarlGodt <karlgodt@excite.de>
2014-05-12 00:50:55 -0100

    /usr/local/petget/testurls.sh: Batch commit changes as of 2014-05-12

--- woof-code/rootfs-skeleton/usr/local/petget/testurls.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/testurls.sh
@@ -4,18 +4,20 @@
 #repository.slacky.eu|http://repository.slacky.eu/slackware-12.2|Packages-slackware-12.2-slacky
 #...only the first field is of interest in this script.
 
+echo "$0:$*" >&2
+
 echo '#!/bin/sh' >  /tmp/petget_urltest
 echo 'echo "Testing the URLs:"' >>  /tmp/petget_urltest
 
 for ONEURLSPEC in `cat /tmp/petget_repos`
 do
  URL_TEST="`echo -n "$ONEURLSPEC" | cut -f 1 -d '|'`"
- 
+
  #[ "`wget -t 2 -T 20 --waitretry=20 --spider -S $ONE_PET_SITE -o /dev/stdout 2>/dev/null | grep '200 OK'`" != "" ]
- 
+
  echo 'echo' >> /tmp/petget_urltest
  echo "wget -t 2 -T 20 --waitretry=20 --spider -S $URL_TEST" >> /tmp/petget_urltest
- 
+
 done
 
 echo 'echo "
