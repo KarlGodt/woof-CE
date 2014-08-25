334a387e8dfc7f92a18d7314f05af025a61e65cc
KarlGodt <karlgodt@excite.de>
2014-08-25 17:15:37 -0100

    woof-code/rootfs-skeleton/usr/sbin/pmonitorx.sh: Added or cleaned up header.

--- woof-code/rootfs-skeleton/usr/sbin/pmonitorx.sh
+++ woof-code/rootfs-skeleton/usr/sbin/pmonitorx.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
 # log a file tail
 
+trap "exit 1" HUP INT QUIT KILL TERM
+
+OUT=/dev/null;ERR=$OUT
+[ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
+[ "$DEBUG" = "2" ] && set -x
+
+Version='1.1'
+
+usage(){
+USAGE_MSG="
+$0 [ PARAMETERS ]
+
+-V|--version : showing version information
+-H|--help : show this usage information
+
+*******  *******  *******  *******  *******  *******  *******  *******  *******
+$2
+"
+exit $1
+}
+
+[ "`echo "$1" | grep -wE "\-help|\-H"`" ] && usage 0
+[ "`echo "$1" | grep -wE "\-version|\-V"`" ] && { echo "$0 -version $Version";exit 0; }
+
+
 #Xdialog --title "Monitoring tail of $1" --smooth --fixed-font --no-cancel --ok-label "Exit" --tailbox $1 18 95
 
 LINES1=0
@@ -11,6 +36,6 @@ while [ 1 ];do
   LINESDIFF=`expr $LINES2 - $LINES1`
   tail -n $LINESDIFF /tmp/xerrs.log
   LINES1=$LINES2
- fi 
+ fi
  sleep 1
 done
