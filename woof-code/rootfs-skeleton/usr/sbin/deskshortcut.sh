a488584a7b3beac2f27d102af2ffe661799c8ba1
KarlGodt <karlgodt@excite.de>
2014-08-31 20:05:01 -0100

    woof-code/rootfs-skeleton/usr/sbin/deskshortcut.sh: Added Header.

--- woof-code/rootfs-skeleton/usr/sbin/deskshortcut.sh
+++ woof-code/rootfs-skeleton/usr/sbin/deskshortcut.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 #Lesser GPL licence v2 (http://www.fsf.org/licensing/licenses/lgpl.html)
-#this is very primitive script to create desktop shortcut.
+# This is very primitive script to create desktop shortcut.
 
+_TITLE_=deskshortcut
+_COMMENT_="Xdialog to create desktop shortcut."
+
+MY_SELF="$0"
+
+test -f /etc/rc.d/f4puppy5 && {
+ set +e
+ source /etc/rc.d/f4puppy5 && {
+ set +n
+ source /etc/rc.d/f4puppy5; } || echo "WARNING : Could not source /etc/rc.d/f4puyppy5 ."
+
+ADD_PARAMETER_LIST=
+ADD_PARAMETERS=
+_provide_basic_parameters
+
+ADD_HELP_MSG="$_COMMENT_"
+_parse_basic_parameters "$@"
+[ "$DO_SHIFT" ] && [ ! "${DO_SHIFT//[[:digit:]]/}" ] && {
+for i in `seq 1 1 $DO_SHIFT`; do shift; done; }
+
+_trap
+} || echo "Warning : No /etc/rc.d/f4puppy5 installed."
 
 while [ 1 ]
 do
