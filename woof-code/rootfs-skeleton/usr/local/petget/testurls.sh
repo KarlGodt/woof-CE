f4db6b96bf74448130d884cb20447a4dc95eea5c
KarlGodt <karlgodt@excite.de>
2014-09-01 18:44:27 -0100

    woof-code/rootfs-skeleton/usr/local/petget/testurls.sh: Header fixes for PPM.

--- woof-code/rootfs-skeleton/usr/local/petget/testurls.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/testurls.sh
@@ -4,6 +4,11 @@
 # repository.slacky.eu|http://repository.slacky.eu/slackware-12.2|Packages-slackware-12.2-slacky
 # ...only the first field is of interest in this script.
 
+_TITLE_=testurls
+_COMMENT_="wget test for entries in petget_repos file"
+
+MY_SELF="$0"
+
 test -f /etc/rc.d/f4puppy5 && {
 source /etc/rc.d/f4puppy5
 
@@ -11,7 +16,7 @@ ADD_PARAMETER_LIST=""
 ADD_PARAMETERS=""
 _provide_basic_parameters
 
-ADD_HELP_MSG="Helper script for /usr/local/petget/ petget and downloadpkgs.sh ."
+ADD_HELP_MSG="Helper script for PPM ."
 _parse_basic_parameters "$@"
 [ "$DO_SHIFT" ] && [ ! "${DO_SHIFT//[[:digit:]]/}" ] && {
     for i in `seq 1 1 $DO_SHIFT`; do shift; done; }
@@ -20,9 +25,7 @@ _trap
 
 echo "$0: START" >&2
 
-OUT=/dev/null;ERR=$OUT
-[ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
-
+[ "$ERR" ] || ERR=/dev/null
 tmpDIR=/tmp/petget
 test -d "$tmpDIR" || mkdir -p "$tmpDIR"
 
@@ -33,7 +36,7 @@ for ONEURLSPEC in `cat "$tmpDIR"/petget_repos`
 do
  URL_TEST="`echo -n "$ONEURLSPEC" | cut -f 1 -d '|'`"
 
- #[ "`wget -t 2 -T 20 --waitretry=20 --spider -S $ONE_PET_SITE -o /dev/stdout 2>/dev/null | grep '200 OK'`" != "" ]
+ #[ "`wget -t 2 -T 20 --waitretry=20 --spider -S $ONE_PET_SITE -o /dev/stdout 2>$ERR | grep '200 OK'`" != "" ]
 
  echo 'echo' >> "$tmpDIR"/petget_urltest
  echo "wget -t 2 -T 20 --waitretry=20 --spider -S $URL_TEST" >> "$tmpDIR"/petget_urltest
@@ -44,7 +47,7 @@ echo 'echo "
 TESTING FINISHED
 Read the above, any that returned \"200 OK\" succeeded."' >>  "$tmpDIR"/petget_urltest
 echo 'echo -n "Press ENTER key to exit: "
-read ENDIT'  >>  "$tmpDIR"/petget_urltest
+read ENDIT_'  >>  "$tmpDIR"/petget_urltest
 
 chmod 777 "$tmpDIR"/petget_urltest
 rxvt -title "Puppy Package Manager: download" -bg orange -fg black -e "$tmpDIR"/petget_urltest
