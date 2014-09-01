cf9e36f9fdddeeb4949e0ed5cc548e2608919e37
KarlGodt <karlgodt@excite.de>
2014-09-01 14:30:06 -0100

    woof-code/rootfs-skeleton/usr/local/petget/findmissingpkgs.sh: Header fixes and PPM: Use tmpDIR=/tmp/petget .

--- woof-code/rootfs-skeleton/usr/local/petget/findmissingpkgs.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/findmissingpkgs.sh
@@ -2,7 +2,7 @@
 #(c) Copyright Barry Kauler 2009, puppylinux.com
 #2009 Lesser GPL licence v2 (http://www.fsf.org/licensing/licenses/lgpl.html).
 # Called from /usr/local/petget/installpreview.sh or check_deps.sh
-# /tmp/petget_pkg_name_aliases_patterns is written by pkg_chooser.sh.
+# "$tmpDIR"/petget_pkg_name_aliases_patterns is written by pkg_chooser.sh.
 # Passed param is a list of dependencies (DB_dependencies field of the pkg database).
 # Results format, see comment end of this script.
 
@@ -43,63 +43,65 @@ DB_dependencies="$1" #in standard format of the package database, field 9.
 . /root/.packages/DISTRO_PKGS_SPECS #has PKGS_SPECS_TABLE
 . /root/.packages/PKGS_MANAGEMENT   #has DISTRO_PPM_DEVX_EXCEPTIONS, PKG_ALIASES_INSTALLED
 
+tmpDIR=/tmp/petget
+test -d "$tmpDIR" || mkdir -p "$tmpDIR"
 
 #need patterns of all installed pkgs...
-if [ ! -f /tmp/petget_installed_patterns_system ];then
+if [ ! -f "$tmpDIR"/petget_installed_patterns_system ];then
 
  INSTALLED_PATTERNS_SYS=`cat /root/.packages/woof-installed-packages | cut -f 2 -d '|' | sed -e 's%^%|%' -e 's%$%|%' -e 's%\\-%\\\\-%g'`
- echo "$INSTALLED_PATTERNS_SYS" > /tmp/petget_installed_patterns_system
+ echo "$INSTALLED_PATTERNS_SYS" > "$tmpDIR"/petget_installed_patterns_system
 
  #PKGS_SPECS_TABLE also has system-installed names, some of them are generic combinations of pkgs...
  INSTALLED_PATTERNS_GEN=`echo "$PKGS_SPECS_TABLE" | grep '^yes' | cut -f 2 -d '|' |  sed -e 's%^%|%' -e 's%$%|%' -e 's%\\-%\\\\-%g'`
 
- echo "$INSTALLED_PATTERNS_GEN" >> /tmp/petget_installed_patterns_system
+ echo "$INSTALLED_PATTERNS_GEN" >> "$tmpDIR"/petget_installed_patterns_system
 
- echo "$PKG_CAT_BuildingBlock" |tr ' ' '\n' | sed -e 's%^%|%' -e 's%$%|%' -e 's%\\-%\\\\-%g' >>/tmp/petget_installed_patterns_system
+ echo "$PKG_CAT_BuildingBlock" |tr ' ' '\n' | sed -e 's%^%|%' -e 's%$%|%' -e 's%\\-%\\\\-%g' >>"$tmpDIR"/petget_installed_patterns_system
  sync
- sort -u /tmp/petget_installed_patterns_system > /tmp/petget_installed_patterns_systemx
- mv -f /tmp/petget_installed_patterns_systemx /tmp/petget_installed_patterns_system
+ sort -u "$tmpDIR"/petget_installed_patterns_system > "$tmpDIR"/petget_installed_patterns_systemx
+ mv -f "$tmpDIR"/petget_installed_patterns_systemx "$tmpDIR"/petget_installed_patterns_system
 fi
 
-[ "$DEBUG" ] && cat /tmp/petget_installed_patterns_system | grep -E 'libz|zlib'
+[ "$DEBUG" ] && cat "$tmpDIR"/petget_installed_patterns_system | grep -E 'libz|zlib'
 
-cp -f /tmp/petget_installed_patterns_system /tmp/petget_installed_patterns_all
+cp -f "$tmpDIR"/petget_installed_patterns_system "$tmpDIR"/petget_installed_patterns_all
 INSTALLED_PATTERNS_USER=`cat /root/.packages/user-installed-packages | cut -f 2 -d '|' | sed -e 's%^%|%' -e 's%$%|%' -e 's%\\-%\\\\-%g'`
-echo "$INSTALLED_PATTERNS_USER" >> /tmp/petget_installed_patterns_all
+echo "$INSTALLED_PATTERNS_USER" >> "$tmpDIR"/petget_installed_patterns_all
 
 #add these alias names to the installed patterns...
 #ALIASES_PATTERNS=`echo -n "$PKG_ALIASES_INSTALLED" | tr -s ' ' | sed -e 's%^ %%' -e 's% $%%' | tr ' ' '\n' | sed -e 's%^%|%' -e 's%$%|%' -e 's%\\-%\\\\-%g'`
-#echo "$ALIASES_PATTERNS" >> /tmp/petget_installed_patterns_all
+#echo "$ALIASES_PATTERNS" >> "$tmpDIR"/petget_installed_patterns_all
 
 #packages may have different names, add them to installed list...
-INSTALLEDALIASES=`grep --file=/tmp/petget_installed_patterns_all /tmp/petget_pkg_name_aliases_patterns | tr ',' '\n'`
-[ "$INSTALLEDALIASES" ] && echo "$INSTALLEDALIASES" >> /tmp/petget_installed_patterns_all
+INSTALLEDALIASES=`grep --file="$tmpDIR"/petget_installed_patterns_all "$tmpDIR"/petget_pkg_name_aliases_patterns | tr ',' '\n'`
+[ "$INSTALLEDALIASES" ] && echo "$INSTALLEDALIASES" >> "$tmpDIR"/petget_installed_patterns_all
 
 #clean it up...
-grep -v '^$' /tmp/petget_installed_patterns_all > /tmp/petget_installed_patterns_all-tmp
-mv -f /tmp/petget_installed_patterns_all-tmp /tmp/petget_installed_patterns_all
+grep -v '^$' "$tmpDIR"/petget_installed_patterns_all > "$tmpDIR"/petget_installed_patterns_all-tmp
+mv -f "$tmpDIR"/petget_installed_patterns_all-tmp "$tmpDIR"/petget_installed_patterns_all
 
-[ "$DEBUG" ] && cat /tmp/petget_installed_patterns_all | grep -E 'libz|zlib'
+[ "$DEBUG" ] && cat "$tmpDIR"/petget_installed_patterns_all | grep -E 'libz|zlib'
 
 #make pkg deps into patterns...
 PKGDEPS_PATTERNS=`echo -n "$DB_dependencies" | tr ',' '\n' | grep '^+' | sed -e 's%^+%%' -e 's%^%|%' -e 's%$%|%'`
-echo "$PKGDEPS_PATTERNS" > /tmp/petget_pkg_deps_patterns
+echo "$PKGDEPS_PATTERNS" > "$tmpDIR"/petget_pkg_deps_patterns
 
-[ "$DEBUG" ] && cat /tmp/petget_pkg_deps_patterns | grep -E 'libz|zlib'
+[ "$DEBUG" ] && cat "$tmpDIR"/petget_pkg_deps_patterns | grep -E 'libz|zlib'
 
 #remove installed pkgs from the list of dependencies...
-MISSINGDEPS_PATTERNS=`grep --file=/tmp/petget_installed_patterns_all -v /tmp/petget_pkg_deps_patterns | grep -v '^$'`
-echo "$MISSINGDEPS_PATTERNS" > /tmp/petget_missingpkgs_patterns #can be read by dependencies.sh, find_deps.sh.
+MISSINGDEPS_PATTERNS=`grep --file="$tmpDIR"/petget_installed_patterns_all -v "$tmpDIR"/petget_pkg_deps_patterns | grep -v '^$'`
+echo "$MISSINGDEPS_PATTERNS" > "$tmpDIR"/petget_missingpkgs_patterns #can be read by dependencies.sh, find_deps.sh.
 
-[ "$DEBUG" ] && cat /tmp/petget_missingpkgs_patterns | grep -E 'libz|zlib'
+[ "$DEBUG" ] && cat "$tmpDIR"/petget_missingpkgs_patterns | grep -E 'libz|zlib'
 
 #notes on results:
-#/tmp/petget_missingpkgs_patterns has a list of missing dependencies, format ex:
+#"$tmpDIR"/petget_missingpkgs_patterns has a list of missing dependencies, format ex:
 #  |kdebase|
 #  |delibs|
 #  |mesa-lib|
 #  |qt|
-#/tmp/petget_installed_patterns_all (read in dependencies.sh) has a list of already installed
+#"$tmpDIR"/petget_installed_patterns_all (read in dependencies.sh) has a list of already installed
 #  packages, both builtin and user-installed. One on each line, exs:
 #  |915resolution|
 #  |a52dec|
