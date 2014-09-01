74a724fefcffdf194a1699f783d71146662e9abb
KarlGodt <karlgodt@excite.de>
2014-09-01 14:30:07 -0100

    woof-code/rootfs-skeleton/usr/local/petget/testurls.sh: Header fixes and PPM: Use tmpDIR=/tmp/petget .

--- woof-code/rootfs-skeleton/usr/local/petget/testurls.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/testurls.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Called from /usr/local/petget/downloadpkgs.sh
-# /tmp/petget_repos has the list of repos, each line in this format:
+# "$tmpDIR"/petget_repos has the list of repos, each line in this format:
 # repository.slacky.eu|http://repository.slacky.eu/slackware-12.2|Packages-slackware-12.2-slacky
 # ...only the first field is of interest in this script.
 
@@ -23,27 +23,30 @@ echo "$0: START" >&2
 OUT=/dev/null;ERR=$OUT
 [ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
 
-echo '#!/bin/sh' >  /tmp/petget_urltest
-echo 'echo "Testing the URLs:"' >>  /tmp/petget_urltest
+tmpDIR=/tmp/petget
+test -d "$tmpDIR" || mkdir -p "$tmpDIR"
 
-for ONEURLSPEC in `cat /tmp/petget_repos`
+echo '#!/bin/sh' >  "$tmpDIR"/petget_urltest
+echo 'echo "Testing the URLs:"' >>  "$tmpDIR"/petget_urltest
+
+for ONEURLSPEC in `cat "$tmpDIR"/petget_repos`
 do
  URL_TEST="`echo -n "$ONEURLSPEC" | cut -f 1 -d '|'`"
 
  #[ "`wget -t 2 -T 20 --waitretry=20 --spider -S $ONE_PET_SITE -o /dev/stdout 2>/dev/null | grep '200 OK'`" != "" ]
 
- echo 'echo' >> /tmp/petget_urltest
- echo "wget -t 2 -T 20 --waitretry=20 --spider -S $URL_TEST" >> /tmp/petget_urltest
+ echo 'echo' >> "$tmpDIR"/petget_urltest
+ echo "wget -t 2 -T 20 --waitretry=20 --spider -S $URL_TEST" >> "$tmpDIR"/petget_urltest
 
 done
 
 echo 'echo "
 TESTING FINISHED
-Read the above, any that returned \"200 OK\" succeeded."' >>  /tmp/petget_urltest
+Read the above, any that returned \"200 OK\" succeeded."' >>  "$tmpDIR"/petget_urltest
 echo 'echo -n "Press ENTER key to exit: "
-read ENDIT'  >>  /tmp/petget_urltest
+read ENDIT'  >>  "$tmpDIR"/petget_urltest
 
-chmod 777 /tmp/petget_urltest
-rxvt -title "Puppy Package Manager: download" -bg orange -fg black -e /tmp/petget_urltest
+chmod 777 "$tmpDIR"/petget_urltest
+rxvt -title "Puppy Package Manager: download" -bg orange -fg black -e "$tmpDIR"/petget_urltest
 echo "$0: END" >&2
 ###END###
