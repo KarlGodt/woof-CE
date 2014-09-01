233ca7c7b41a3075539de65a8119a2ee5fbd6123
KarlGodt <karlgodt@excite.de>
2014-09-01 14:30:07 -0100

    woof-code/rootfs-skeleton/usr/local/petget/postfilterpkgs.sh: Header fixes and PPM: Use tmpDIR=/tmp/petget .

--- woof-code/rootfs-skeleton/usr/local/petget/postfilterpkgs.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/postfilterpkgs.sh
@@ -9,6 +9,23 @@
 #120813 fix subcategory icon.
 #120817 modify 'category' field: Document;edit becomes mini-Document-edit (will use icon mini-Document-edit.xpm)
 
+test -f /etc/rc.d/f4puppy5 && {
+source /etc/rc.d/f4puppy5
+
+ADD_PARAMETER_LIST=""
+ADD_PARAMETERS=""
+_provide_basic_parameters
+
+ADD_HELP_MSG="Helper script for /usr/local/petget/ petget and downloadpkgs.sh ."
+_parse_basic_parameters "$@"
+[ "$DO_SHIFT" ] && [ ! "${DO_SHIFT//[[:digit:]]/}" ] && {
+    for i in `seq 1 1 $DO_SHIFT`; do shift; done; }
+_trap
+}
+
+tmpDIR=/tmp/petget
+test -d "$tmpDIR" || mkdir -p "$tmpDIR"
+
 #ui_Ziggy and ui_Classic pass in two params, ex: EXE true
 [ $2 ] && echo -n "$2" > /var/local/petget/postfilter_${1}
 
@@ -21,39 +38,39 @@ DEF_CHK_NLS='false'
 [ -e /var/local/petget/postfilter_DEV ] && DEF_CHK_DEV="`cat /var/local/petget/postfilter_DEV`"
 [ -e /var/local/petget/postfilter_DOC ] && DEF_CHK_DOC="`cat /var/local/petget/postfilter_DOC`"
 [ -e /var/local/petget/postfilter_NLS ] && DEF_CHK_NLS="`cat /var/local/petget/postfilter_NLS`"
-cp -f /tmp/petget/filterpkgs.results /tmp/petget/filterpkgs.results.post
+cp -f "$tmpDIR"/filterpkgs.results "$tmpDIR"/filterpkgs.results.post
 
 #120525 quick filtering but not perfect...
 #PETs: _DEV _DOC _NLS
 #Ubuntu,Debian DEBs: -dev_ -doc_ -docs_ -langpack -lang-
 #Mageia RPMs: -devel- -doc-
-sed -i -e '/-dbg_/d' /tmp/petget/filterpkgs.results.post #120525 always take out the debug pkgs.
-[ "$DEF_CHK_DEV" = "false" ] && sed -i -e '/_DEV/d' -e '/-dev_/d' -e '/-devel-/d' /tmp/petget/filterpkgs.results.post
-[ "$DEF_CHK_DOC" = "false" ] && sed -i -e '/_DOC/d' -e '/-doc_/d' -e '/-docs_/d' -e '/-doc-/d' /tmp/petget/filterpkgs.results.post
-[ "$DEF_CHK_NLS" = "false" ] && sed -i -e '/_NLS/d' -e '/-langpack/d' -e '/-lang-/d' /tmp/petget/filterpkgs.results.post
+sed -i -e '/-dbg_/d' "$tmpDIR"/filterpkgs.results.post #120525 always take out the debug pkgs.
+[ "$DEF_CHK_DEV" = "false" ] && sed -i -e '/_DEV/d' -e '/-dev_/d' -e '/-devel-/d' "$tmpDIR"/filterpkgs.results.post
+[ "$DEF_CHK_DOC" = "false" ] && sed -i -e '/_DOC/d' -e '/-doc_/d' -e '/-docs_/d' -e '/-doc-/d' "$tmpDIR"/filterpkgs.results.post
+[ "$DEF_CHK_NLS" = "false" ] && sed -i -e '/_NLS/d' -e '/-langpack/d' -e '/-lang-/d' "$tmpDIR"/filterpkgs.results.post
 #120504b fix filtering out _EXE... 120515 must escape the dashes...
 if [ "$DEF_CHK_EXE" = "false" ];then
- grep -E '_DEV|_DOC|_NLS|\-dev_|\-doc_|\-docs_|\-langpack|\-lang\-|\-devel\-|\-doc\-' /tmp/petget/filterpkgs.results.post > /tmp/petget/filterpkgs.results.post.tmp
- mv -f /tmp/petget/filterpkgs.results.post.tmp /tmp/petget/filterpkgs.results.post
+ grep -E '_DEV|_DOC|_NLS|\-dev_|\-doc_|\-docs_|\-langpack|\-lang\-|\-devel\-|\-doc\-' "$tmpDIR"/filterpkgs.results.post > "$tmpDIR"/filterpkgs.results.post.tmp
+ mv -f "$tmpDIR"/filterpkgs.results.post.tmp "$tmpDIR"/filterpkgs.results.post
 fi
 
 ##120529 append an icon to each entry...
-#cp -f /tmp/petget/filterpkgs.results.post /tmp/petget/filterpkgs.results.post-noicons
+#cp -f "$tmpDIR"/filterpkgs.results.post "$tmpDIR"/filterpkgs.results.post-noicons
 #FLG_APPICONS="`cat /var/local/petget/flg_appicons`" #see configure.sh
 #if [ "$FLG_APPICONS" = "true" ];then
 # #ex: 'abiword0-1.2.3|description of abiword|stuff' becomes 'abiword|abiword0-1.2.3|description of abiword|stuff'
-# sed -i -r -e 's%(^[a-zA-Z]*)%\1|\1%' /tmp/petget/filterpkgs.results.post
+# sed -i -r -e 's%(^[a-zA-Z]*)%\1|\1%' "$tmpDIR"/filterpkgs.results.post
 #fi
 
 ##120811 icon name is now 2nd field, want append "mini-"...
 ###120813 also append "mini-" to subcategory, ex: Document;edit becomes mini-Document;mini-edit
 ##ex line: htop-0.9-i486|System|View Running Processes|puppy-wary5-official
 ##so, get "mini-System", which is name of an icon in /usr/local/lib/X11/mini-icons
-#sed -i -e 's%|%|mini-%' /tmp/petget/filterpkgs.results.post
+#sed -i -e 's%|%|mini-%' "$tmpDIR"/filterpkgs.results.post
 ###120813 finds first ; followed by a character, appends "mini-" before the char...
-##sed -i -e 's%|%|mini-%' -r -e 's%;([a-z])%;mini-\1%' /tmp/petget/filterpkgs.results.post
+##sed -i -e 's%|%|mini-%' -r -e 's%;([a-z])%;mini-\1%' "$tmpDIR"/filterpkgs.results.post
 
 #120817 category field: Document;edit becomes mini-Document-edit...
-sed -i -e 's%|%|mini-%' -e 's%;%-%' /tmp/petget/filterpkgs.results.post
+sed -i -e 's%|%|mini-%' -e 's%;%-%' "$tmpDIR"/filterpkgs.results.post
 
 ###END###
