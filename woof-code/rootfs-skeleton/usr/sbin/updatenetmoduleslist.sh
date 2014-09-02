29bde58febb7d99550addf49d4f22052c6d25d14
KarlGodt <karlgodt@excite.de>
2014-09-02 20:15:33 -0100

    woof-code/rootfs-skeleton/usr/sbin/updatenetmoduleslist.sh: Get rid of __old_header__ . The old header is now in f4puppy5 .

--- woof-code/rootfs-skeleton/usr/sbin/updatenetmoduleslist.sh
+++ woof-code/rootfs-skeleton/usr/sbin/updatenetmoduleslist.sh
@@ -5,36 +5,6 @@
 #w478 old k2.6.18.1 has madwifi modules (ath_pci.ko) in /lib/modules/2.6.18.1/net.
 #v423 now using busybox depmod, which generates modules.dep in "old" format.
 
-__old_header__(){
-###KRG Fr 31. Aug 23:34:58 GMT+1 2012
-
-trap "exit 1" HUP INT QUIT KILL TERM
-
-OUT=/dev/null;ERR=$OUT
-[ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
-[ "$DEBUG" = "2" ] && set -x
-
-Version='1.1'
-
-usage(){
-USAGE_MSG="
-$0 [ PARAMETERS ]
-
--V|--version : showing version information
--H|--help : show this usage information
-
-*******  *******  *******  *******  *******  *******  *******  *******  *******
-$2
-"
-exit $1
-}
-
-[ "`echo "$1" | grep -wE "\-help|\-H"`" ] && usage 0
-[ "`echo "$1" | grep -wE "\-version|\-V"`" ] && { echo "$0 -version $Version";exit 0; }
-
-###KRG Fr 31. Aug 23:34:58 GMT+1 2012
-}
-
 test -f /etc/rc.d/f4puppy5 && {
 source /etc/rc.d/f4puppy5
 
