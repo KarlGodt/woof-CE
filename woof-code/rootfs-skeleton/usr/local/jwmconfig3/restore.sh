2cbdb8ec31f948db160055cf168cd67069fbfcf7
KarlGodt <karlgodt@excite.de>
2014-09-03 21:09:03 -0100

    woof-code/rootfs-skeleton/usr/local/jwmconfig3/restore.sh: Double quote filenames.

--- woof-code/rootfs-skeleton/usr/local/jwmconfig3/restore.sh
+++ woof-code/rootfs-skeleton/usr/local/jwmconfig3/restore.sh
@@ -6,31 +6,28 @@
 PRO="restore.sh"
 echo $$
 SCRIPT_DIR="/usr/local/jwmconfig3"
-. $SCRIPT_DIR/path
+. "$SCRIPT_DIR/path"
 . /etc/rc.d/f4puppy5
 
 echo $DBG 12
 
 ##---backup--->
-cp -f $colorFILEbak "$colorFILEbak.0"
-cp -f $themeFILEbak "$themeFILEbak.0"
+cp -f "$colorFILEbak" "$colorFILEbak.0"
+cp -f "$themeFILEbak" "$themeFILEbak.0"
 
-cp -f $colorFILEbak $colorFILE
-cp -f $themeFILEbak $themeFILE
+cp -f "$colorFILEbak" "$colorFILE"
+cp -f "$themeFILEbak" "$themeFILE"
 
 # Patriot Sep 2009
   # Attempting some robustness
   # Update only for known -bg option applets: blinky and xload
 
-  . $colorFILE #Get MENU_BG, PAGER_BG
+  . "$colorFILE" #Get MENU_BG, PAGER_BG
 
-  . $SCRIPT_DIR/func -trayapply2
-
+  . "$SCRIPT_DIR/func" -trayapply2
 
   sync
 
   pidof jwm >$OUT && jwm -restart
-
-
 
 ###END###
