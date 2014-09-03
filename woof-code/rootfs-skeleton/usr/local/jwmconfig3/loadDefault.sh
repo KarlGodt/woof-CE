1e047e4137a4891453e1d961c443c5a8bb29ba4d
KarlGodt <karlgodt@excite.de>
2014-09-03 20:07:21 -0100

    woof-code/rootfs-skeleton/usr/local/jwmconfig3/loadDefault.sh: CLEANUPS.

--- woof-code/rootfs-skeleton/usr/local/jwmconfig3/loadDefault.sh
+++ woof-code/rootfs-skeleton/usr/local/jwmconfig3/loadDefault.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-PRO="/loadDefault.sh"
+
+PRO="loadDefault.sh"
 SCRIPT_DIR="/usr/local/jwmconfig3"
 . $SCRIPT_DIR/path
 
 
-. $ColorFileDef
+. $colorFILEdef
 
 if [ -z "$MENU_BG" ]; then
  pidof jwm >/dev/null && jwm -restart
  exit 2
-else 
+else
 
 . $SCRIPT_DIR/func -gradientcolors
- 
+
 . $SCRIPT_DIR/func -themedef
-  
+
 fi
 
-echo "$DefTheme" > "$ThemeFile"
+echo "$DefTheme" > "$themeFILE"
 
 #########
 #John Doe created code for the applet backgrounds, old jwmconfig, port here...
-. $SCRIPT_DIR/func -trayapply 
+. $SCRIPT_DIR/func -trayapply
 #end John Doe's code.
 ########
 
