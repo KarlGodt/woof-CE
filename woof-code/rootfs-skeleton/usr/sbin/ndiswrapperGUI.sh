913d0f3084169f276261c2b844be85a04e6a0040
KarlGodt <karlgodt@excite.de>
2014-09-05 19:46:19 -0100

    woof-code/rootfs-skeleton/usr/sbin/ndiswrapperGUI.sh: Header, use f4puppy5, clean ups.

--- woof-code/rootfs-skeleton/usr/sbin/ndiswrapperGUI.sh
+++ woof-code/rootfs-skeleton/usr/sbin/ndiswrapperGUI.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
-#(c) copyright Raul Suarez 2006 
-#Puppy ndiswrapper GUI setup script.
+#(c) copyright Raul Suarez 2006
+# Puppy ndiswrapper GUI setup script.
 ## updated by Dougal, November 20th 2007
 # Update: Sep. 16th 2008: replace all `` subshells with $()
 # Update: Oct. 5th:  rewrite selectDriverFile using gtkdialog
 # Update: Oct. 10th: add safeguards in case PREV_LOCATION is blank
 # Update: Oct. 11th: fix the "ok" of the chooser exit...
 # Update: Oct. 26th: localize
+
+  _TITLE_=
+_COMMENT_=
+
+MY_SELF="$0"
+
+test -f /etc/rc.d/f4puppy5 && {
+source /etc/rc.d/f4puppy5
+
+ADD_PARAMETER_LIST=""
+ADD_PARAMETERS=""
+_provide_basic_parameters
+
+ADD_HELP_MSG="$_COMMENT_"
+_parse_basic_parameters "$@"
+[ "$DO_SHIFT" ] && [ ! "${DO_SHIFT//[[:digit:]]/}" ] && {
+  for oneSHIFT in `seq 1 1 $DO_SHIFT`; do shift; done; }
+
+_trap
+}
 
 #=============================================================================
 #============= FUNCTIONS USED IN THE SCRIPT ==============
@@ -43,37 +63,37 @@ selectDriverFile()
     unset NETWIZ_Ndiswrapper_Chooser
 
     echo "$EXIT"
-	case $EXIT in 
-	 Cancel|abort) return 1 ;;
-	 OK) 
-	   PREV_LOCATION=${INF_FILE_NAME%/*}
+    case $EXIT in
+     Cancel|abort) return 1 ;;
+     OK)
+       PREV_LOCATION=${INF_FILE_NAME%/*}
        echo "$PREV_LOCATION" > "$CONFIG_DIR/prev_location"
        # an extra protection, in case the dialog can't handle blank...
        [ "$PREV_LOCATION" ] || PREV_LOCATION="$HOME"
-       case "$INF_FILE_NAME" in 
+       case "$INF_FILE_NAME" in
         *.[iI][nN][fF])
           return 0
           ;;
         *) # else
           giveErrorDialog "$L_MESSAGE_Bad_Inf_Name"
-          ;;     
+          ;;
        esac
-	   ;;
-	esac
+       ;;
+    esac
   done
 }
 #selectDriverFile()
 #{
   #while true ; do
     #INF_FILE_NAME="$(Xdialog --left --title \"Select the driver information file \(.INF\)\" \
-     #--stdout --no-buttons --fselect \"${PREV_LOCATION}/*\" 0 0)" 
+     #--stdout --no-buttons --fselect \"${PREV_LOCATION}/*\" 0 0)"
     #if [ $? -eq 0 ] ; then
       ##PREV_LOCATION="`expr "$INF_FILE_NAME" : '\(.*\)/'`"
       #PREV_LOCATION=${INF_FILE_NAME%/*}
       #echo "$PREV_LOCATION" > "$CONFIG_DIR/prev_location"
-      ##echo "$INF_FILE_NAME" | grep -Eiq "\.inf$" 
+      ##echo "$INF_FILE_NAME" | grep -Eiq "\.inf$"
       ##if [ $? -eq 0 ] ; then
-      #case "$INF_FILE_NAME" in 
+      #case "$INF_FILE_NAME" in
        #*.[iI][nN][fF])
          #selectDriverFile_RC=0
          #break
@@ -81,7 +101,7 @@ selectDriverFile()
        #*) # else
          #Xdialog --screen-center --title "$L_TITLE_Netwiz_Ndiswrapper" \
             #--msgbox "The file name should end in \".inf\"" 0 0
-         #;;     
+         #;;
       #esac #fi
     #else
       #selectDriverFile_RC=1
@@ -109,16 +129,16 @@ showNdiswrapperGUI()
     expr "$INF_FILE_NAME" : '.*/\(.*\).inf' | tr "[A-Z]" "[a-z]"
     ndiswrapper -i "$INF_FILE_NAME" > /tmp/net-setup_NDISWRAPPER_LOAD.txt
     NDISWRAPPER_RESULT=$?
-		case $NDISWRAPPER_RESULT in
-			0 | 25 | 255) 
-					Xdialog --left --screen-center --title "$L_TITLE_Netwiz_Ndiswrapper" \
+        case $NDISWRAPPER_RESULT in
+            0 | 25 | 255)
+                    Xdialog --left --screen-center --title "$L_TITLE_Netwiz_Ndiswrapper" \
               --msgbox "$(ndiswrapper -l)" 0 0
-		      return 0
+              return 0
           ;;
-			*) 	Xdialog --left --screen-center --title "L_TITLE_Netwiz_Ndiswrapper" \
+            *)  Xdialog --left --screen-center --title "L_TITLE_Netwiz_Ndiswrapper" \
               --msgbox "$(cat /tmp/net-setup_NDISWRAPPER_LOAD.txt)" 0 0
           ;;
-		esac
+        esac
   fi
   return 1
 } # end of showNdiswrapperGUI
@@ -131,8 +151,8 @@ showNdiswrapperGUI()
 # If ran by itself it shows the interface, Otherwise it's only used as a function library
 CURRENT_CONTEXT=$(expr "$0" : '.*/\(.*\)$' )
 if [ "${CURRENT_CONTEXT}" = "ndiswrapperGUI.sh" ] ; then
-	showNdiswrapperGUI
-fi 
+    showNdiswrapperGUI
+fi
 
 #=============================================================================
 #=============== END OF SCRIPT BODY ====================
