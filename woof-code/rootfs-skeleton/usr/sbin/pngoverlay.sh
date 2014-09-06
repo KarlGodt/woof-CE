af67d9433e771c8c610c19cc04aef6694a422ad2
KarlGodt <karlgodt@excite.de>
2014-09-06 03:27:38 -0100

    woof-code/rootfs-skeleton/usr/sbin/pngoverlay.sh: _TITLE_ , _COMMENT_ , ADD_PARAMETER and ADD_HELP .

--- woof-code/rootfs-skeleton/usr/sbin/pngoverlay.sh
+++ woof-code/rootfs-skeleton/usr/sbin/pngoverlay.sh
@@ -33,15 +33,15 @@ exit $1
 [ "`echo "$1" | grep -wE "\-version|\-V"`" ] && { echo "$0 -version $Version";exit 0; }
 }
 
-  _TITLE_=
-_COMMENT_=
+  _TITLE_=PNG_Overlay
+_COMMENT_="CLI to run pngtopam pamcomp pamrgbatopng"
 
 MY_SELF="$0"
 
 test -f /etc/rc.d/f4puppy5 && {
 source /etc/rc.d/f4puppy5
 
-ADD_PARAMETER_LIST=""
+ADD_PARAMETER_LIST="FILE1.png FILE2.png OUTFILE"
 ADD_PARAMETERS=""
 _provide_basic_parameters
 
@@ -62,7 +62,7 @@ _trap
 pngtopam -alphapam "${1}" > /tmp/pngoverlay_${$}_1.pam
 pngtopam -alphapam "${2}" > /tmp/pngoverlay_${$}_2.pam
 #1st image on top, 2nd on bottom, 3rd is output...
-pamcomp -mixtransparency /tmp/pngoverlay_${$}_2.pam /tmp/pngoverlay_${$}_1.pam > /tmp/pngoverlay_${$}_out.png 2> /dev/null
+pamcomp -mixtransparency /tmp/pngoverlay_${$}_2.pam /tmp/pngoverlay_${$}_1.pam > /tmp/pngoverlay_${$}_out.png 2>$ERR
 pamrgbatopng /tmp/pngoverlay_${$}_out.png > "${3}"
 rm -f /tmp/pngoverlay_${$}_1.pam
 rm -f /tmp/pngoverlay_${$}_2.pam
