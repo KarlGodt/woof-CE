0f7fe331e2a1caa2f8972243310d447ce347368c
KarlGodt <karlgodt@excite.de>
2014-09-08 19:58:43 -0100

    /usr/local/lib/xarchive/wrappers/rar-wrap.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/usr/local/lib/xarchive/wrappers/rar-wrap.sh
+++ woof-code/rootfs-skeleton/usr/local/lib/xarchive/wrappers/rar-wrap.sh
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+echo >>/tmp/xarchive_errs.log
 
 # set up exit status variables
 E_UNSUPPORTED=65
@@ -53,8 +54,10 @@ done
 # the shifting will leave the files passed as
 # all the remaining args "$@"
 opt="$1"
+echo opt $opt >>/tmp/xarchive_errs.log
 shift 1
 archive="$1"
+echo archive $archive >>/tmp/xarchive_errs.log
 shift 1
 
 # Command line options for prog functions
@@ -70,15 +73,15 @@ PASS_EXTRACT_OPTS="x -o-"
 case "$opt" in
     -i) # info: output supported extentions for progs that exist
         if [ ! "$AWK_PROG" ]; then
-            echo none of the awk programs $AWK_PROGS found >/dev/stderr
-            echo extentions $EXTS ignored >/dev/stderr
+            echo none of the awk programs $AWK_PROGS found >>/tmp/xarchive_errs.log
+            echo extentions $EXTS ignored >>/tmp/xarchive_errs.log
         elif [ "$(which $RAR_PROG)" ]; then
             for ext in $EXTS; do
                 printf "%s;" $ext
             done
         else
-            echo command $RAR_PROG not found > /dev/stderr 
-            echo extentions $EXTS ignored > /dev/stderr 
+            echo command $RAR_PROG not found >>/tmp/xarchive_errs.log 
+            echo extentions $EXTS ignored >>/tmp/xarchive_errs.log 
         fi
         printf "\n"
         exit
