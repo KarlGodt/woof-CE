3a80db596bf0538dcdb006d85d909d02fea1bae9
KarlGodt <karlgodt@excite.de>
2014-09-11 16:38:23 -0100

    woof-code/rootfs-skeleton/usr/sbin/e2ore3.sh: Stub to handle OPTIONS .

--- woof-code/rootfs-skeleton/usr/sbin/e2ore3.sh
+++ woof-code/rootfs-skeleton/usr/sbin/e2ore3.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 #Lesser GPL licence v2 (http://www.fsf.org/licensing/licenses/lgpl.html)
-#used by probepart.
-#read device, example '/dev/hda4' from stdin.
+# Used by probepart.
+# Read device, example '/dev/hda4' from stdin.
 
+test "$*" && {
+    :
+} || {
 read MYDEVICE
 [ "$MYDEVICE" = "" ] && exit
 MYSTR="`tune2fs -l $MYDEVICE | grep 'Filesystem features:' | grep 'has_journal'`"
 
 [ "$MYSTR" = "" ] && exit 0 #ext2
 exit 1 #ext3
+}
