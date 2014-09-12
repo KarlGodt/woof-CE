4f389fdf502b8ebef4cb941121b9d6a19914ba70
KarlGodt <karlgodt@excite.de>
2014-09-12 15:05:48 -0100

    woof-code/rootfs-skeleton/usr/sbin/pngoverlay.sh: Added Header.

--- woof-code/rootfs-skeleton/usr/sbin/pngoverlay.sh
+++ woof-code/rootfs-skeleton/usr/sbin/pngoverlay.sh
@@ -7,6 +7,29 @@
 # bottom-image top-image output-image
 #overlays the two images, with common areas of transparency in output image.
 
+  _TITLE_=
+_COMMENT_=""
+
+MY_SELF="$0"
+
+test -f /etc/rc.d/f4puppy5 && {
+ set +e
+ source /etc/rc.d/f4puppy5 && {
+ set +n
+ source /etc/rc.d/f4puppy5; } || echo "WARNING : Could not source /etc/rc.d/f4puyppy5 ."
+
+ADD_PARAMETER_LIST=
+ADD_PARAMETERS=
+_provide_basic_parameters
+
+ADD_HELP_MSG="$_COMMENT_"
+_parse_basic_parameters "$@"
+[ "$DO_SHIFT" ] && [ ! "${DO_SHIFT//[[:digit:]]/}" ] && {
+for i in `seq 1 1 $DO_SHIFT`; do shift; done; }
+
+_trap
+} || echo "Warning : No /etc/rc.d/f4puppy5 installed."
+
 [ ! $3 ] && exit 1
 [ ! -e "$1" ] && exit 1
 [ ! -e "$2" ] && exit 1
