78df8eabb839606f01b5a7142294458a0477458f
KarlGodt <karlgodt@excite.de>
2014-09-13 16:57:08 -0100

    /usr/sbin/youtube.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/usr/sbin/youtube.sh
+++ woof-code/rootfs-skeleton/usr/sbin/youtube.sh
@@ -11,34 +11,34 @@ use_err() {
   exit 65
 }
 
-if [ ! $1 ]; then use_err; fi
+if [ ! "$1" ]; then use_err; fi
 
 while getopts ":f:" Option
 do
   case $Option in
-    f     ) fn=$OPTARG;;
+    f     ) fn="$OPTARG";;
     *     ) use_err;;
   esac
 done
 
 shift $(($OPTIND - 1))
 
-if [ -z $1 ];  then use_err; fi
-if [ -z $fn ]; then fn=$1; fi
+if [ -z "$1" ];  then use_err; fi
+if [ -z "$fn" ]; then fn="$1"; fi
 
-vidID=$1
-filename=$fn".mp4"
+vidID="$1"
+filename="${fn}".mp4
 geturl() { echo "GET $1 HTTP/1.1";echo "Host: $2";echo;echo;sleep 2;echo '^C'; }
 
 echo "Finding hostname"
 hostl=`geturl /watch youtube.com | nc youtube.com 80 | grep Location | sed 's|Location: http://\([^/]*\)/.*|\1|' | tr -d '\r\n'`
-echo "Connecting to "$hostl
+echo "Connecting to ""$hostl"
 
-watch="/watch?v="$vidID
-tid=`geturl $watch $hostl | nc youtube.com 80 | grep '"t":' | sed 's/.*"t": "\([^"]*\)".*/\1/'`
-
+watch='/watch?v='"$vidID"
+tid=`geturl "$watch" "$hostl" | nc youtube.com 80 | grep '"t":' | sed 's/.*"t": "\([^"]*\)".*/\1/'`
+echo "tid='$tid'"
 echo "Locating video file"
-get_video="/get_video?video_id="$vidID"&t="$tid"&el=detailpage&ps=&fmt=18"
-url=`geturl $get_video $hostl | nc youtube.com 80 | grep Location | sed 's/Location: \(.*\)$/\1/' | tr -d '\r\n'`
+get_video='/get_video?video_id='"$vidID"'&t='"$tid"'&el=detailpage&ps=&fmt=18'
+url=`geturl "$get_video" "$hostl" | nc youtube.com 80 | grep Location | sed 's/Location: \(.*\)$/\1/' | tr -d '\r\n'`
 
-wget -O - -t 7 -w 5 --waitretry=14 --random-wait '--user-agent=Mozilla/5.0' -e robots=off $url > $filename
+wget -O - -t 7 -w 5 --waitretry=14 --random-wait '--user-agent=Mozilla/5.0' -e robots=off "$url" > "$filename"
