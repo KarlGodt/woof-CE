a9c418122d5edffa07064b05dfdfc86cd5d43e06
KarlGodt <karlgodt@excite.de>
2014-09-17 10:57:14 -0100

    jump_over_old_header.sh: Security fixes .

--- jump_over_old_header.sh
+++ jump_over_old_header.sh
@@ -1,39 +1,105 @@
 #!/bin/ash
 
-{ echo "FIX ME"; exit 1; }
-QUIET=-q
+#{ echo "FIX ME"; exit 1; }
+
+
+# defaults
+QUIET=-q # -q option for grep
+GIT=1   # change bang in git repo
+SYS=   # change bang in current OS
+DRY=  #dry run - do not do actually change bang
+while [ "$1" ]; do
+case "$1" in
+-D) DRY=1;QUIET=-q;;
+-d) set -x;;
+-S) GIT='';SYS=1;;
+-v) VERBOSE=1;oQUIET=1;;
+*) echo "$0:Unrecognized option '$1'"; exit 1;;
+esac
+shift
+done
+[ "$oQUIET" ] && QUIET='';
+echo "${0##*/}:GIT=$GIT' SYS=$SYS' DRY=$DRY'"
+
 
 pwd
 
 cd `pwd`/woof-code/rootfs-skeleton/ || { echo "Could not change into `pwd`/woof-code/rootfs-skeleton/"; exit 1; }
 
+
+
 for file in bin/* sbin/* usr/bin/* usr/sbin/* usr/local/*/*
 do
    [ -L "$file" ] && continue
    [ -f "$file" ] || continue
-   file "$file" | grep -i text | grep $QUIET -viE 'ISO-8859|murga\-lua\-dynamic|perl|python|pascal|bacon|ASCII C program|C++ program|HTML document|X pixmap image|PNG image data' || { echo "$file is not a shell script file by detection";continue; }
+   file "$file" | grep -i text | grep $QUIET -viE 'ISO-8859|murga\-lua\-dynamic|perl|python|pascal|bacon|ASCII C program|C++ program|HTML document|X pixmap image|PNG image data' || { [ "$QUIET" ] || echo "$file is not a shell script file by detection"; continue; }
    case "$file" in
-   *.txt|*.TXT|*rc|*.xpm|*.svg|*.png|*.jpg|*.wav|*.au|*.bac|*.dat|*.pupdev|*.gs|*Help) echo "$file is not a shell script file by EXT"; continue;;
+   *.txt|*.TXT|*rc|*.xpm|*.svg|*.png|*.jpg|*.wav|*.au|*.bac|*.dat|*.pupdev|*.gs|*Help)
+   [ "$QUIET" ] || echo "$file is not a shell script file by EXT"; continue;;
    esac
 
-   grep $QUIET '__old_default_info_header__(){' "$file" && continue
-   grep $QUIET 'f4puppy5' "$file" || { echo "$file has no f4puppy5";continue; }
+   gPATTERN1='__old_header__(){'
+   grep $QUIET "$gPATTERN1" "$file" && { [ "$QUIET" ] || echo "$file has '$gPATTERN1'"; continue; }
+   gPATTERN2='f4puppy5'
+   grep $QUIET "$gPATTERN2" "$file" || { [ "$QUIET" ] || echo "$file has no '$gPATTERN2'"; continue; }
 
-   test "`grep -m2 '_TITLE_=' "$file" | wc -l`" -ge 2 || continue
+   #test "`grep -m2 '_TITLE_=' "$file" | wc -l`" -ge 2 || continue
 
    echo "$file"
    #geany "$file" && sleep 1
 
-   lineNR1=`grep -n -m2 '_TITLE_=' "$file" | tail -n1 | awk -F'[: ]' '{print $1}'`
+   unset lineNR1 lineNR2
+   #gPATTERN90='^###KRG Fr 31. Aug'
+   gPATTERN90='trap\ \"exit\ 1\"\ HUP\ INT\ QUIT\ KILL\ TERM'
+   lineNR1=`grep $QUIET -n -m1 "$gPATTERN90" "$file" | tail -n1 | awk -F'[: ]' '{print $1}'`
+   echo -e "\\033[1;31mlineNR1=$lineNR1'\\033[0;39m"
    [ "$lineNR1" ] || continue
    [ "${lineNR1//[0-9]/}" ] && continue
    [ "`echo "$lineNR1" | wc -l`" != 1 ] && continue
-   sed -i".bak$$" "$lineNR1 i\__orig_default_header__(){" "$file" || break
-   sleep 0.2
-   lineNR2=`grep -n -m2 '#\*\*\*\*\*\*\*\*\*\*\*\*' "$file" |tail -n1 | awk -F'[: ]' '{print $1}'`
+
+   #gPATTERN91='^###KRG Fr 31. Aug'
+   gPATTERN91='^\[\ \"\`echo\ \"\$1\"\ \|\ grep\ \-wE\ \"\-version\|\-V\"`\"\ \]\ '
+   lineNR2=`grep $QUIET -n "$gPATTERN91" "$file" | tail -n1 | awk -F'[: ]' '{print $1}'`
+   echo -e "\\033[1;33mlineNR2=$lineNR2'\\033[0;39m"
    [ "$lineNR2" ] || continue
    [ "${lineNR2//[0-9]/}" ] && continue
    [ "`echo "$lineNR2" | wc -l`" != 1 ] && continue
-   sed -i".bak2$$" "$lineNR2 a\}" "$file" || break
 
+   [ "$lineNR1" -a "$lineNR2" ] || {
+     echo "$file : One of lineNR missing"
+    }
+   [ "$lineNR1" = "$lineNR2" ] && { echo "lineNR1 is lineNR2"; continue; }
+
+
+
+   [ "$DRY" ] && {
+       echo -e "\\033[1;32mWould alter '$file'\\033[0;39m"
+       } || {
+        sPATTERN11="$gPATTERN1"
+        sPATTERN1="$lineNR1 "'i \\'"$sPATTERN11"
+        echo "'$sPATTERN1'"
+       #sed -i".bak$$" "$lineNR1 i\__orig_default_header__(){" "$file" || break
+       #sed -i".bak$$" "$lineNR1 i\\""$gPATTERN1" "$file"             #|| break
+       [ "$VERBOSE" ] && cat "$file" | sed "$sPATTERN1" | head -n $((lineNR1+5)) | tail -n10
+        sed -i".bak$$" "$sPATTERN1" "$file"
+       [ $? = 0 ] || { echo "sed 1 : Failure"; break; }
+       sleep 0.2
+       }
+
+
+
+   [ "$DRY" ] && {
+       echo -e "\\033[1;32mWould alter '$file'\\033[0;39m"
+       break
+       } || {
+       sPATTERN2="$((lineNR2+1)) a \\}"
+       echo "'$sPATTERN2'"
+       [ "$VERBOSE" ] && cat "$file" | sed "$sPATTERN2" | head -n $((lineNR2+5)) | tail -n10
+       #sed -i".bak2$$" "$((lineNR2+1)) a\}" "$file" || break
+        sed -i".bak2$$" "$sPATTERN2" "$file"   #|| break
+       [ $? = 0 ] && rm "${file}.bak"* || { echo "sed 2 : Failure"; break; }
+       sleep 0.2
+       defaulttexteditor "$file"
+       #break
+        }
 done
