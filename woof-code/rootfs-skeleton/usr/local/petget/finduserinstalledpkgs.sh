9a5d8d5396233331cae35af1138d5a1f6b52905d
KarlGodt <karlgodt@excite.de>
2014-09-17 18:21:40 -0100

    woof-code/rootfs-skeleton/usr/local/petget/finduserinstalledpkgs.sh: batch retire __old_header__() .

--- woof-code/rootfs-skeleton/usr/local/petget/finduserinstalledpkgs.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/finduserinstalledpkgs.sh
@@ -30,21 +30,16 @@ _trap
 #called from /usr/local/petget/pkg_chooser.sh
 #find all pkgs that have been user-installed, format for display.
 
-
+__old_header__(){  #BEGIN
 ###KRG Fr 31. Aug 23:34:58 GMT+1 2012
 
-
-
 trap "exit 1" HUP INT QUIT KILL TERM
-
 
 OUT=/dev/null;ERR=$OUT
 [ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
 [ "$DEBUG" = "2" ] && set -x
 
-
 Version='1.1'
-
 
 usage(){
 USAGE_MSG="
@@ -62,10 +57,10 @@ exit $1
 [ "`echo "$1" | grep -wiE "help|\-H"`" ] && usage 0
 [ "`echo "$1" | grep -wiE "\-version|\-V"`" ] && { echo "$0 -version $Version";exit 0; }
 
-echo "$0:$*" >&2
-
 ###KRG Fr 31. Aug 23:34:58 GMT+1 2012
+}  ###__old_header__(){ #END
 
+echo "$0:$*" >&2
 
 #/root/.packages/usr-installed-packages has the list of installed pkgs...
 touch /root/.packages/user-installed-packages
