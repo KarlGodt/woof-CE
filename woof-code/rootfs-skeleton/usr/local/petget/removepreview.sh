2b00e0736e8db66b71afa04a7cc0856dde0c09ab
KarlGodt <karlgodt@excite.de>
2014-09-17 21:31:06 -0100

    woof-code/rootfs-skeleton/usr/local/petget/removepreview.sh: __old_header__() function removed.

--- woof-code/rootfs-skeleton/usr/local/petget/removepreview.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/removepreview.sh
@@ -38,36 +38,6 @@ _trap
 #v424 need info box if user has clicked when no pkgs installed.
 
 
-__old_header__(){  #BEGIN
-###KRG Fr 31. Aug 23:34:58 GMT+1 2012
-
-trap "exit 1" HUP INT QUIT KILL TERM
-
-OUT=/dev/null;ERR=$OUT
-[ "$DEBUG" ] && { OUT=/dev/stdout;ERR=/dev/stderr; }
-[ "$DEBUG" = "2" ] && set -x
-
-Version='1.1'
-
-usage(){
-USAGE_MSG="
-$0 [ PARAMETERS ]
-
--V|--version : showing version information
--H|--help : show this usage information
-
-*******  *******  *******  *******  *******  *******  *******  *******  *******
-$2
-"
-exit $1
-}
-
-[ "`echo "$1" | grep -wiE "help|\-H"`" ] && usage 0
-[ "`echo "$1" | grep -wiE "\-version|\-V"`" ] && { echo "$0 -version $Version";exit 0; }
-
-echo "$0:$*" >&2
-###KRG Fr 31. Aug 23:34:58 GMT+1 2012
-}  ###__old_header__(){  #END  ###__old_header__(){ #END
 
 . /etc/DISTRO_SPECS #has DISTRO_BINARY_COMPAT, DISTRO_COMPAT_VERSION
 . /root/.packages/DISTRO_PKGS_SPECS
