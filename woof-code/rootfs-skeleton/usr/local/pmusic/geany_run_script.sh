2228a8e0d6bb2411a39c34dc7353a4b1fa96f89a
KarlGodt <karlgodt@excite.de>
2014-09-18 15:29:20 -0100

    woof-code/rootfs-skeleton/usr/local/pmusic/geany_run_script.sh: Header added and other header and END related batch script fixes.

--- woof-code/rootfs-skeleton/usr/local/pmusic/geany_run_script.sh
+++ woof-code/rootfs-skeleton/usr/local/pmusic/geany_run_script.sh
@@ -14,3 +14,5 @@ echo "Press return to continue"
 #to be more compatible with shells like dash
 dummy_var=""
 read dummy_var
+# Very End of this file 'usr/local/pmusic/geany_run_script.sh' #
+###END###
