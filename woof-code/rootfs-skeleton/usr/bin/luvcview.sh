a4fbcff28ccdc957d364cb5f173727d82f18dc3e
KarlGodt <karlgodt@excite.de>
2014-09-23 22:31:05 -0100

    /usr/bin/luvcview.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/usr/bin/luvcview.sh
+++ woof-code/rootfs-skeleton/usr/bin/luvcview.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 luvcview >/tmp/luvcview 2>&1
-ERROR=`grep -i "error" /tmp/luvcview`
+ERROR=`grep -iE 'error|exit fatal' /tmp/luvcview`
 if [[ "$ERROR" != "" ]];then
- Xdialog  --msgbox "Luvcview has encountered an error. \n please unsure your webcam is connected \n and configured correctly" 0 0 0
- exit
+ Xdialog  --msgbox "Luvcview has encountered an error. \n please ensure your webcam is connected \n and configured correctly" 0 0 0
+ exit 1
 fi
+
