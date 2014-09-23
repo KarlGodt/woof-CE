357e08d15cef006b8baa8e9a8f65cf36569a1032
KarlGodt <karlgodt@excite.de>
2014-09-23 22:29:22 -0100

    /usr/sbin/ice_config.sh : Attempt to use ash instead bash ... ash has no export -f functionality. Also use rpl.sh . Freak code ...

--- woof-code/rootfs-skeleton/usr/sbin/ice_config.sh
+++ woof-code/rootfs-skeleton/usr/sbin/ice_config.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
-#icewm configuration ver 0.6
-#01micko gpl 2010
+#!/bin/ash
+# icewm configuration ver 0.6
+#  01micko gpl 2010
 # Credit to tronkel for the idea, thanks Jack
 #20100730
 #20100731 #added check to see if icewm is running, added taskbar position, top/bottom, added autohide option
 #20100801 #added window list on taskbar
 #20100802 #added show/hide cpu status
 
-  _TITLE_="Puppy_ice_config.sh"
+  _TITLE_="Puppy_ice_config"
 _VERSION_=1.0omega
-_COMMENT_="$_TITLE_:Puppy Linux shell script [to TODO here]"
+_COMMENT_="$_TITLE_:Puppy Linux shell script
+with GTKdialog GUI to alter a few variables in
+$HOME/.icewm/preferences
+config file ."
 
 MY_SELF="$0"
 MY_PID=$$
@@ -31,14 +34,21 @@ for i in `seq 1 1 $DO_SHIFT`; do shift; done; }
 
 _trap
 } || echo "Warning : No /etc/rc.d/f4puppy5 installed."
+# various variables if not set globally in f4puppy5
+VERBOSE=1
+QUIET=
+VERB=-v
 
+__check_current_wm__(){
 CURWM=`head -c5 /etc/windowmanager`
 if [[ $CURWM != icewm ]];then Xdialog --title "Icewm" --timeout 6 --msgbox "You are not running Icewm. \nIcewm needs to be running to use this tool."  0 0 0 &
         exit
 fi
+}
+
 IMGDIR="/usr/share/pixmaps"
 
-. $HOME/.icewm/preferences 2>/dev/null
+. $HOME/.icewm/preferences 2>$ERR
 if [ "$ShowTaskBar" = "1" ];then CHKBOX1=true
         else CHKBOX1=false
 fi
@@ -64,38 +74,45 @@ if [ "$TaskBarShowCPUStatus" = "0" ];then CHKBOX8=true
         else CHKBOX8=false
 fi
 #desktop number function
-function DESKTOPS_old(){
+#function
+DESKTOPS_old(){
         REPLACE=`grep 'WorkspaceNames' $HOME/.icewm/preferences`
-if [ "$NUM" = "1" ];then rpl -q "$REPLACE" "WorkspaceNames = \" 1 \"" $HOME/.icewm/preferences
-elif [ "$NUM" = "2" ];then rpl -q "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \"" $HOME/.icewm/preferences
-elif [ "$NUM" = "3" ];then rpl -q "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \"" $HOME/.icewm/preferences
-elif [ "$NUM" = "4" ];then rpl -q "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \"" $HOME/.icewm/preferences
-elif [ "$NUM" = "5" ];then rpl -q "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \", \" 5 \"" $HOME/.icewm/preferences
-elif [ "$NUM" = "6" ];then rpl -q "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \", \" 5 \", \" 6 \"" $HOME/.icewm/preferences
-elif [ "$NUM" = "7" ];then rpl -q "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \", \" 5 \", \" 6 \", \" 7 \"" $HOME/.icewm/preferences
-elif [ "$NUM" = "8" ];then rpl -q "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \", \" 5 \", \" 6 \", \" 7\", \" 8 \"" $HOME/.icewm/preferences
+if [ "$NUM" = "1" ];then rpl.sh $QUIET $VERB "$REPLACE" "WorkspaceNames = \" 1 \"" $HOME/.icewm/preferences
+elif [ "$NUM" = "2" ];then rpl.sh $QUIET $VERB "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \"" $HOME/.icewm/preferences
+elif [ "$NUM" = "3" ];then rpl.sh $QUIET $VERB "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \"" $HOME/.icewm/preferences
+elif [ "$NUM" = "4" ];then rpl.sh $QUIET $VERB "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \"" $HOME/.icewm/preferences
+elif [ "$NUM" = "5" ];then rpl.sh $QUIET $VERB "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \", \" 5 \"" $HOME/.icewm/preferences
+elif [ "$NUM" = "6" ];then rpl.sh $QUIET $VERB "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \", \" 5 \", \" 6 \"" $HOME/.icewm/preferences
+elif [ "$NUM" = "7" ];then rpl.sh $QUIET $VERB "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \", \" 5 \", \" 6 \", \" 7 \"" $HOME/.icewm/preferences
+elif [ "$NUM" = "8" ];then rpl.sh $QUIET $VERB "$REPLACE" "WorkspaceNames = \" 1 \", \" 2 \", \" 3 \", \" 4 \", \" 5 \", \" 6 \", \" 7\", \" 8 \"" $HOME/.icewm/preferences
         fi
 }
 
-function DESKTOPS(){
+#function
+DESKTOPS(){
         REPLACE=`grep 'WorkspaceNames' $HOME/.icewm/preferences`
         for num in `seq 1 1 $NUM`;do
 STRING="$STRING \" $num \","
         done
 STRING="${STRING%,*}"
 echo "$STRING"
-rpl -q "$REPLACE" "WorkspaceNames = $STRING" $HOME/.icewm/preferences
+rpl.sh $QUIET $VERB "$REPLACE" "WorkspaceNames = $STRING" $HOME/.icewm/preferences
 }
-export -f DESKTOPS
+#export -f DESKTOPS
 
-function MSG(){
+#function
+__MSG__(){
         Xdialog --title "Icewm" --timeout 10 --msgbox "Please restart Icewm from: \n Start > Shutdown > Restart Icewm"  0 0 0 &
 }
-export -f MSG
+#export -f MSG
+MSG="Xdialog --title \"Icewm\" --timeout 10 --msgbox \"Please restart Icewm from: \n Start > Shutdown > Restart Icewm\"  0 0 0 &"
+echo "#!/bin/ash
+$MSG" >/tmp/ice_config_ok.sh
+chmod +x /tmp/ice_config_ok.sh
 
 NUM="`grep 'WorkspaceNames' $HOME/.icewm/preferences|tail -c4|head -c1`"
 NUMBERS="<item>$NUM</item>"
-for I in `seq 1 1 8`; do NUMBERS=`echo "$NUMBERS<item>$I</item>"`; done
+for I in `seq 0 1 9`; do NUMBERS=`echo "$NUMBERS<item>$I</item>"`; done
 
 #MAIN GUI
 export MAIN="
@@ -110,57 +127,57 @@ export MAIN="
    <checkbox tooltip-text=\"Checked shows Taskbar, Unchecked hides Taskbar\">
      <label>Toggles Show/Hide Taskbar</label>
           <variable>CHKBOX1</variable>
-          <action>if true rpl 'ShowTaskBar=0' 'ShowTaskBar=1' $HOME/.icewm/preferences 2>/dev/null</action>
-          <action>if false rpl 'ShowTaskBar=1' 'ShowTaskBar=0' $HOME/.icewm/preferences 2>/dev/null</action>
+          <action>if true rpl.sh $QUIET $VERB 'ShowTaskBar=0' 'ShowTaskBar=1' $HOME/.icewm/preferences 2>$ERR</action>
+          <action>if false rpl.sh $QUIET $VERB 'ShowTaskBar=1' 'ShowTaskBar=0' $HOME/.icewm/preferences 2>$ERR</action>
           <default>$CHKBOX1</default>
         </checkbox>
         <checkbox tooltip-text=\"Checked shows Show Desktop Button, Unchecked hides Show Desktop Button\">
      <label>Toggles Show/Hide Show Desktop Button</label>
           <variable>CHKBOX2</variable>
-          <action>if true rpl 'TaskBarShowShowDesktopButton=0' 'TaskBarShowShowDesktopButton=1' $HOME/.icewm/preferences 2>/dev/null</action>
-          <action>if false rpl 'TaskBarShowShowDesktopButton=1' 'TaskBarShowShowDesktopButton=0' $HOME/.icewm/preferences 2>/dev/null</action>
+          <action>if true rpl.sh $QUIET $VERB 'TaskBarShowShowDesktopButton=0' 'TaskBarShowShowDesktopButton=1' $HOME/.icewm/preferences 2>$ERR</action>
+          <action>if false rpl.sh $QUIET $VERB 'TaskBarShowShowDesktopButton=1' 'TaskBarShowShowDesktopButton=0' $HOME/.icewm/preferences 2>$ERR</action>
           <default>$CHKBOX2</default>
         </checkbox>
         <checkbox tooltip-text=\"Checked shows Taskbar Window Icons, Unchecked hides Taskbar Window Icons\">
      <label>Toggles Show/Hide Taskbar Window Icons</label>
           <variable>CHKBOX3</variable>
-          <action>if true rpl 'TaskBarShowWindowIcons=0' 'TaskBarShowWindowIcons=1' $HOME/.icewm/preferences 2>/dev/null</action>
-          <action>if false rpl 'TaskBarShowWindowIcons=1' 'TaskBarShowWindowIcons=0' $HOME/.icewm/preferences 2>/dev/null</action>
+          <action>if true rpl.sh $QUIET $VERB 'TaskBarShowWindowIcons=0' 'TaskBarShowWindowIcons=1' $HOME/.icewm/preferences 2>$ERR</action>
+          <action>if false rpl.sh $QUIET $VERB 'TaskBarShowWindowIcons=1' 'TaskBarShowWindowIcons=0' $HOME/.icewm/preferences 2>$ERR</action>
           <default>$CHKBOX3</default>
         </checkbox>
         <checkbox tooltip-text=\"Checked shows normal Taskbar Height, Unchecked Doubles Taskbar Height\">
      <label>Toggles Single/Double Taskbar Height</label>
           <variable>CHKBOX4</variable>
-          <action>if true rpl 'TaskBarDoubleHeight=1' 'TaskBarDoubleHeight=0' $HOME/.icewm/preferences 2>/dev/null</action>
-          <action>if false rpl 'TaskBarDoubleHeight=0' 'TaskBarDoubleHeight=1' $HOME/.icewm/preferences 2>/dev/null</action>
+          <action>if true rpl.sh $QUIET $VERB 'TaskBarDoubleHeight=1' 'TaskBarDoubleHeight=0' $HOME/.icewm/preferences 2>$ERR</action>
+          <action>if false rpl.sh $QUIET $VERB 'TaskBarDoubleHeight=0' 'TaskBarDoubleHeight=1' $HOME/.icewm/preferences 2>$ERR</action>
           <default>$CHKBOX4</default>
         </checkbox>
         <checkbox tooltip-text=\"Checked shows Taskbar at bottom of screen, Unchecked at top\">
      <label>Toggles Bottom/Top Taskbar Position</label>
           <variable>CHKBOX5</variable>
-          <action>if true rpl 'TaskBarAtTop=1' 'TaskBarAtTop=0' $HOME/.icewm/preferences 2>/dev/null</action>
-          <action>if false rpl 'TaskBarAtTop=0' 'TaskBarAtTop=1' $HOME/.icewm/preferences 2>/dev/null</action>
+          <action>if true rpl.sh $QUIET $VERB 'TaskBarAtTop=1' 'TaskBarAtTop=0' $HOME/.icewm/preferences 2>$ERR</action>
+          <action>if false rpl.sh $QUIET $VERB 'TaskBarAtTop=0' 'TaskBarAtTop=1' $HOME/.icewm/preferences 2>$ERR</action>
           <default>$CHKBOX5</default>
         </checkbox>
         <checkbox tooltip-text=\"Unchecked Auto Hides the Taskbar\">
      <label>Toggles Show/Autohide the Taskbar</label>
           <variable>CHKBOX6</variable>
-          <action>if true rpl 'TaskBarAutoHide=1' 'TaskBarAutoHide=0' $HOME/.icewm/preferences 2>/dev/null</action>
-          <action>if false rpl 'TaskBarAutoHide=0' 'TaskBarAutoHide=1' $HOME/.icewm/preferences 2>/dev/null</action>
+          <action>if true rpl.sh $QUIET $VERB 'TaskBarAutoHide=1' 'TaskBarAutoHide=0' $HOME/.icewm/preferences 2>$ERR</action>
+          <action>if false rpl.sh $QUIET $VERB 'TaskBarAutoHide=0' 'TaskBarAutoHide=1' $HOME/.icewm/preferences 2>$ERR</action>
           <default>$CHKBOX6</default>
         </checkbox>
         <checkbox tooltip-text=\"Checked hides Window List Button on Taskbar, Unchecked shows\">
      <label>Toggles Show/Hide Window List button</label>
           <variable>CHKBOX7</variable>
-          <action>if true rpl 'TaskBarShowWindowListMenu=1' 'TaskBarShowWindowListMenu=0' $HOME/.icewm/preferences 2>/dev/null</action>
-          <action>if false rpl 'TaskBarShowWindowListMenu=0' 'TaskBarShowWindowListMenu=1' $HOME/.icewm/preferences 2>/dev/null</action>
+          <action>if true rpl.sh $QUIET $VERB 'TaskBarShowWindowListMenu=1' 'TaskBarShowWindowListMenu=0' $HOME/.icewm/preferences 2>$ERR</action>
+          <action>if false rpl.sh $QUIET $VERB 'TaskBarShowWindowListMenu=0' 'TaskBarShowWindowListMenu=1' $HOME/.icewm/preferences 2>$ERR</action>
           <default>$CHKBOX7</default>
         </checkbox>
         <checkbox tooltip-text=\"Checked hides CPU Status in System Tray, Unchecked shows\">
      <label>Toggles Hide/Show CPU Status in System Tray</label>
           <variable>CHKBOX8</variable>
-          <action>if true rpl 'TaskBarShowCPUStatus=1' 'TaskBarShowCPUStatus=0' $HOME/.icewm/preferences 2>/dev/null</action>
-          <action>if false rpl 'TaskBarShowCPUStatus=0' 'TaskBarShowCPUStatus=1' $HOME/.icewm/preferences 2>/dev/null</action>
+          <action>if true rpl.sh $QUIET $VERB 'TaskBarShowCPUStatus=1' 'TaskBarShowCPUStatus=0' $HOME/.icewm/preferences 2>$ERR</action>
+          <action>if false rpl.sh $QUIET $VERB 'TaskBarShowCPUStatus=0' 'TaskBarShowCPUStatus=1' $HOME/.icewm/preferences 2>$ERR</action>
           <default>$CHKBOX8</default>
         </checkbox>
    <hbox>
@@ -173,14 +190,44 @@ $NUMBERS
    </frame>
    <hbox>
         <button ok>
-         <action>DESKTOPS</action>
-         <action>MSG</action>
+
+         <action>/tmp/ice_config_ok.sh &</action>
          <action>EXIT:OK</action>
         </button>
    </hbox>
  </vbox>
 </window>"
-gtkdialog3 -p MAIN
+#<action>DESKTOPS</action>
+
+# various variables if not set globally in f4puppy5
+VERBOSE=1
+QUIET=
+VERB=-v
+DBG=-d
+NOWARN=-w
+
+# run GUI
+gtkdialog3 $DBG -p MAIN |
+while read aLINE;  # alternative to <action>
+do
+        c=$((c+1))
+        [ "$VERBOSE" ] && echo "$c aLINE='$aLINE'"
+        case "$aLINE" in
+         NUM=\"[0-9]\") NEWNUM=${aLINE//[[:alpha:][:punct:]]/}
+         [ "$VERBOSE" ] && echo "NEWNUM='$NEWNUM'"
+          for N in `seq 0 1 $NEWNUM`; do
+           wsNAME="$wsNAME \" $N \","
+          done
+          wsNAME="${wsNAME%,*}"
+         [ "$VERBOSE" ] && echo "wsNAME='$wsNAME'"
+         sed -i".sedBAK" "s%WorkspaceNames=.*%WorkspaceNames=$wsNAME%" $HOME/.icewm/preferences
+         [ $? = 0 ] && rm $HOME/.icewm/preferences.sedBAK || /bin/cp -a --backup=numbered  $HOME/.icewm/preferences.sedBAK $HOME/.icewm/preferences
+       ;;
+       *):
+       ;;
+       esac
+done
+
 unset MAIN
 
 # Very End of this file 'usr/sbin/ice_config.sh' #
