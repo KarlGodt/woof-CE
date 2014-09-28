f371b699c69128feafc0ed22e1c00bdb1cd58636
KarlGodt <karlgodt@excite.de>
2014-09-28 18:30:08 -0100

    woof-code/rootfs-skeleton/usr/local/petget/testurls.sh: Add test for HAVE_F4PUPPY5 variable.

--- woof-code/rootfs-skeleton/usr/local/petget/testurls.sh
+++ woof-code/rootfs-skeleton/usr/local/petget/testurls.sh
@@ -10,7 +10,7 @@ _COMMENT_="wget test for entries in petget_repos file"
 MY_SELF="$0"
 
 test -f /etc/rc.d/f4puppy5 && {
-source /etc/rc.d/f4puppy5
+[ "$HAVE_F4PUPPY5" ] || source /etc/rc.d/f4puppy5
 
 ADD_PARAMETER_LIST=""
 ADD_PARAMETERS=""
