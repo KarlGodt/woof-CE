822d8726a0da242d0d4a16e085d67ef67f9c4a9b
KarlGodt <karlgodt@excite.de>
2014-09-30 09:44:33 -0100

    add_header.sh: Finetuning of options. Add -O ONCE option, Add support for SYS and GIT options, Add support for -v option to mv and chmod, Fix Header syntax

--- add_header.sh
+++ add_header.sh
@@ -5,6 +5,8 @@ cat >&2 <<EoI
 $0
 -D) DRY=1 QUIET=-q
 -d) set -x
+-O) ONCE=1
+-G) GIT=1 SYS=''
 -S) GIT='' SYS=1
 -v) VERBOSE=1 oQUIET=1
 -h|*help) usage 0
@@ -12,17 +14,23 @@ EoI
 exit $1
 }
 
+VERBOSE= # give more messages about whats going on
+VERB=    # Option passed to mv and chmod
 QUIET=-q # -q option for grep
 GIT=1   # change bang in git repo
 SYS=   # change bang in current OS
 DRY=  #dry run - do not do actually change bang
+ONCE= #run only one time and show altered file in defaulttexteditor
+
 while [ "$1" ]; do
 case "$1" in
 -D) DRY=1;QUIET=-q;;
 -d) set -x;;
+-O) ONCE=1;;
+-G) GIT=1;SYS='';;
 -S) GIT='';SYS=1;;
--v) VERBOSE=1;oQUIET=1;;
--h|*help) usage 0;;
+-v) VERBOSE=1;VERB=-v;oQUIET=1;;
+-h|*help|*usage) usage 0;;
 *) [ "$PATTERN" ] && PATTERN="$PATTERN $1" || PATTERN="$1";; #echo "$0:Unrecognized option '$1'"; exit 1;;
 esac
 shift
@@ -39,24 +47,47 @@ for file in bin/* sbin/* usr/bin/* usr/sbin/* usr/local/*/*
 do
    [ -L "$file" ] && continue
    [ -f "$file" ] || continue
-   file "$file" | grep -i text | grep $QUIET -viE 'ISO-8859|murga\-lua\-dynamic|perl|python|pascal|bacon|ASCII C program|C++ program|HTML document|X pixmap image|PNG image data' || { [ "$VERBOSE" ] && echo "$file is not a shell script file by detection"; continue; }
+   file "$file" | grep -i text | grep $QUIET -viE 'ISO-8859|murgaLua_Dynamic|perl|python|pascal|bacon|ASCII C program|C++ program|HTML document|X pixmap image|PNG image data' || { [ "$VERBOSE" ] && echo "$file is not a shell script file by detection"; continue; }
   case "$file" in
    *.txt|*.TXT|*rc|*.xpm|*.svg|*.png|*.jpg|*.wav|*.au|*.bac|*.dat|*.pupdev|*.gs|*Help)
    [ "$VERBOSE" ] && echo "$file is not a shell script file by EXT"; continue;;
    esac
 
    gPATTERN='f4puppy5'
-   grep "$gPATTERN" "$file" && continue
+   if [ "$GIT" -a ! "$SYS" ]; then
+    grep "$gPATTERN" "$file" && continue
+   elif [ "$SYS" -a ! "$GIT" ]; then
+    grep "$gPATTERN" "/$file" && continue
+   else break
+   fi
 
-   PERM=`stat -c %a "$file"`
-   [ "${PERM//[0-6]/}" ] || continue
-   echo "$file"
+   if [ "$GIT" -a ! "$SYS" ]; then
+    PERM=`stat -c %a "$file"`
+    [ "${PERM//[0-6]/}" ] || continue
+   elif [ "$SYS" -a ! "$GIT" ]; then
+    PERM=`stat -c %a "/$file"`
+    [ "${PERM//[0-6]/}" ] || continue
+   else break
+   fi
 
+    echo "$file"
+
+  if [ "$GIT" -a ! "$SYS" ]; then
    SHELLBANG=`head -n1 "$file"`
    case "$SHELLBANG" in
    '#!'*) true;;
    *) echo "'$SHELLBANG' does not start with '!#'";continue;;
    esac
+  elif [ "$SYS" -a ! "$GIT" ]; then
+   SHELLBANG=`head -n1 "/$file"`
+   case "$SHELLBANG" in
+   '#!'*) true;;
+   *) echo "'$SHELLBANG' does not start with '!#'";continue;;
+   esac
+  else break
+  fi
+
+   rm /tmp/${file##*/}
 
    cat >/tmp/${file##*/} <<EoI
 ${SHELLBANG}
@@ -89,14 +120,42 @@ _trap
 #
 EoI
 
+ if [ "$GIT" -a ! "$SYS" ]; then
+  cat "$file" | sed '1d' >>/tmp/${file##*/}
+ elif [ "$SYS" -a ! "$GIT" ]; then
+  cat "/$file" | sed '1d' >>/tmp/${file##*/}
+ else break
+ fi
+
   if [ ! "$DRY" ]; then
-   cat "$file" | sed '1d' >>/tmp/${file##*/}
-   rm "$file"
-   mv /tmp/${file##*/} "$file"
-   chmod $PERM "$file"
+
+   if [ "$GIT" -a ! "$SYS" ]; then
+    rm "$file"
+    mv $VERB /tmp/${file##*/} "$file"
+    chmod $PERM "$file"
+   elif [ "$SYS" -a ! "$GIT" ]; then
+    rm "/$file"
+    mv $VERB /tmp/${file##*/} "/$file"
+    chmod $VERB $PERM "/$file"
+   else break
+   fi
+
   fi
 
-  #defaulttexteditor "$file"
-  #break
+_run_once(){
+ if [ "$GIT" -a ! "$SYS" ]; then
+  defaulttexteditor "$file"
+  defaulttexteditor /tmp/${file##*/}
+  break
+ elif [ "$SYS" -a ! "$GIT" ]; then
+  defaulttexteditor "/$file"
+  defaulttexteditor /tmp/${file##*/}
+  break
+ else break
+ fi
+}
+[ "$ONCE" ] && _run_once
+
+sleep 1
 
 done
