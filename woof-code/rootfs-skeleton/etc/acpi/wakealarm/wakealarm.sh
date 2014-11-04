1ee9c1a4e53c753776667a1adbdcf2c46eff29bb
KarlGodt <karlgodt@excite.de>
2014-11-04 18:17:54 +0000

    /etc/acpi/wakealarm/wakealarm.sh: Changed fixed filename to use variable.

--- woof-code/rootfs-skeleton/etc/acpi/wakealarm/wakealarm.sh
+++ woof-code/rootfs-skeleton/etc/acpi/wakealarm/wakealarm.sh
@@ -1,10 +1,11 @@
 #!/bin/ash
 
+FILE=/root/TUBE/FlashC0mPIo.flv  #Jonny Cash Hurt
 
-if test -f /root/TUBE/FlashC0mPIo.flv; then
+if test -f "$FILE"; then
  amixer set Master,0 100
  amixer set Mono,0 100
  amixer set PCM,0 100
 
- /usr/bin/mplayer -loop 0 /root/TUBE/FlashC0mPIo.flv
+ /usr/bin/mplayer -loop 0 "$FILE"
 fi
