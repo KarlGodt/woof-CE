94f8645e2020b7ba2dc0feba738e7168160da58b
KarlGodt <karlgodt@excite.de>
2014-11-18 22:41:33 -0100

    woof-code/rootfs-skeleton/usr/local/net_setup/usr/sbin/ndiswrapperGUI.sh: Added some # REM: Xdialog lines to be able to grep Xdialog examples in woof source files.

--- woof-code/rootfs-skeleton/usr/local/net_setup/usr/sbin/ndiswrapperGUI.sh
+++ woof-code/rootfs-skeleton/usr/local/net_setup/usr/sbin/ndiswrapperGUI.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#(c) copyright Raul Suarez 2006 
+#(c) copyright Raul Suarez 2006
 #Puppy ndiswrapper GUI setup script.
 #v4.00 BK 29apr2008: bugfix.
 
@@ -9,28 +9,30 @@
 
 selectDriverFile()
 {
-	CONTINUE="false"
-	while [ $CONTINUE == "false" ] ; do
-	  INF_FILE_NAME="`Xdialog --left --title \"Select the driver information file (.INF)\" \
-	   --stdout --no-buttons --fselect \"${PREV_LOCATION}/*\" 0 0`" 
-	  if [ $? -eq 0 ] ; then
-	    PREV_LOCATION="`expr "$INF_FILE_NAME" : '\(.*\)/'`"
+    CONTINUE="false"
+    while [ $CONTINUE == "false" ] ; do
+        # REM: Xdialog fselect
+      INF_FILE_NAME="`Xdialog --left --title \"Select the driver information file (.INF)\" \
+       --stdout --no-buttons --fselect \"${PREV_LOCATION}/*\" 0 0`"
+      if [ $? -eq 0 ] ; then
+        PREV_LOCATION="`expr "$INF_FILE_NAME" : '\(.*\)/'`"
         echo $PREV_LOCATION > "$CONFIG_DIR/prev_location"
-	    echo $INF_FILE_NAME | grep -Eiq "\.inf$" 
-	    if [ $? -eq 0 ] ; then
-				selectDriverFile_RC=0
-				CONTINUE="true"
-	    else
-	      Xdialog --screen-center --title "Puppy Ethernet Wizard: ndiswrapper" \
-	              --msgbox "The file name should end in \".inf\"" 0 0
-	    fi
-	  else
-	    selectDriverFile_RC=1
-	    CONTINUE="true"
-	  fi
-	done
-	
-	return $selectDriverFile_RC
+        echo $INF_FILE_NAME | grep -Eiq "\.inf$"
+        if [ $? -eq 0 ] ; then
+                selectDriverFile_RC=0
+                CONTINUE="true"
+        else
+        # REM: Xdialog msgbox
+          Xdialog --screen-center --title "Puppy Ethernet Wizard: ndiswrapper" \
+                  --msgbox "The file name should end in \".inf\"" 0 0
+        fi
+      else
+        selectDriverFile_RC=1
+        CONTINUE="true"
+      fi
+    done
+
+    return $selectDriverFile_RC
 } # end of selectDriverFile
 
 #=============================================================================
@@ -47,27 +49,29 @@ showNdiswrapperGUI()
   showNdiswrapperGUI_RC=1
   selectDriverFile
   if [ $? -eq 0 ] ; then
-  
+
     echo "INF_FILE_NAME=$INF_FILE_NAME" #TEST
-    
+
     #v4.00 fails if space in path, need quotes...
     expr "$INF_FILE_NAME" : '.*/\(.*\).inf' | tr "[A-Z]" "[a-z]"
-    
+
     echo "INF_FILE_NAME=$INF_FILE_NAME" #TEST
-    
+
     #v4.00 fails if space in path, need quotes...
     ndiswrapper -i "$INF_FILE_NAME" > /tmp/net-setup_NDISWRAPPER_LOAD.txt
     NDISWRAPPER_RESULT=$?
-		case ${NDISWRAPPER_RESULT} in
-			0 | 25 | 255) 
-					Xdialog --left --screen-center --title "Puppy Ethernet Wizard: ndiswrapper" \
+        case ${NDISWRAPPER_RESULT} in
+            0 | 25 | 255)
+                # REM: Xdialog msgbox
+                    Xdialog --left --screen-center --title "Puppy Ethernet Wizard: ndiswrapper" \
               --msgbox "`ndiswrapper -l`" 0 0
-		      showNdiswrapperGUI_RC=0
+              showNdiswrapperGUI_RC=0
           ;;
-			*) 	Xdialog --left --screen-center --title "Puppy Ethernet Wizard: ndiswrapper" \
+            *) # REM: Xdialog msgbox
+             Xdialog --left --screen-center --title "Puppy Ethernet Wizard: ndiswrapper" \
               --msgbox "`cat /tmp/net-setup_NDISWRAPPER_LOAD.txt`" 0 0
           ;;
-		esac
+        esac
   fi
   return $showNdiswrapperGUI_RC
 } # end of showNdiswrapperGUI
@@ -80,8 +84,8 @@ showNdiswrapperGUI()
 # If ran by itself it shows the interface, Otherwise it's only used as a function library
 CURRENT_CONTEXT=`expr "$0" : '.*/\(.*\)$' `
 if [ "${CURRENT_CONTEXT}" == "ndiswrapperGUI.sh" ] ; then
-	showNdiswrapperGUI
-fi 
+    showNdiswrapperGUI
+fi
 
 #=============================================================================
 #=============== END OF SCRIPT BODY ====================
