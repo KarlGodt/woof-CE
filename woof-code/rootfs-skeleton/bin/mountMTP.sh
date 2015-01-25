17676bf7fa45ccfed832cdfb97ad6d9d4508e74e
KarlGodt <karlgodt@excite.de>
2015-01-25 14:05:41 -0100

    /bin/mountMTP.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/bin/mountMTP.sh
+++ woof-code/rootfs-skeleton/bin/mountMTP.sh
@@ -1,6 +1,9 @@
 #!/bin/ash
 . /etc/rc.d/f4puppy5
 
+# REM: Prevent multiple instances
+_pidof $Q go-mtpfs && _exit 3 "go-mtpfs already running"
+
 # REM: Use other dir than /mnt in case mount troubles,
 #      fuse mounts in /mnt will render whole /mnt directory
 #      inaccessible
@@ -24,12 +27,17 @@ test -e "$OLD_TARG" || _exit 55 "'$OLD_TARG' does not exist"
 # make a backup of mount
 test -e "$OLD_TARG".BAK || cp -a "$OLD_TARG" "$OLD_TARG".BAK
 # REM: Create mountMTP wrapper if not exists
-test -f /bin/mountMTP || echo 'exit 0' >/bin/mountMTP
-ln $VERB -sf mountMTP /bin/mount
-test $? = 0 || _exit 6 "Failed to link /bin/mount -> mountMTP"
+test -f /bin/mountMTP || {
+    echo 'exit 0' >/bin/mountMTP || _exit 56 "Could not create /bin/mountMTP wrapper" ;
+    chmod $VERB +x /bin/mountMTP || _exit 57 "Could not make /bin/mountMTP executable";
+    }
 
 # REM: If called from external program, needs returnvalues
+#      Needs to be placed here, to prevent link creation
 test "`which go-mtpfs`" || _exit 7 "go-mtpfs not installed in PATH"
+
+ln $VERB -sf mountMTP /bin/mount
+test $? = 0 || _exit 6 "Failed to link /bin/mount -> mountMTP"
 
 # REM: Now start go-mtpfs
 #     Needs forking to able to re-link original mount wrapper
