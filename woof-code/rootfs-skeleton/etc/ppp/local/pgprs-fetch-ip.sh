c4e82d842c1af6d3c6a53148a4bc6e5c44a3a381
KarlGodt <karlgodt@excite.de>
2015-03-12 15:05:11 -0100

    /etc/ppp/local/pgprs-fetch-ip.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/etc/ppp/local/pgprs-fetch-ip.sh
+++ woof-code/rootfs-skeleton/etc/ppp/local/pgprs-fetch-ip.sh
@@ -8,9 +8,10 @@ ipDETECT_ADDRESS=icanhazip.com
 sleep 5s
 
 while test "`pidof pppd`"; do
-read carRIER </sys/class/net/$ACTIVE_INTERFACE/carrier 2>/dev/null
-test "$carRIER" = 1 && break
 sleep 1s
+[ -e /sys/class/net/$ACTIVE_INTERFACE/carrier ] && read carRIER </sys/class/net/$ACTIVE_INTERFACE/carrier 2>/dev/null
+test "$carRIER" = 1 && break
+#sleep 1s
 done
 
 rm -f "$tmpDIR"/wget.log
@@ -19,11 +20,11 @@ rm -f "$tmpDIR"/wget.out
 mkdir -p "$tmpDIR"
 
 date >> "$logFILE"
-grep -i ip /var/log/messages >> "$logFILE"
+[ -s /var/log/messages ] && grep -i ip /var/log/messages >> "$logFILE"
 
 wget -o "$tmpDIR"/wget.log -O "$tmpDIR"/wget.out $ipDETECT_ADDRESS
-read IP <"$tmpDIR"/wget.out
-echo "$IP" >> "$logFILE"
+[ -s "$tmpDIR"/wget.log ] && read IP <"$tmpDIR"/wget.out
+[ "$IP" ] && echo "$IP" >> "$logFILE" || echo "Could not get IP" >> "$logFILE"
 
 echo >> "$logFILE"
 
