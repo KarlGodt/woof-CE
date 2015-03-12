766af3b2cc8a052b1b3cc4ae3e7fb65ad0448d17
KarlGodt <karlgodt@excite.de>
2015-03-12 15:05:41 -0100

    /usr/sbin/usb_modeswitch.sh : Replaced by the one found currently in the system.

--- woof-code/rootfs-skeleton/usr/sbin/usb_modeswitch.sh
+++ woof-code/rootfs-skeleton/usr/sbin/usb_modeswitch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/ash
 #
 # New header by Karl Reimer Godt, September 2014
   _TITLE_="Puppy_usb_modeswitch.sh"
@@ -15,7 +15,7 @@ ADD_PARAMETER_LIST=""
 ADD_PARAMETERS=""
 _provide_basic_parameters
 
-TWO_HELP=''; TWO_VERSION=''; TWO_VERBOSE=''; TWO_DEBUG=''; ## Set to anything if code requires further down (ie. custom usage or version message)
+TWO_HELP='1'; TWO_VERSION='1'; TWO_VERBOSE='1'; TWO_DEBUG='1'; ## Set to anything if code requires further down (ie. custom usage or version message)
 ADD_HELP_MSG="$_COMMENT_"
 _parse_basic_parameters "$@"
 [ "$DO_SHIFT" ] && [ ! "${DO_SHIFT//[[:digit:]]/}" ] && {
@@ -26,20 +26,55 @@ _trap
 }
 # End new header
 #
+
+# ZTE MF628+ (tested version from Telia / Sweden)
+# ZTE MF626
+# ZTE MF636 (aka "Telstra / BigPond 7.2 Mobile Card")
+
+#SUBSYSTEM=="usb", ATTRS{idProduct}=="2000", ATTRS{idVendor}=="19d2", ACTION=="add", RUN+="/usr/sbin/usb_modeswitch.sh -v 0x%s{idVendor} -p 0x%s{idProduct} -V 0x19d2 -P 0x0031 -M '55534243123456782000000080000c85010101180101010101000000000000'"
+
+#/usr/sbin/usb_modeswitch.sh -v 0x19d2 -p 0x2000 -V 0x19d2 -P 0x0031 -M '55534243123456782000000080000c85010101180101010101000000000000'
+
 ARGUMENTS="$@"
+echo "$ARGUMENTS"
+
+tmpDIR=/tmp/usb_modeswitch
+mkdir -p "$tmpDIR"
+logFILE="$tmpDIR"/usb_modeswitch-"`date +%F-%T`".log
+modeswitchCONF=/etc/usb_modeswitch.conf
+[ -f "$modeswitchCONF" ] || modeswitchCONF='""'
+modeswitchBIN=/usr/sbin/usb_modeswitch
+modprobeBIN=/sbin/modprobe
+modprobeCONF=/etc/modprobe.conf
+DRIVER=option
+grep ^$DRIVER /proc/modules || modprobe $VERB $DRIVER
+if test -f $modprobeCONF; then
+USBSTORAGEDELAY="`grep 'options usb\-storage ' $modprobeCONF | grep -w -o 'delay_use=[0-9]*' | cut -f 2 -d '='`"
+fi
+[ "$USBSTORAGEDELAY" = "" ] && USBSTORAGEDELAY="5" #default value
+
 if [ "$ARGUMENTS" = "" -o "$ARGUMENTS" = "-W" ];then #config-file method of invocation
- usb_modeswitch -W -c /etc/usb_modeswitch.conf &> /tmp/usb_modeswitch.log #log both output and errors
+ echo "configfile invocation"
+ $modeswitchBIN -W -c $modeswitchCONF &> $logFILE #log both output and errors
+ retval=$?
  sync
- cat /tmp/usb_modeswitch.log #show output to user
+ cat $logFILE #show output to user
 
 else #udev-rule invocation
- USBSTORAGEDELAY="`grep 'options usb-storage ' /etc/modprobe.conf | grep -w --only-matching 'delay_use=[0-9]*' | cut -f 2 -d '='`"
- [ "$USBSTORAGEDELAY" = "" ] && USBSTORAGEDELAY="5" #default value
- if [ "`uname -r`" != "2.6.25.16" ] || [ "`echo "$ARGUMENTS" | grep ' -H'`" = "" ];then
-  sh -c "sleep $USBSTORAGEDELAY; sleep 5; /usr/sbin/usb_modeswitch $ARGUMENTS -s 9 -W >> /tmp/usb_modeswitch.log 2>&1" &
+ echo "udev invocation"
+ #USBSTORAGEDELAY="`grep 'options usb-storage ' /etc/modprobe.conf | grep -w --only-matching 'delay_use=[0-9]*' | cut -f 2 -d '='`"
+ #[ "$USBSTORAGEDELAY" = "" ] && USBSTORAGEDELAY="5" #default value
+ if [ "`uname -r`" != "2.6.25.16" ] || [ "`echo "$ARGUMENTS" | grep ' \-H'`" = "" ];then
+  echo "default invocation"
+  sh -c "sleep $USBSTORAGEDELAY; sleep 5; $modeswitchBIN $ARGUMENTS -s 9 -W >> $logFILE 2>&1" &
  else
+  echo "kernel 2.6.25.16 invocation , or not -H option"
   #Re-modprobe option driver for Huawei modems in kernel 2.6.25.16, after allowing time for it to switch.
-  sh -c "sleep $USBSTORAGEDELAY; sleep 5; /usr/sbin/usb_modeswitch $ARGUMENTS -s 10 -W >> /tmp/usb_modeswitch.log 2>&1; /sbin/modprobe option" >> /tmp/usb_modeswitch.log 2>&1 &
+  sh -c "sleep $USBSTORAGEDELAY; sleep 5; $modeswitchBIN $ARGUMENTS -s 10 -W >> $logFILE 2>&1; $modprobeBIN $VERB $DRIVER" >> $logFILE 2>&1 &
  fi
+ retVAL=$?
+ test "$retVAL" = 0 || cat $logFILE
 fi
-exit 0
+
+[ "$retVAL" ] || retVAL=0
+exit $retVAL
