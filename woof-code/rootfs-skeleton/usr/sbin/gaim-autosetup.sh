944324e7077f5066579f954e5141e72062d14cc6
KarlGodt <karlgodt@excite.de>
2015-04-22 15:50:12 -0100

    /usr/sbin/gaim-autosetup.sh: Added BATCHMARKER01 and sourcing of f4puppy5 .

--- woof-code/rootfs-skeleton/usr/sbin/gaim-autosetup.sh
+++ woof-code/rootfs-skeleton/usr/sbin/gaim-autosetup.sh
@@ -16,6 +16,8 @@
 #                           
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
+. /etc/rc.d/f4puppy5
+# BATCHMARKER01 - Marker for Line-Position to bulk insert code into.
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 # bugfix bk 2006
