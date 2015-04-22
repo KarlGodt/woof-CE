2e1574caf01f523caaedd54b391c466dd294d2b0
KarlGodt <karlgodt@excite.de>
2015-04-22 15:49:56 -0100

    /usr/sbin/e2ore3.sh: Added BATCHMARKER01 and sourcing of f4puppy5 .

--- woof-code/rootfs-skeleton/usr/sbin/e2ore3.sh
+++ woof-code/rootfs-skeleton/usr/sbin/e2ore3.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #Lesser GPL licence v2 (http://www.fsf.org/licensing/licenses/lgpl.html)
 #used by probepart.
+. /etc/rc.d/f4puppy5
+# BATCHMARKER01 - Marker for Line-Position to bulk insert code into.
 #read device, example '/dev/hda4' from stdin.
 
 read MYDEVICE
