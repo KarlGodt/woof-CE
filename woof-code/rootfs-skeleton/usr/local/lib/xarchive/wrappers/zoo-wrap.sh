abe64b49ded4f79b31b2fe279a53026dfa11f53f
KarlGodt <karlgodt@excite.de>
2015-05-05 18:39:11 -0100

    /usr/local/lib/xarchive/wrappers/zoo-wrap.sh: Whitespace cleanup and debugging output.

--- woof-code/rootfs-skeleton/usr/local/lib/xarchive/wrappers/zoo-wrap.sh
+++ woof-code/rootfs-skeleton/usr/local/lib/xarchive/wrappers/zoo-wrap.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Based on the improved zip-wrap.sh by 2010 Alexander .S.T. Ross, which is based on ace-wrap.sh by 2005 Lee Bigelow <ligelowbee@yahoo.com> 
+# Based on the improved zip-wrap.sh by 2010 Alexander .S.T. Ross, which is based on ace-wrap.sh by 2005 Lee Bigelow <ligelowbee@yahoo.com>
 
 #--------------------------------------------------------------------------------
 # Copyright (C) 2010 Alexander .S.T. Ross
@@ -22,14 +22,14 @@
 # zoo-wrap.sh - bash zoo wrapper for xarchive.
 
 #ChangeLog ----------------------------------------------------------------------
-#	2010-09-23  Alexander .S.T. Ross (abushcrafter) Email: <http://www.google.com/recaptcha/mailhide/d?k=01uNeUuXxeNm9FA3Zciuoqzw==&c=nVfKeb7kjqZVVIQanqJwEC2DP5zrALkSERTopYvj_pU=>
-#		* 0.0.1: 
+#   2010-09-23  Alexander .S.T. Ross (abushcrafter) Email: <http://www.google.com/recaptcha/mailhide/d?k=01uNeUuXxeNm9FA3Zciuoqzw==&c=nVfKeb7kjqZVVIQanqJwEC2DP5zrALkSERTopYvj_pU=>
+#       * 0.0.1:
 echo >>/tmp/xarchive_errs.log
-
+ echo "$0:$*" >>/tmp/xarchive_errs.log
 
 E_UNSUPPORTED=65
 
-# Supported file extentions for ace 
+# Supported file extentions for ace
 EXTS="zoo"
 
 # Program to wrap
@@ -82,34 +82,34 @@ case "$opt" in
                 printf "%s;" $ext
             done
         else
-            echo command $ACE_PROG not found >>/tmp/xarchive_errs.log 
-            echo extentions $EXTS ignored >>/tmp/xarchive_errs.log 
+            echo command $ACE_PROG not found >>/tmp/xarchive_errs.log
+            echo extentions $EXTS ignored >>/tmp/xarchive_errs.log
         fi
         printf "\n"
         exit
         ;;
 
-    -o) # open: mangle output of ace cmd for xarchive 
+    -o) # open: mangle output of ace cmd for xarchive
         # format of ace output:
         # Date    ³Time ³Packed     ³Size     ³Ratio³File
-        # 17.09.02³00:32³     394116³   414817³  95%³ OggDS0993.exe 
-	# 1                   2         3        4    5
-	$ACE_PROG $OPEN_OPTS "$archive" | $AWK_PROG -v uuid=${UID} '
+        # 17.09.02³00:32³     394116³   414817³  95%³ OggDS0993.exe
+    # 1                   2         3        4    5
+    $ACE_PROG $OPEN_OPTS "$archive" | $AWK_PROG -v uuid=${UID} '
         #only process lines starting with two numbers and a dot
         /^[0-9][0-9]\./ {
           date=substr($1,1,8)
           time=substr($1,10,5)
           #need to strip the funky little 3 off the end of size
           size=substr($3,1,(length($3)-1))
-          
+
           #split line at ratio and a space, second item is our name
           split($0, linesplit, ($4 " "))
           name=linesplit[2]
-          
+
           uid=uuid; gid=uuid; link="-"; attr="-"
           printf "%s;%s;%s;%s;%s;%s;%s;%s\n",name,size,attr,uid,gid,date,time,link
         }'
-	exit
+    exit
         ;;
 
     -a) # add:  to archive passed files
@@ -121,43 +121,43 @@ case "$opt" in
         #     $ACE_PROG $ADD_OPTS "$archive" "$(basename "$1")"
         #     shift 1
         # done
-        exit $E_UNSUPPORTED 
+        exit $E_UNSUPPORTED
         ;;
 
-    -n) # new: create new archive with passed files 
+    -n) # new: create new archive with passed files
         # create will only be passed the first file, the
         # rest will be "added" to the new archive
         # ONY HAVE UNACE, no creating...
         # cd "$(dirname "$1")"
         # $ACE_PROG $NEW_OPTS "$archive" "$(basename "$1")"
-        exit $E_UNSUPPORTED 
+        exit $E_UNSUPPORTED
         ;;
 
-    -r) # remove: from archive passed files 
+    -r) # remove: from archive passed files
         # ONY HAVE UNACE, no removing...
         # $ACE_PROG $REMOVE_OPTS "$archive" "$@"
-        exit $E_UNSUPPORTED 
+        exit $E_UNSUPPORTED
         ;;
 
-    -e) # extract: from archive passed files 
+    -e) # extract: from archive passed files
         # xarchive will put is the right extract dir
         # so we just have to extract.
         $XTERM_PROG -e $ACE_PROG $EXTRACT_OPTS "$archive"
         exit
         ;;
 
-    -v) # view: from archive passed files 
+    -v) # view: from archive passed files
         exit $E_UNSUPPORTED
         ;;
 
      *) echo "error, option $opt not supported"
-        echo "use one of these:" 
-        echo "-i                #info" 
-        echo "-o archive        #open" 
-        echo "-a archive files  #add" 
-        echo "-n archive file   #new" 
-        echo "-r archive files  #remove" 
-        echo "-e archive files  #extract" 
-        echo "-v archive file   #view"  
+        echo "use one of these:"
+        echo "-i                #info"
+        echo "-o archive        #open"
+        echo "-a archive files  #add"
+        echo "-n archive file   #new"
+        echo "-r archive files  #remove"
+        echo "-e archive files  #extract"
+        echo "-v archive file   #view"
         exit
 esac
