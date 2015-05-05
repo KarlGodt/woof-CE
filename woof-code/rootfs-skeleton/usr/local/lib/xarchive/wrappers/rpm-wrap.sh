32b057096290f401ec5885e5d88e45b6abe320e2
KarlGodt <karlgodt@excite.de>
2015-05-05 18:44:04 -0100

    /usr/local/lib/xarchive/wrappers/rpm-wrap.sh: Debug output, space cleanup.

--- woof-code/rootfs-skeleton/usr/local/lib/xarchive/wrappers/rpm-wrap.sh
+++ woof-code/rootfs-skeleton/usr/local/lib/xarchive/wrappers/rpm-wrap.sh
@@ -1,26 +1,26 @@
 #! /bin/bash
 # rpm-wrap.sh - bash rpm wrapper for xarchive frontend
-# Copyright (C) 2005 Lee Bigelow <ligelowbee@yahoo.com> 
-# 
+# Copyright (C) 2005 Lee Bigelow <ligelowbee@yahoo.com>
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 echo >>/tmp/xarchive_errs.log
-
+echo "$0:$*" >>/tmp/xarchive_errs.log
 # set up exit status variables
 E_UNSUPPORTED=65
 
-# Supported file extentions for tar 
+# Supported file extentions for tar
 EXTS="rpm"
 
 # Setup awk program
@@ -65,16 +65,16 @@ case "$opt" in
                 printf "%s;" $ext
             done
         else
-            echo program $RPM_PROG not found >>/tmp/xarchive_errs.log 
+            echo program $RPM_PROG not found >>/tmp/xarchive_errs.log
             echo extentions $EXTS ignored >>/tmp/xarchive_errs.log
         fi
         printf "\n"
         exit
         ;;
 
-    -o) # open: mangle output of rpm cmd for xarchive 
+    -o) # open: mangle output of rpm cmd for xarchive
         # format of output:
-        # usr grp attr size time(epoch) name 
+        # usr grp attr size time(epoch) name
         # 1   2   3    4    5           6
         $RPM_PROG -q --qf '[%{FILEUSERNAME} %{FILEGROUPNAME} %{FILEMODES:perms} %{FILESIZES} %{FILEMTIMES} %{FILENAMES}\n]' -p "$archive" | $AWK_PROG '
         {
@@ -88,10 +88,10 @@ case "$opt" in
           date_cmd="date -d \"1970-01-01 UTC " $5 " seconds\" +\"%Y-%m-%d\""
           date_cmd | getline date
           close(date_cmd)
-          
+
           split($0, linesplit, $5 " ")
-          name=linesplit[2] 
-          link="-"          
+          name=linesplit[2]
+          link="-"
           printf "%s;%s;%s;%s;%s;%s;%s;%s\n",name,size,attr,uid,gid,date,time,link
         }'
         exit
@@ -102,17 +102,17 @@ case "$opt" in
         exit $E_UNSUPPORTED
         ;;
 
-    -n) # create new archive unsupported 
+    -n) # create new archive unsupported
         # use appropriate rpm tools to build rpms
         exit $E_UNSUPPORTED
         ;;
 
-    -r) # removing from archive unsupported 
+    -r) # removing from archive unsupported
         # use appropriate rpm tools to modify rpms
         exit $E_UNSUPPORTED
         ;;
 
-    -e) # extract: from archive passed files 
+    -e) # extract: from archive passed files
         # convert rpm to a temporary cpio file
         tmpcpio="$(mktemp -t cpiotmp.XXXXXX)"
         $RPM2CPIO_PROG "$archive" > "$tmpcpio"
@@ -128,12 +128,12 @@ case "$opt" in
         ;;
 
      *) echo "error, option $opt not supported"
-        echo "use one of these:" 
-        echo "-i                #info" 
-        echo "-o archive        #open" 
-        echo "-a archive files  #add" 
-        echo "-n archive file   #new" 
-        echo "-r archive files  #remove" 
-        echo "-e archive files  #extract" 
+        echo "use one of these:"
+        echo "-i                #info"
+        echo "-o archive        #open"
+        echo "-a archive files  #add"
+        echo "-n archive file   #new"
+        echo "-r archive files  #remove"
+        echo "-e archive files  #extract"
         exit
 esac
