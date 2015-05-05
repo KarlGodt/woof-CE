e7e84352d91ef957befb57eaeeff536bd226eedc
KarlGodt <karlgodt@excite.de>
2015-05-05 18:36:12 -0100

    /usr/local/lib/xarchive/wrappers/tar-wrap.sh: Reordered debugging output.

--- woof-code/rootfs-skeleton/usr/local/lib/xarchive/wrappers/tar-wrap.sh
+++ woof-code/rootfs-skeleton/usr/local/lib/xarchive/wrappers/tar-wrap.sh
@@ -15,9 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-echo >>/tmp/xarchive_errs.log
-echo $0 >>/tmp/xarchive_errs.log
-echo "$@" >>/tmp/xarchive_errs.log
+#echo >>/tmp/xarchive_errs.log
+#echo $0 >>/tmp/xarchive_errs.log
+#echo "$@" >>/tmp/xarchive_errs.log
+echo "$0:$*" >>/tmp/xarchive_errs.log
 # set up exit status variables
 E_UNSUPPORTED=65
 
