18e949c8fdfeb750eecdb9122c87053c822d912d
KarlGodt <karlgodt@excite.de>
2015-05-13 21:54:50 -0100

    /usr/sbin/pngoverlay.sh: Batch adding of VERB , Q , ERR , OUT variables. Shorten tests ( omitt !=  ).

--- woof-code/rootfs-skeleton/usr/sbin/pngoverlay.sh
+++ woof-code/rootfs-skeleton/usr/sbin/pngoverlay.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/ash
 # Barry Kauler 2011 GPL3 (/usr/share/doc/legal)
 #pngoverlay.sh is an alternative to pngoverlay written by vovchik (in BaCon)
 # (vovchik's pngoverlay requires X to be running, which may be a disadvantage)
@@ -12,13 +12,13 @@
 [ ! $3 ] && exit 1
 [ ! -e "$1" ] && exit 1
 [ ! -e "$2" ] && exit 1
-[ "`echo -n "$1" | grep 'png$'`" = "" ] && exit 1
-[ "`echo -n "$2" | grep 'png$'`" = "" ] && exit 1
+[ "`echo "$1" | grep 'png$'`" ] || exit 1
+[ "`echo "$2" | grep 'png$'`" ] || exit 1
 
-pngtopam -alphapam "${1}" > /tmp/pngoverlay_${$}_1.pam
-pngtopam -alphapam "${2}" > /tmp/pngoverlay_${$}_2.pam
+pngtopam -alphapam "${1}" >/tmp/pngoverlay_${$}_1.pam
+pngtopam -alphapam "${2}" >/tmp/pngoverlay_${$}_2.pam
 #1st image on top, 2nd on bottom, 3rd is output...
-pamcomp -mixtransparency /tmp/pngoverlay_${$}_2.pam /tmp/pngoverlay_${$}_1.pam > /tmp/pngoverlay_${$}_out.png 2> /dev/null
+pamcomp -mixtransparency /tmp/pngoverlay_${$}_2.pam /tmp/pngoverlay_${$}_1.pam >/tmp/pngoverlay_${$}_out.png 2>$ERR
 pamrgbatopng /tmp/pngoverlay_${$}_out.png > "${3}"
 rm -f /tmp/pngoverlay_${$}_1.pam
 rm -f /tmp/pngoverlay_${$}_2.pam
