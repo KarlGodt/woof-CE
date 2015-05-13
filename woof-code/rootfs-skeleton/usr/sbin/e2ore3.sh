888b8aedee87dd6c997ba1a158de237f30557b12
KarlGodt <karlgodt@excite.de>
2015-05-13 21:53:30 -0100

    /usr/sbin/e2ore3.sh: Batch adding of VERB , Q , ERR , OUT variables. Shorten tests ( omitt !=  ).

--- woof-code/rootfs-skeleton/usr/sbin/e2ore3.sh
+++ woof-code/rootfs-skeleton/usr/sbin/e2ore3.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/ash
 #Lesser GPL licence v2 (http://www.fsf.org/licensing/licenses/lgpl.html)
 #used by probepart.
 . /etc/rc.d/f4puppy5
@@ -6,8 +6,8 @@
 #read device, example '/dev/hda4' from stdin.
 
 read MYDEVICE
-[ "$MYDEVICE" = "" ] && exit
+[ "$MYDEVICE" ] || exit
 MYSTR="`tune2fs -l $MYDEVICE | grep 'Filesystem features:' | grep 'has_journal'`"
 
-[ "$MYSTR" = "" ] && exit 0 #ext2
+[ "$MYSTR" ] || exit 0 #ext2
 exit 1 #ext3
