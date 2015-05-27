b0343ce680c2fe0a766bae857c50862555ee701e
KarlGodt <karlgodt@excite.de>
2015-05-27 12:45:49 -0100

    git_helper/add_VERB2.sh: Changes.

--- git_helper/add_VERB2.sh
+++ git_helper/add_VERB2.sh
@@ -6,3 +6,5 @@
 for f in *; do test -L "$f" -o -d "$f" && continue;echo "$f"; sed -i 's% != "" ] &&  != "" ] %  %g' "$f" || break;done
 
 for f in *; do test -L "$f" -o -d "$f" && continue;echo "$f"; sed -i 's%   &&  % \&\& %g' "$f" || break;done
+
+for f in *; do test -L "$f" -o -d "$f" && continue;echo "$f"; sed -i 's% 2>\$ERR`% 2>/dev/null`%g' "$f" || break;done
