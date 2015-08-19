7279cc66396fdbfef3f65cd636f69cd6110e41c2
KarlGodt <karlgodt@excite.de>
2015-08-19 17:36:14 -0100

    crossfire/s/cf_cancel_me.sh : Check if ROD is applied already.

--- woof-code/rootfs-skeleton/root/.crossfire/s/cf_cancel_me.sh
+++ woof-code/rootfs-skeleton/root/.crossfire/s/cf_cancel_me.sh
@@ -44,7 +44,87 @@ echo draw 3 "Need <number> ie: script $0 50 ."
 # *** Actual script to cancel multiple times *** #
 test $NUMBER -ge 1 || NUMBER=1 #paranoid precaution
 
-echo "issue 1 1 apply $ROD"
+__say_apply_params(){
+cat >&1 <<EoI
+ void command_apply(object *op, const char *params) {
+    int aflag = 0;
+    object *inv = op->inv;
+    object *item;
+
+    if (*params == '\0') {
+        apply_by_living_below(op);
+        return;
+    }
+
+    while (*params == ' ')
+        params++;
+    if (!strncmp(params, "-a ", 3)) {
+        aflag = AP_APPLY;
+        params += 3;
+    }
+    if (!strncmp(params, "-u ", 3)) {
+        aflag = AP_UNAPPLY;
+        params += 3;
+    }
+    if (!strncmp(params, "-b ", 3)) {
+        params += 3;
+        if (op->container)
+            inv = op->container->inv;
+        else {
+            inv = op;
+            while (inv->above)
+                inv = inv->above;
+        }
+    }
+    while (*params == ' ')
+        params++;
+
+    item = find_best_apply_object_match(inv, op, params, aflag);
+    if (item == NULL)
+        item = find_best_apply_object_match(inv, op, params, AP_NULL);
+    if (item) {
+        apply_by_living(op, item, aflag, 0);
+    } else
+        draw_ext_info_format(NDI_UNIQUE, 0, op, MSG_TYPE_COMMAND, MSG_TYPE_COMMAND_ERROR,
+                             "Could not find any match to the %s.",
+                             params);
+}
+EoI
+
+}
+
+__check_if_item_is_active(){
+
+local ITEM="$*" oR tmpR
+test "$ITEM" || { echo "draw 3  $ITEM missing"; exit 1; }
+
+echo watch drawinfo
+sleep 0.1
+echo request items actv
+while :; do
+read -t 1 tmpR
+case $tmpR in
+*"$ITEM"*) HAVE_ITEM_APPLIED=YES; break;;
+'') break;;
+esac
+test "$oR" = "$tmpR" && break
+oR=$tmpR
+sleep 0.1
+done
+echo unwatch drawinfo
+
+}
+
+__check_if_item_is_active "$ROD"
+
+test "$HAVE_ITEM_APPLIED" || echo "issue 1 1 apply -a $ROD"
+
+_simple_apply_item(){
+echo "issue 1 1 apply -u $ROD"
+sleep 2
+echo "issue 1 1 apply -a $ROD"
+}
+
 
 for one in `seq 1 1 $NUMBER`
 do
