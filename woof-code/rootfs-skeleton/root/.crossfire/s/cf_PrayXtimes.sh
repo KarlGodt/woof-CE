811ca8e12947135188d20ae8ff38b950e01a6cd4
KarlGodt <karlgodt@excite.de>
2015-09-29 19:05:59 -0100

    /root/.crossfire/s/cf_PrayXtimes.sh: Get rid of possible stray messages so to read REPLY for requests does not catch unusable drawinfo messages that could confuse the code processing from the REPLY string.

--- woof-code/rootfs-skeleton/root/.crossfire/s/cf_PrayXtimes.sh
+++ woof-code/rootfs-skeleton/root/.crossfire/s/cf_PrayXtimes.sh
@@ -106,6 +106,8 @@ USLEEP=$(( USLEEP- ((SPEED/10000)*1000) ))
 
 _check_hp_and_return_home(){
 
+local REPLY
+
 test "$1" && local currHP=$1
 test "$2" && local currHPMin=$2
 
@@ -115,9 +117,17 @@ test "$currHPMin"  || local currHPMin=$((MHP/10))
 
 [ "$DEBUG" ] && echo draw 3 currHP=$currHP currHPMin=$currHPMin
 if test "$currHP" -le $currHPMin; then
+
+read -t 1 REPLY
 echo issue 1 1 apply -a rod of word of recall
+unset REPLY
+read -t 1 REPLY
+
 echo issue 1 1 fire center ## Todo check if already applied and in inventory
 echo issue 1 1 fire_stop
+unset REPLY
+read -t 1 REPLY
+
 echo unwatch drawinfo
 exit
 fi
@@ -129,46 +139,85 @@ unset HP
 
 _cast_create_food_and_eat(){
 
-local lEAT_FOOD
+local lEAT_FOOD REPLY1 REPLY2 REPLY3 REPLY4
 
 test "$*" && lEAT_FOOD="$@"
 test "$EAT_FOOD" || lEAT_FOOD=$FOOD_DEF
 test "$EAT_FOOD" || lEAT_FOOD=food
 
 echo issue pickup 0
+
+for i in `seq 1 1 2`; do
 sleep 1
+read -t 1 REPLY1
+[ "$DEBUG" ] && echo draw 3 "REPLY1=$REPLY1"
+unset REPLY1
+done
+
 echo issue 1 1 cast create food $lEAT_FOOD
+
+for i in `seq 1 1 2`; do
 sleep 1
+read -t 1 REPLY2
+[ "$DEBUG" ] && echo draw 3 "REPLY2=$REPLY2"
+unset REPLY2
+done
+
 echo issue 1 1 fire center ## Todo handle bungling the spell
 sleep 0.1
 echo issue 1 1 fire_stop
 sleep 1
 
+for i in `seq 1 1 2`; do
+read -t 1 REPLY3
+[ "$DEBUG" ] && echo draw 3 "REPLY3=$REPLY3"
+unset REPLY3
+done
+
 echo issue 1 1 apply ## Todo check if food is there on tile
+for i in `seq 1 1 2`; do
+sleep 1
+read -t 1 REPLY4
+[ "$DEBUG" ] && echo draw 3 "REPLY4=$REPLY4"
+unset REPLY4
+done
 
 }
 
 _apply_horn_of_plenty_and_eat(){
+local REPLY
 
+read -t 1 REPLY
 echo issue 1 1 apply -a Horn of Plenty
 sleep 1
+unset REPLY
+read -t 1 REPLY
+
 echo issue 1 1 fire center ## Todo handle bungling
 echo issue 1 1 fire_stop
 sleep 1
-echo issue 1 1 apply ## Todo check if food is there on tile
+unset REPLY
+read -t 1 REPLY
 
+echo issue 1 1 apply ## Todo check if food is there on tile
+unset REPLY
+read -t 1 REPLY
 }
 
 
 _eat_food(){
+
+local REPLY
 
 test "$*" && EAT_FOOD="$@"
 test "$EAT_FOOD" || EAT_FOOD=waybread
 
 #_check_food_inventory ## Todo: check if food is in INV
 
+read -t 1 REPLY
 echo issue 1 1 apply $EAT_FOOD
-
+unset REPLY
+read -t 1 REPLY
 }
 
 _check_food_level(){
@@ -180,7 +229,10 @@ _check_food_level(){
 test "$*" && MIN_FOOD_LEVEL="$@"
 test "$MIN_FOOD_LEVEL" || MIN_FOOD_LEVEL=200
 
-local FOOD_LVL='';
+local FOOD_LVL=''
+local REPLY
+
+read -t 1 REPLY # empty the stream of messages
 
 echo watch drawinfo
 sleep 1
@@ -200,15 +252,19 @@ if test "$FOOD_LVL" -lt $MIN_FOOD_LEVEL; then
  #_eat_food
  _cast_create_food_and_eat $EAT_FOOD
 
- sleep 2
+ sleep 1
+ unset REPLY
+ read -t 1 REPLY
+ sleep 1
  unset FOOD_LVL
+ echo request stat hp   #hp,maxhp,sp,maxsp,grace,maxgrace,food
+ #sleep 0.1
+ sleep 1
  read -t1 Re2 Stat2 Hp2 HP2 MHP2 SP2 MSP2 GR2 MGR2 FOOD_LVL
  [ "$DEBUG" ] && echo draw 3 HP=$HP2 $MHP2 $SP2 $MSP2 $GR2 $MGR2 FOOD_LVL=$FOOD_LVL #DEBUG
 
- test "$FOOD_LVL" || break
- test "${FOOD_LVL//[[:digit:]]/}" && break
-
-
+ #test "$FOOD_LVL" || break
+ #test "${FOOD_LVL//[[:digit:]]/}" && break
 
  #return $?
  break
