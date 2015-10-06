4b7a3545b9a62e403044e3a7d1583fa93cc8443b
KarlGodt <karlgodt@excite.de>
2015-10-05 23:51:37 -0100

    /root/.crossfire/s/cf_PrayXtimes.sh: Use _draw() . PAUSE_CHECK_FOOD renamed to COUNT_CHECK_FOOD .

--- woof-code/rootfs-skeleton/root/.crossfire/s/cf_PrayXtimes.sh
+++ woof-code/rootfs-skeleton/root/.crossfire/s/cf_PrayXtimes.sh
@@ -30,7 +30,8 @@ export PATH=/bin:/usr/bin
 
 
 # Global variables
-PAUSE_CHECK_FOOD=10 # number between praying attemts to check foodlevel.
+__variables__(){
+COUNT_CHECK_FOOD=10 # number between praying attempts to check foodlevel.
                     #  1 would mean check every pray, which is too much
 EAT_FOOD=waybread   # set to desired food to eat ie food, mushroom, booze, .. etc.
 FOOD_DEF=$EAT_FOOD     # default
@@ -40,11 +41,12 @@ MIN_FOOD_LEVEL_DEF=200 # default minimum. 200 starts to beep. waybread has foodv
 HP_MIN_DEF=20          # minimum HP to return home. Lowlevel charakters probably need this set.
 
 DEBUG=1; #set to ANYTHING ie "1" to enable, empty to disable
+}
 
-
+__set_global_variables(){
 # Log file path in /tmp
-MY_SELF=`realpath "$0"`
-MY_BASE=${MY_SELF##*/}
+#MY_SELF=`realpath "$0"`
+#MY_BASE=${MY_SELF##*/}
 TMP_DIR=/tmp/crossfire
 mkdir -p "$TMP_DIR"
 REPLY_LOG="$TMP_DIR"/"$MY_BASE".$$.rpl
@@ -52,11 +54,17 @@ REQUEST_LOG="$TMP_DIR"/"$MY_BASE".$$.req
 ON_LOG="$TMP_DIR"/"$MY_BASE".$$.ion
 
 exec 2>>"$TMP_DIR"/"$MY_BASE".$$.err
+}
+
+MY_SELF=`realpath "$0"`
+MY_BASE=${MY_SELF##*/}
+test -f "${MY_SELF%/*}"/"${MY_BASE}".conf && . "${MY_SELF%/*}"/"${MY_BASE}".conf
+test -f "${MY_SELF%/*}"/cf_functions.sh   && . "${MY_SELF%/*}"/cf_functions.sh
 
 
 # *** Here begins program *** #
-echo draw 2 "$0 has started.."
-echo draw 2 "PARAM:$* PID:$$ PPID :$PPID"
+_draw 2 "$0 has started.."
+_draw 2 "PARAM:$* PID:$$ PPID :$PPID"
 
 # *** Check for parameters *** #
 [ "$*" ] && {
@@ -65,11 +73,11 @@ PARAM_1="$1"
 # *** implementing 'help' option *** #
 case "$PARAM_1" in *"help"*)
 
-echo draw 5 "Script to pray given number times."
-echo draw 5 "Syntax:"
-echo draw 5 "script $0 <number>"
-echo draw 5 "For example: 'script $0 50'"
-echo draw 5 "will issue 50 times the use_skill praying command."
+_draw 5 "Script to pray given number times."
+_draw 5 "Syntax:"
+_draw 5 "script $0 <number>"
+_draw 5 "For example: 'script $0 50'"
+_draw 5 "will issue 50 times the use_skill praying command."
 
         exit 0
 ;; esac
@@ -77,29 +85,30 @@ echo draw 5 "will issue 50 times the use_skill praying command."
 # *** testing parameters for validity *** #
 PARAM_1test="${PARAM_1//[[:digit:]]/}"
 test "$PARAM_1test" && {
-echo draw 3 "Only :digit: numbers as first option allowed."
+_draw 3 "Only :digit: numbers as first option allowed."
         exit 1 #exit if other input than letters
         }
 
 NUMBER=$PARAM_1
 
 } || {
-echo draw 3 "Script needs number of praying attempts as argument."
+_draw 3 "Script needs number of praying attempts as argument."
         exit 1
 }
 
 test "$1" || {
-echo draw 3 "Need <number> ie: script $0 50 ."
+_draw 3 "Need <number> ie: script $0 50 ."
         exit 1
 }
 
-test -f "${MY_SELF%/*}"/cf_functions.sh && . "${MY_SELF%/*}"/cf_functions.sh
+#test -f "${MY_SELF%/*}"/cf_functions.sh && . "${MY_SELF%/*}"/cf_functions.sh
 
+_get_player_speed(){
 echo request stat cmbt
 #read REQ_CMBT
 #snprintf(buf, sizeof(buf), "request stat cmbt %d %d %d %d %d\n", cpl.stats.wc, cpl.stats.ac, cpl.stats.dam, cpl.stats.speed, cpl.stats.weapon_sp);
-read Req Stat Cmbt WC AC DAM SPEED W_SPEED
-[ "$DEBUG" ] && echo draw 7 "wc=$WC:ac=$AC:dam=$DAM:speed=$SPEED:weaponspeed=$W_SPEED"
+read -t 1 Req Stat Cmbt WC AC DAM SPEED W_SPEED
+[ "$DEBUG" ] && _draw 7 "wc=$WC:ac=$AC:dam=$DAM:speed=$SPEED:weaponspeed=$W_SPEED"
 case $SPEED in
 1[0-9][0-9][0-9][0-9]) USLEEP=1500000;;
 2[0-9][0-9][0-9][0-9]) USLEEP=1400000;;
@@ -112,11 +121,12 @@ case $SPEED in
 9[0-9][0-9][0-9][0-9]) USLEEP=700000;;
 *) USLEEP=600000;;
 esac
-[ "$DEBUG" ] && echo draw 10 "USLEEP=$USLEEP:SPEED=$SPEED"
+[ "$DEBUG" ] && _draw 10 "USLEEP=$USLEEP:SPEED=$SPEED"
 
-USLEEP=$(( USLEEP- ((SPEED/10000)*1000) ))
-[ "$DEBUG" ] && echo draw 7 "Sleeping $USLEEP usleep micro-seconds between praying"
-
+USLEEP=$(( USLEEP - ( (SPEED/10000) * 1000 ) ))
+[ "$DEBUG" ] && _draw 7 "Sleeping $USLEEP usleep micro-seconds between praying"
+}
+_get_player_speed
 
 
 # *** Actual script to pray multiple times *** #
@@ -131,18 +141,18 @@ echo "issue 1 1 use_skill praying"
 usleep $USLEEP
 
 c=$((c+1))
-test $c -ge $PAUSE_CHECK_FOOD && {
+test $c -ge $COUNT_CHECK_FOOD && {
 c=0
 _check_food_level
 #_check_hp_and_return_home $HP $HP_MIN_DEF
 _check_hp_and_return_home $HP
 unset Re Stat Hp HP MHP SP MSP GR MGR FOOD_LVL
 unset Re2 Stat2 Hp2 HP2 MHP2 SP2 MSP2 GR2 MGR2
-echo draw 5 "$((NUMBER-one)) prayings left"
+_draw 5 "$((NUMBER-one)) prayings left"
 }
 
 done
 
 # *** Here ends program *** #
 test -f /root/.crossfire/sounds/su-fanf.raw && aplay /root/.crossfire/sounds/su-fanf.raw
-echo draw 2 "$0 is finished."
+_draw 2 "$0 is finished."
