de3e8ad5230a20c41c53deb6976825a2c4c97039
KarlGodt <karlgodt@excite.de>
2015-10-08 17:34:41 -0100

    /root/.crossfire/s/cf_BalmOfFirstAid.sh: source .conf file after cf_functions.sh to override default variables.

--- woof-code/rootfs-skeleton/root/.crossfire/s/cf_BalmOfFirstAid.sh
+++ woof-code/rootfs-skeleton/root/.crossfire/s/cf_BalmOfFirstAid.sh
@@ -6,10 +6,10 @@ export PATH=/bin:/usr/bin
 
 MY_SELF=`realpath "$0"`
 MY_BASE=${MY_SELF##*/}
-test -f "${MY_SELF%/*}"/"${MY_BASE}".conf && . "${MY_SELF%/*}"/"${MY_BASE}".conf
 test -f "${MY_SELF%/*}"/cf_functions.sh   && . "${MY_SELF%/*}"/cf_functions.sh
-
 _set_global_variables
+# *** Override any VARIABLES in cf_functions.sh *** #
+test -f "${MY_SELF%/*}"/"${MY_BASE}".conf && . "${MY_SELF%/*}"/"${MY_BASE}".conf
 
 # *** Here begins program *** #
 _say_start_msg "$@"
