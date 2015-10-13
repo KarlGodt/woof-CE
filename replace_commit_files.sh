5bc3ae1f170bde664f1b7e5d2e1c5f6723de5fed
KarlGodt <karlgodt@excite.de>
2015-10-13 11:41:57 -0100

    replace_commit_files.sh: Changes for being placed in git_helper/ directory.

--- replace_commit_files.sh
+++ replace_commit_files.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/ash
 
 error(){
         exit=$1; shift
@@ -12,21 +12,27 @@ error_cont(){
     return $exit
 }
 
-CURRENT_DIR=`pwd`
-cd "$CURRENT_DIR"
+DEBUG=1
 
-DIRS=`find woof-code/rootfs-skeleton/ -type d -not -name "*.git"`
+#CURRENT_DIR=`pwd`
+#cd "$CURRENT_DIR"
+MY_SELF=`realpath "$0"`
+CURRENT_DIR=${MY_SELF%/*}
+cd "$CURRENT_DIR" || exit 1
+
+
+DIRS=`find ../woof-code/rootfs-skeleton/ -type d -not -name "*.git"`
 
 while read ONE_DIR; do
 [ "$DEBUG" ] && echo "ONE_DIR='$ONE_DIR'"
 test "$ONE_DIR" || continue
-
+test -L "$ONE_DIR" && continue
 #ONE_DIR_IN_SYSTEM=`echo "$ONE_DIR" | sed 's!^\.*/woof-code/rootfs-skeleton!!'`
-ONE_DIR_IN_SYSTEM=`echo "$ONE_DIR" | sed 's!^woof-code/rootfs-skeleton!!'`
+ONE_DIR_IN_SYSTEM=`echo "$ONE_DIR" | sed 's!^../woof-code/rootfs-skeleton!!'`
 [ "$DEBUG" ] && echo "ONE_DIR_IN_SYSTEM='$ONE_DIR_IN_SYSTEM'"
 test -d "$ONE_DIR_IN_SYSTEM" || continue
 
- cd "$CURRENT_DIR/$ONE_DIR" || error 1 "Could not cd into '$CURRENT_DIR/$ONE_DIR'"
+ cd "$CURRENT_DIR/../$ONE_DIR" || error 1 "Could not cd into '$CURRENT_DIR/../$ONE_DIR'"
 
  FILES=`ls -1Av | grep -v '.git'`
 
@@ -51,7 +57,7 @@ test -d "$ONE_DIR_IN_SYSTEM" || continue
 
  [ -e "$ONE_DIR_IN_SYSTEM/$ONE_FILE" ] && {
 
-         cd "$CURRENT_DIR/$ONE_DIR" || error 1 "Could not cd into '$CURRENT_DIR/$ONE_DIR'"
+         cd "$CURRENT_DIR/../$ONE_DIR" || error 1 "Could not cd into '$CURRENT_DIR/../$ONE_DIR'"
 
          diff -q "$ONE_DIR_IN_SYSTEM/$ONE_FILE" ./"$ONE_FILE" && continue #returns 1 if differ
 
@@ -60,14 +66,14 @@ test -d "$ONE_DIR_IN_SYSTEM" || continue
 
          if [ "$modSYSfile" -lt "$modGITfile" ]; then
          echo FILE in GIT newer
-         cp -a --remove-destination ./"$ONE_FILE" "$ONE_DIR_IN_SYSTEM"/
+         /bin/cp -a --remove-destination ./"$ONE_FILE" "$ONE_DIR_IN_SYSTEM"/
          continue
          else
-         cp -a --remove-destination "$ONE_DIR_IN_SYSTEM/$ONE_FILE" .
+         /bin/cp -a --remove-destination "$ONE_DIR_IN_SYSTEM/$ONE_FILE" .
          sleep 1
          fi
 
-         cd "$CURRENT_DIR" || error 1 "Could not cd into '$CURRENT_DIR'"
+         cd "$CURRENT_DIR"/../ || error 1 "Could not cd into '$CURRENT_DIR/../'"
 
          git add "$ONE_DIR/$ONE_FILE" || continue
 
@@ -75,7 +81,7 @@ test -d "$ONE_DIR_IN_SYSTEM" || continue
 by the one found currently in the system." || error_cont 1 "git commit failed."
          sleep 1
 
-         cd "$CURRENT_DIR/$ONE_DIR" || error 1 "Could not cd into '$CURRENT_DIR/$ONE_DIR'"
+         cd "$CURRENT_DIR/../$ONE_DIR" || error 1 "Could not cd into '$CURRENT_DIR/../$ONE_DIR'"
 
         } || { echo "$ONE_DIR_IN_SYSTEM/$ONE_FILE : No such so."; }
 
