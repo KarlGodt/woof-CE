708883233ed717151eb6b7c978fe6d0533385ca7
KarlGodt <karlgodt@excite.de>
2015-10-21 22:06:17 -0100

    /etc/acpi/wakealarm/wakealarm_wake.sh: Cleanup. Use _debug instead simple echo .

--- woof-code/rootfs-skeleton/etc/acpi/wakealarm/wakealarm_wake.sh
+++ woof-code/rootfs-skeleton/etc/acpi/wakealarm/wakealarm_wake.sh
@@ -29,17 +29,17 @@ read label col alrm_date <<EoI
 EoI
 
 
-echo rtc_time=$rtc_time
-echo alrm_time=$alrm_time
-echo rtc_date=$rtc_date
-echo alrm_date$alrm_date
+_debug rtc_time=$rtc_time
+_debug alrm_time=$alrm_time
+_debug rtc_date=$rtc_date
+_debug alrm_date$alrm_date
 
 if test $alrm_date -a $alrm_time
 then
 :
- echo "$0:$*:$@: Wakealarm is set"
+ _notice "$0:$*:$@: Wakealarm is set"
 else
- echo "$0:$*:$@: Wakealarm is not set"
+ _info "$0:$*:$@: Wakealarm is not set"
  exit 0
 fi
 
@@ -49,63 +49,53 @@ curr_h=${rtc_time%%:*}
 curr_h=${curr_h#*0}
 test $curr_h || curr_h=24
 
-echo curr_h=$curr_h
+_debug curr_h=$curr_h
 
 curr_m=${rtc_time%:*}
 curr_m=${curr_m#*:}
 curr_m=${curr_m#*0}
 test $curr_m || curr_m=60
 
-echo  curr_m=$curr_m
+_debug  curr_m=$curr_m
 
 alrm_h=${alrm_time%%:*}
 alrm_h=${alrm_h#*0}
 test $alrm_h || alrm_h=24
 
-echo alrm_h=$alrm_h
+_debug alrm_h=$alrm_h
 
 alrm_m=${alrm_time%:*}
- echo alrm_m=$alrm_m
+ _debug alrm_m=$alrm_m
 alrm_m=${alrm_m#*:}
- echo alrm_m=$alrm_m
+ _debug alrm_m=$alrm_m
 alrm_m=${alrm_m#*0}
- echo alrm_m=$alrm_m
+ _debug alrm_m=$alrm_m
 test $alrm_m || alrm_m=60
 
-echo alrm_m=$alrm_m
+_debug alrm_m=$alrm_m
 
 if test $rtc_date = $alrm_date
 then
  :
- echo "$0:$*:$@: date is same"
+ _info "$0:$*:$@: date is same"
 elif test $rtc_date = $((alrm_date + 1)) && test $alrm_m = 59 -o $alrm_h = 23
 then
  :
 
 else
- echo "$0:$*:$@: date differs"
+ _info "$0:$*:$@: date differs"
  exit 0
 fi
 
 if test $curr_h = $alrm_h || test $curr_h = $((alrm_h+1)) -a $$alrm_m = 59
 then
  m_diff=$(( curr_m - alrm_m ))
- echo m_diff=$m_diff
+ _debug m_diff=$m_diff
 
    case $m_diff in
-   -*) echo "$0:$*:$@: Apparently less than one minute ago"; exit 2;;
+   -*) _notice "$0:$*:$@: Apparently less than one minute ago"; exit 2;;
    0|1) test -x /etc/acpi/wakealarm/wakealarm_cmd && exec /etc/acpi/wakealarm/wakealarm_cmd;;
-   *)  echo "$0:$*:$@: Apparently more than one minute ago"; exit 1;;
+   *)  _notice "$0:$*:$@: Apparently more than one minute ago"; exit 1;;
    esac
 fi
-
-
-
-
-
-
-
-
-
-
 
