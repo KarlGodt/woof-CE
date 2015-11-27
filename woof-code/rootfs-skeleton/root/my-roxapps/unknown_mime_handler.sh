e3200a0e158b6296e369fbeed29ad3e34f6eb061
KarlGodt <karlgodt@excite.de>
2015-11-27 16:39:38 -0100

    /root/my-roxapps/unknown_mime_handler.sh: Code cleanup. Source f4puppy5 . Add ocfs2 support and be in sync with filemnt and rc.shutdown .

--- woof-code/rootfs-skeleton/root/my-roxapps/unknown_mime_handler.sh
+++ woof-code/rootfs-skeleton/root/my-roxapps/unknown_mime_handler.sh
@@ -1,22 +1,25 @@
 #!/bin/ash
 
+. /etc/rc.d/f4puppy5
+
 #EXT=`echo "$@" |rev|cut -f1 -d'.'|rev`
  EXT="${@##*.}"
+_debug "EXT=$EXT"
 
 case $EXT in
 #here petget special
-lapet|xzpet|lopet|b2pet)
+b2pet|lapet|lopet|xzpet)
 exec /usr/local/petget/petget "$@"
 ;;
 
 #here comes the additional handlers
-sqfs|squashfs|mnx|rfs|rfs3|rfs4|xfs|jfs|btrfs)
+btr|btrfs|jfs|mnx|ofs|rfs|rfs3|rfs4|sqfs|squashfs|xfs)
 filemnt "$@"
 ;;
 *~)
-file "$@" | grep -i 'text' && defaulttexteditor "$@"
+file "$@" | grep $Q -i 'text' && defaulttexteditor "$@"
 ;;
-bz2|xz|lzma|lzo)
+bz2|lz|lzm|lzma|lzo|xz|Z)
 pupzip "$@"
 ;;
 #here comes the additional handlers end
