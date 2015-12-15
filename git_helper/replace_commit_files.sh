7bbb3bcf3d82de2231b444c980e7eb74c437017c
KarlGodt <karlgodt@excite.de>
2015-12-15 15:55:39 -0100

    git_helper/: Directory added. Scripts: winstall.sh diffinstalled.sh replace_commit_files.sh update_files_from_running_system update_files.sh
    From Fox3-Dell755 branch.

---
+++ git_helper/replace_commit_files.sh
@@ -0,0 +1,111 @@
+#!/bin/ash
+
+. /etc/rc.d/PUPSTATE
+. /etc/DISTRO_SPECS
+. /etc/rc.d/f4puppy5
+
+_cd_program_dir || exit
+
+COMMIT_MSG=" Replaced
+ by the one found currently in the system."
+[ "$*" ] && COMMIT_MSG="$*"
+
+
+DIR_PRE='../'
+
+_help(){
+echo "$0:"
+echo "Batch script to find dirs in woof-code/rootfs-skeleton/"
+echo "and contents in these dirs by ls -1vA ."
+echo "diff -q these contents and continue if diff returns zero."
+echo "Otherwise use stat -c %Y to test modification times of"
+echo "both files in GIT and SYSTEM and updating either"
+echo "Git if System is newer OR System if Git is newer."
+exit 0
+}
+
+case $1 in ''):;; -*help) _help;;esac
+
+_error(){
+    _exit_=$1; shift
+    echo "$*"
+    exit $_exit_
+}
+
+_error_cont(){
+    _exit_=$1; shift
+    echo "$*"
+    return $_exit_
+}
+
+CURRENT_DIR=`pwd`
+cd "$CURRENT_DIR"
+
+#DIRS=`find  -type d`
+DIRS=`find ${DIR_PRE}woof-code/rootfs-skeleton -type d`
+
+
+while read ONE_DIR; do
+#echo "ONE_DIR='$ONE_DIR'"
+test "$ONE_DIR" || continue
+
+#ONE_DIR_IN_SYSTEM=`echo "$ONE_DIR" | sed 's!^\.*/woof-code/rootfs-skeleton!!'`
+ONE_DIR_IN_SYSTEM=`echo "$ONE_DIR" | sed "s!^${DIR_PRE}woof-code/rootfs-skeleton!!"`
+_debugx "ONE_DIR_IN_SYSTEM='$ONE_DIR_IN_SYSTEM'"
+
+test -d "$ONE_DIR_IN_SYSTEM" || continue
+
+ cd "$CURRENT_DIR/$ONE_DIR" || _error 1 "Could not cd into '$CURRENT_DIR/$ONE_DIR'"
+
+ FILES=`ls -1vA`
+
+ while read ONE_FILE; do
+ _debugx "ONE_FILE='$ONE_FILE'"
+ test "$ONE_FILE" || continue
+
+ test -f "$ONE_DIR_IN_SYSTEM/$ONE_FILE" || continue
+ test -L "$ONE_DIR_IN_SYSTEM/$ONE_FILE" && continue
+
+ case $ONE_FILE in
+ *.gz|*.xpm|*.afm|*.pfb|*.ttf|*.au|*.wav|*.ogg|*.jpg|fonts.*|*.pcf|*.png|*.so|*.so.conf|*.svg|yaf-splash)
+ _notice "Skipping '$ONE_FILE'"; continue;;
+ esac
+
+ [ -e "$ONE_DIR_IN_SYSTEM/$ONE_FILE" ] && {
+
+     cd "$CURRENT_DIR/$ONE_DIR" || _error 1 "Could not cd into '$CURRENT_DIR/$ONE_DIR'"
+
+     diff -q "$ONE_DIR_IN_SYSTEM/$ONE_FILE" ./"$ONE_FILE" && continue #returns 1 if differ
+
+     MODIFIED1=`stat -c %Y "$ONE_DIR_IN_SYSTEM/$ONE_FILE"`
+     MODIFIED2=`stat -c %Y ./"$ONE_FILE"`
+
+     if test "$MODIFIED2" -ge "$MODIFIED1"; then
+     echo "'$ONE_FILE' in WOOF directory newer - replacing the one in system ..."
+     /bin/cp $VERB -a --remove-destination ./"$ONE_FILE" "$ONE_DIR_IN_SYSTEM/"
+     continue
+     else
+     /bin/cp $VERB -a --remove-destination "$ONE_DIR_IN_SYSTEM/$ONE_FILE" .
+     fi
+
+     sleep 1
+
+     cd "$CURRENT_DIR" || _error 1 "Could not cd into '$CURRENT_DIR'"
+
+     git add "$ONE_DIR/$ONE_FILE" || continue
+
+     git commit -m "$ONE_DIR_IN_SYSTEM/$ONE_FILE : ""$COMMIT_MSG" || _error_cont 1 "git commit failed."
+     sleep 1
+
+     cd "$CURRENT_DIR/$ONE_DIR" || _error 1 "Could not cd into '$CURRENT_DIR/$ONE_DIR'"
+
+    } || { _err "$ONE_DIR_IN_SYSTEM/$ONE_FILE : No such so."; }
+
+ done <<EOI
+`echo "$FILES"`
+EOI
+
+
+done <<EOH
+`echo "$DIRS"`
+EOH
