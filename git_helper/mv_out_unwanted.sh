52ccf055ce782b6b522141ec30e7c1cf12326b76
KarlGodt <karlgodt@excite.de>
2015-12-21 15:17:24 -0100

    git_helper/mv_out_unwanted.sh: Added. Script to move unwanted untracked files out of the current repository.

---
+++ git_helper/mv_out_unwanted.sh
@@ -0,0 +1,25 @@
+#!/bin/ash
+
+. /etc/rc.d/f4puppy5
+. /etc/rc.d/PUPSTATE
+. /etc/DISTRO_SPECS
+
+
+MV_DIR=../../moved_out
+
+
+_cd_program_dir || _exit $? "Could not change into working directory"
+
+mkdir $VERB -p "$MV_DIR"
+cd ..
+
+git commit --short | while read Q f
+do
+test -f "$f" -o -d "$f" || continue
+
+mvdir=${f%/*}
+test "$mvdir" || mvdir=/
+mkdir $VERB -p "$MV_DIR"/"$mvdir" || break
+mv $VERB "$f" "$MV_DIR"/"$mvdir"  || break
+
+done
