74324ed82a61a2c6f71b61c2caaddc8c4ff6b968
KarlGodt <karlreimergodt@yahoo.de>
2016-09-27 02:18:39 -0100

    /git_helper/winstall.sh: Added code for dry-run since i did not used the code much and was wondering how exactly it was copying which file to where location. Also use /bin/find instead busybox find . Try to run mkfontdir since Xorg choked afterwards that it could not find font 'fixed' .. and crashed ( R7-1.3.0 ) ..

--- git_helper/winstall.sh
+++ git_helper/winstall.sh
@@ -1,8 +1,14 @@
 #!/bin/ash
 
 
+dryRUN=${dryRUN:-}
 
-DIRS=`find -type d`
+case $PWD in
+*/rootfs-skeleton) :;;
+*) echo "Needs to run inside rootfs-skeleton directory."; exit 1;;
+esac
+
+DIRS=`/bin/find -type d`
 
 
 while read onedir
@@ -20,8 +26,14 @@ echo " onedir=$onedir" >&2
 echo "onedirS=$onedirS" >&2
 #sleep 5
 
-#in case of '.' unset onedirS will test wrong and cp . ""/ .. 
-test -e "$onedirS" || { /bin/cp -v -ax --remove-destination "$onedir" "${onedirS%/*}/" || exit; continue; }
+#in case of '.' unset onedirS will test wrong and cp . ""/ ..
+test -e "$onedirS" || {
+  if test "$dryRUN"; then
+   echo "Would now copy $onedir ${onedirS%/*}/"
+  else
+   /bin/cp $VERB -ax --remove-destination "$onedir" "${onedirS%/*}/" || exit;
+  fi
+    continue; }
 
  while read oneFILE
  do
@@ -31,11 +43,19 @@ test -e "$onedirS" || { /bin/cp -v -ax --remove-destination "$onedir" "${onedirS
  [ -d "$onedir/$oneFILE" ] && continue
 
  if test -L "$onedir/$oneFILE"; then
- /bin/cp -v -ax --remove-destination "$onedir/$oneFILE" "$onedirS"/ || : #exit
+  if test "$dryRUN"; then
+   echo "Would now copy $onedir/$oneFILE $onedirS/"
+  else
+   /bin/cp $VERB -ax --remove-destination "$onedir/$oneFILE" "$onedirS"/ || : #exit
+  fi
  else
- /bin/cp -v -ax --backup=numbered "$onedir/$oneFILE" "$onedirS"/ || : #exit
+  if test "$dryRUN"; then
+   echo "Would now copy $onedir/$oneFILE $onedirS/"
+  else
+   /bin/cp $VERB -ax --backup=numbered "$onedir/$oneFILE" "$onedirS"/ || : #exit
+  fi
  fi
- 
+
  #echo "onedir/oneFILE=$onedir/$oneFILE"
  #sleep 1
  done <<EoI
@@ -50,13 +70,48 @@ EoI
 _remove_backups(){
 #find backups and diff them. if identical to the original file
 # remove them. this is in case of mutiple runs and multipe backups.
-BUPS=`find / -xdev -name "*.~[0-9]~"`
-for i in $BUPS; do 
-file=${i%.~*}; 
-echo $file; 
-diff -qs $i $file && rm $i;
+BUPS=`/bin/find / -xdev -name "*.~[0-9]~"`
+for i in $BUPS; do
+file=${i%.~*};
+echo $i $file;
+diff $Q -s $i $file && rm $VERB $i;
 done
 echo
-find / -xdev -name "*.~[0-9]~"
+/bin/find / -xdev -name "*.~[0-9]~"
 }
 _remove_backups
+
+#TODO: _mkfontdir
+_mkfontdir(){
+c=0
+if test -s /etc/X11/xorg.conf; then
+FP=`grep -i fontpath /etc/X11/xorg.conf | awk '{print $2}' | cut -f2 -d'"'`
+ if test "$FP"; then
+ for d in $FP; do
+ test -L $d && continue
+ test -d $d || continue
+ c=$((c+1))
+ echo $d
+ mkfontdir $d
+ done
+ test "$c" = 0 || return #found some dirs
+ fi
+fi
+# else fallback
+ for d1 in /usr/share/fonts /usr/share/X11/fonts \
+ /usr/local/share/fonts /usr/local/share/X11/fonts \
+ /usr/X11R7/share/fonts /usr/X11R7/share/X11/fonts \
+ /usr/lib/X11/fonts /usr/X11R7/lib/fonts /usr/X11R7/lib/X11/fonts
+do
+ test -L $d1 && continue
+ test -d $d1 || continue
+ for d2 in default TTF Type1 misc default/TTF default/Type1 default/misc
+ do
+ test -L $d1/$d2 && continue
+ test -d $d1/$d2 || continue
+ echo $d1/$d2
+ mkfontdir $d1/$d2
+ done
+done
+}
+_mkfontdir
