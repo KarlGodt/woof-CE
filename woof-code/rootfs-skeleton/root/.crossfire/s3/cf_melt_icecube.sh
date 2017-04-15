9e144618f43dbab47187d162022103e323881622
KarlGodt <karlgodt@excite.de>
2017-04-15 00:55:32 -0100

    /root/.crossfire/s3/cf_melt_icecube.sh:

--- woof-code/rootfs-skeleton/root/.crossfire/s3/cf_melt_icecube.sh
+++ woof-code/rootfs-skeleton/root/.crossfire/s3/cf_melt_icecube.sh
@@ -44,33 +44,9 @@ _draw 5 "-v to say what is being issued to server."
         exit 0
 }
 
-
-#DEBUG=1   # unset to disable, set to anything to enable
-#LOGGING=1 # unset to disable, set to anything to enable
-
-DRAW_INFO=drawinfo # drawextinfo (older clients)
+#DRAW_INFO=drawinfo # drawextinfo (older clients)
 
 LOGFILE=${LOGFILE:-/tmp/cf_script.rpl}
-
-_log_(){
-[ "$LOGGING" ] || return 0
-echo "$*" >>"$LOGFILE"
-}
-
-__black()  { echo draw 0  "$*"; }
-__bold()   { echo draw 1  "$*"; }
-__blue()   { echo draw 5  "$*"; }
-__navy()   { echo draw 2  "$*"; }
-__lgreen() { echo draw 8  "$*"; }
-__green()  { echo draw 7  "$*"; }
-__red()    { echo draw 3  "$*"; }
-__dorange(){ echo draw 6  "$*"; }
-__orange() { echo draw 4  "$*"; }
-__gold()   { echo draw 11 "$*"; }
-__tan()    { echo draw 12 "$*"; }
-__brown()  { echo draw 10 "$*"; }
-__gray()   { echo draw 9  "$*"; }
-alias __grey=__gray
 
 
 # *** Here begins program *** #
@@ -105,20 +81,6 @@ sleep 0.1
 done
 
 
-__watch(){
-echo watch $DRAW_INFO
-}
-
-__unwatch(){
-echo unwatch $DRAW_INFO
-}
-
-__just_exit(){
-_red "Exiting $0."
-_unwatch
-#echo unwatch drawinfo
-exit $1
-}
 
 # *** Getting Player's Speed *** #
 _get_player_speed
@@ -133,80 +95,12 @@ _watch
 
 TIMEB=`date +%s`
 
-__old_if_number(){
-
-    if test "$NUMBER"; then
-
-for one in `seq 1 1 $NUMBER`
-do
-
-REPLY=
-OLD_REPLY=
-
-#echo "issue 1 1 mark icecube"
-_is 1 1 mark icecube
-
- while :; do
- read -t 1 REPLY
- _log "$REPLY"
- _debug "REPLY='$REPLY'"
-
- case $REPLY in
- *Could*not*find*an*object*that*matches*) break 2;;
- '') break;;
- esac
- unset REPLY
- sleep 0.1s
- done
-
-sleep 1s
-
 while :;
 do
 
 REPLY=
 OLD_REPLY=
 
-#echo "issue 1 1 apply flint and steel"
-_is 1 1 apply flint and steel
-
- while :; do
- read -t 1 REPLY
- _log "$REPLY"
- _debug "REPLY='$REPLY'"
-
- case $REPLY in
- *used*up*flint*and*steel*) break 3;;
- *fail*) FAIL=$((FAIL+1));;  # ORDER of fail STRINGS IMPORTANT !!!
- *Could*not*find*any*match*to*the*flint*and*steel*) break 3;;
- *You*light*the*icecube*) SUCC=$((SUCC+1)); break 2;;
- *You*need*to*mark*a*lightable*object.*)    break 2;;
- '') break;;
- #*) NO_FAIL=1;;
- #*) break;;
- esac
- unset REPLY
- sleep 0.1s
- done
-
-sleep 1s
-
-done
-
-done #NUMBER
-
-fi
-}
-
-#    else #PARAM_1
-
-while :;
-do
-
-REPLY=
-OLD_REPLY=
-
-#echo "issue 1 1 mark icecube"
 _is 1 1 mark icecube
 
 while :; do
@@ -231,7 +125,6 @@ do
 REPLY=
 OLD_REPLY=
 
-#echo "issue 1 1 apply flint and steel"
 _is 1 1 apply flint and steel
 
  while :; do
@@ -262,18 +155,6 @@ test "$one" = "$NUMBER" && break
 
 done #true
 
-#    fi #^!PARAM_1
-
-__get_time_end(){
-test "$TIMEB" || return 0
-TIMEE=`date +%s`
-TIMEX=$((TIMEE-TIMEB))
-TIMEM=$((TIMEX/60))
-TIMES=$(( TIMEX - (TIMEM*60) ))
-case $TIMES in [0-9]) TIMES="0$TIMES";; esac
-
-_green "Script loop took $TIMEM:$TIMES minutes."
-}
 
 # *** Here ends program *** #
 #echo draw 2 "$0 is finished."
