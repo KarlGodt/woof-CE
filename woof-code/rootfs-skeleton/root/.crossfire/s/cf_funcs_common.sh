134dd8465be3c63639ca92f16e86da2a856dd0cd
KarlGodt <karlreimergodt@yahoo.de>
2018-01-15 07:13:18 -0100

    /root/.crossfire/s/cf_funcs_common.sh: Adjustments to various functions.

--- woof-code/rootfs-skeleton/root/.crossfire/s/cf_funcs_common.sh
+++ woof-code/rootfs-skeleton/root/.crossfire/s/cf_funcs_common.sh
@@ -297,8 +297,8 @@ done
 _check_drawinfo(){  ##+++2018-01-08
 _debug "_check_drawinfo:$*"
 
-DEBUG0=$DEBUG;       DEBUG=${DEBUG:-''}
-LOGGING0=$LOGGING; LOGGING=${LOGGING:-1}
+oDEBUG=$DEBUG;       DEBUG=${DEBUG:-'1'}
+oLOGGING=$LOGGING; LOGGING=${LOGGING:-1}
 
 _draw 2 "Checking drawinfo ..."
 
@@ -312,7 +312,7 @@ do
 # like You search the area.
 # It could be something else,
 # but I have no better idea for the moment.
-#__is 0 0 search
+_is 0 0 search
 _is 0 0 examine
 
  unset cnt0 TICKS
@@ -325,18 +325,18 @@ _is 0 0 examine
  #_debug 3 "$cnt0:$REPLY"
 
  case $REPLY in
- #*drawinfo*'You search'*|*drawinfo*'You spot'*)       DRAWINFO0=drawinfo;    break 2;;
- #*drawextinfo*'You search'*|*drawextinfo*'You spot'*) DRAWINFO0=drawextinfo; break 2;;
- *drawinfo*'That is'*|*drawinfo*'These are'*)       DRAWINFO0=drawinfo;    break 2;;
- *drawextinfo*'That is'*|*drawextinfo*'These are'*) DRAWINFO0=drawextinfo; break 2;;
- *drawinfo*'This is'*|*drawinfo*'Those are'*)       DRAWINFO0=drawinfo;    break 2;;
- *drawextinfo*'This is'*|*drawextinfo*'Those are'*) DRAWINFO0=drawextinfo; break 2;;
+ *drawinfo*'You search'*|*drawinfo*'You spot'*)       DRAWINFO0=drawinfo;    break 2;;
+ *drawextinfo*'You search'*|*drawextinfo*'You spot'*) DRAWINFO0=drawextinfo; break 2;;
+ *drawinfo*'That is'*|*drawinfo*'Those are'*)         DRAWINFO0=drawinfo;    break 2;;
+ *drawextinfo*'That is'*|*drawextinfo*'Those are'*)   DRAWINFO0=drawextinfo; break 2;;
+ *drawinfo*'This is'*|*drawinfo*'These are'*)         DRAWINFO0=drawinfo;    break 2;;
+ *drawextinfo*'This is'*|*drawextinfo*'These are'*)   DRAWINFO0=drawextinfo; break 2;;
  *tick*) TICKS=$((TICKS+1)); test "$TICKS" -gt 19 && break 1;;
  '') break 1;;
  *) :;;
  esac
 
- sleep 0.1
+ sleep 0.001
  done
 
 _sleep
@@ -351,8 +351,8 @@ test "$DRAWINFO0" = "$DRAWINFO" || {
     DRAWINFO=$DRAWINFO0
 }
 
-DEBUG=$DEBUG0
-LOGGING=$LOGGING0
+DEBUG=$oDEBUG
+LOGGING=$oLOGGING
 _draw 6 "Done."
 }
 
@@ -363,10 +363,10 @@ case $1 in
 esac
 
 _move_back_and_forth 2
-sleep ${SLEEP}s
+_sleep
 _draw 3 "Exiting $0. $@"
 echo unwatch
-echo unwatch $DRAWINFO
+
 beep -l 1000 -f 700
 test ${RV//[0-9]/} && RV=3
 exit ${RV:-0}
@@ -374,17 +374,26 @@ exit ${RV:-0}
 
 _just_exit(){
 echo draw 3 "Exiting $0."
-echo unwatch
+_unwatch
 exit ${1:-0}
 }
 
 _emergency_exit(){
-_is 1 1 apply -u rod of word of recall
-_is 1 1 apply -a rod of word of recall
+
+case $RETURN_ITEM in
+''|*rod*|*staff*|*wand*|*horn*)
+_is 1 1 apply -u ${RETURN_ITEM:-'rod of word of recall'}
+_is 1 1 apply -a ${RETURN_ITEM:-'rod of word of recall'}
 _is 1 1 fire center
-_draw 3 "Emergency Exit $0 !"
-_unwatch $DRAWINFO
 _is 1 1 fire_stop
+;;
+*scroll*) -is 1 1 apply ${RETURN_ITEM};;
+*) :;;
+esac
+
+_draw 3 "Emergency Exit $0 !"
+_unwatch
+
 beep -l 1000 -f 700
 exit ${1:-0}
 }
@@ -407,7 +416,7 @@ __draw 3 "$eMSG"
 exit ${RV:-1}
 }
 
-_direction_to_number(){
+__direction_to_number(){
 DIRECTION=${1:-$DIRECTION}
 test "$DIRECTION" || return 0
 
@@ -422,11 +431,11 @@ case $DIRECTION in
 6|southwest|sw)    DIRECTION=6;;
 7|west|w)          DIRECTION=7;;
 8|northwest|nw)    DIRECTION=8;;
-*) ERROR=1 _error "Not recognized: '$DIRECTION'"
+*) ERROR=1 _error "Not recognized: '$DIRECTION'";;
 esac
 }
 
-_number_to_direction(){
+__number_to_direction(){
 DIRECTION=${1:-$DIRECTION}
 test "$DIRECTION" || return 0
 DIRECTION=`echo "$DIRECTION" | tr '[A-Z]' '[a-z]'`
@@ -440,7 +449,45 @@ case $DIRECTION in
 6|southwest|sw)    DIRECTION=southwest;;
 7|west|w)          DIRECTION=west;;
 8|northwest|nw)    DIRECTION=northwest;;
-*) ERROR=1 _error "Not recognized: '$DIRECTION'"
+*) ERROR=1 _error "Not recognized: '$DIRECTION'";;
+esac
+}
+
+_direction_to_number(){ # cf_funcs_move.sh
+local lDIRECTION=${1:-$DIRECTION}
+test "$lDIRECTION" || return 0
+
+lDIRECTION=`echo "$lDIRECTION" | tr '[A-Z]' '[a-z]'`
+case $lDIRECTION in
+0|center|centre|c) DIRECTION=0; DIRB=;          DIRF=;;
+1|north|n)         DIRECTION=1; DIRB=south;     DIRF=north;;
+2|northeast|ne)    DIRECTION=2; DIRB=southwest; DIRF=northeast;;
+3|east|e)          DIRECTION=3; DIRB=west;      DIRF=east;;
+4|southeast|se)    DIRECTION=4; DIRB=northwest; DIRF=southeast;;
+5|south|s)         DIRECTION=5; DIRB=north;     DIRF=south;;
+6|southwest|sw)    DIRECTION=6; DIRB=northeast; DIRF=southwest;;
+7|west|w)          DIRECTION=7; DIRB=east;      DIRF=west;;
+8|northwest|nw)    DIRECTION=8; DIRB=southeast; DIRF=northwest;;
+*) ERROR=1 _error "Not recognized: '$lDIRECTION'";;
+esac
+}
+
+_number_to_direction(){ # cf_funcs_move.sh
+local lDIRECTION=${1:-$DIRECTION}
+test "$lDIRECTION" || return 0
+
+lDIRECTION=`echo "$lDIRECTION" | tr '[A-Z]' '[a-z]'`
+case $lDIRECTION in
+0|center|centre|c) DIRECTION=center;    DIRB=;          DIRF=;;
+1|north|n)         DIRECTION=north;     DIRB=south;     DIRF=north;;
+2|northeast|ne)    DIRECTION=northeast; DIRB=southwest; DIRF=northeast;;
+3|east|e)          DIRECTION=east;      DIRB=west;      DIRF=east;;
+4|southeast|se)    DIRECTION=southeast; DIRB=northwest; DIRF=southeast;;
+5|south|s)         DIRECTION=south;     DIRB=north;     DIRF=south;;
+6|southwest|sw)    DIRECTION=southwest; DIRB=northeast; DIRF=southwest;;
+7|west|w)          DIRECTION=west;      DIRB=east;      DIRF=west;;
+8|northwest|nw)    DIRECTION=northwest; DIRB=southeast; DIRF=northwest;;
+*) ERROR=1 _error "Not recognized: '$lDIRECTION'";;
 esac
 }
 
@@ -472,34 +519,34 @@ echo $GERUNDET
 _set_sync_sleep(){
 _debug "_set_sync_sleep:$*"
 
-PL_SPEED1=${1:-$PL_SPEED1}
-PL_SPEED1=${PL_SPEED1:-50}
+local lPL_SPEED=${1:-$PL_SPEED}
+lPL_SPEED=${lPL_SPEED:-50000}
 
-  if test "$PL_SPEED1" -gt 60; then
+  if test "$lPL_SPEED" -gt 60000; then
 SLEEP=0.4; DELAY_DRAWINFO=1.0; TMOUT=1
-elif test "$PL_SPEED1" -gt 55; then
+elif test "$lPL_SPEED" -gt 55000; then
 SLEEP=0.5; DELAY_DRAWINFO=1.1; TMOUT=1
-elif test "$PL_SPEED1" -gt 50; then
+elif test "$lPL_SPEED" -gt 50000; then
 SLEEP=0.6; DELAY_DRAWINFO=1.2; TMOUT=1
-elif test "$PL_SPEED1" -gt 45; then
+elif test "$lPL_SPEED" -gt 45000; then
 SLEEP=0.7; DELAY_DRAWINFO=1.4; TMOUT=1
-elif test "$PL_SPEED1" -gt 40; then
+elif test "$lPL_SPEED" -gt 40000; then
 SLEEP=0.8; DELAY_DRAWINFO=1.6; TMOUT=1
-elif test "$PL_SPEED1" -gt 35; then
+elif test "$lPL_SPEED" -gt 35000; then
 SLEEP=1.0; DELAY_DRAWINFO=2.0; TMOUT=2
-elif test "$PL_SPEED1" -gt 30; then
+elif test "$lPL_SPEED" -gt 30000; then
 SLEEP=1.5; DELAY_DRAWINFO=3.0; TMOUT=2
-elif test "$PL_SPEED1" -gt 25; then
+elif test "$lPL_SPEED" -gt 25000; then
 SlEEP=2.0; DELAY_DRAWINFO=4.0; TMOUT=2
-elif test "$PL_SPEED1" -gt 20; then
+elif test "$lPL_SPEED" -gt 20000; then
 SlEEP=2.5; DELAY_DRAWINFO=5.0; TMOUT=2
-elif test "$PL_SPEED1" -gt 15; then
+elif test "$lPL_SPEED" -gt 15000; then
 SLEEP=3.0; DELAY_DRAWINFO=6.0; TMOUT=2
-elif test "$PL_SPEED1" -gt 10; then
+elif test "$lPL_SPEED" -gt 10000; then
 SLEEP=4.0; DELAY_DRAWINFO=8.0; TMOUT=2
-elif test "$PL_SPEED1" -ge 0;  then
+elif test "$lPL_SPEED" -ge 0;  then
 SLEEP=5.0; DELAY_DRAWINFO=10.0; TMOUT=2
-elif test "$PL_SPEED1" = "";   then
+elif test "$lPL_SPEED" = "";   then
 _draw 3 "WARNING: Could not set player speed. Using defaults."
 else
 _exit 1 "ERROR while processing player speed."
@@ -508,67 +555,200 @@ fi
 _info "Setting SLEEP=$SLEEP ,TMOUT=$TMOUT ,DELAY_DRAWINFO=$DELAY_DRAWINFO"
 }
 
-_get_player_speed(){
-_debug "_get_player_speed:$*"
+__set_sync_sleep(){
+_debug "__set_sync_sleep:$*"
 
-if test "$1" = '-l'; then
- spdcnt=$((spdcnt+1))
- test "$spdcnt" -ge ${COUNT_CHECK_FOOD:-10} || return 1
- spdcnt=0
+local lPL_SPEED=${1:-$PL_SPEED1}
+lPL_SPEED=${lPL_SPEED:-50}
+
+  if test "$lPL_SPEED" -gt 60; then
+SLEEP=0.4; DELAY_DRAWINFO=1.0; TMOUT=1
+elif test "$lPL_SPEED" -gt 55; then
+SLEEP=0.5; DELAY_DRAWINFO=1.1; TMOUT=1
+elif test "$lPL_SPEED" -gt 50; then
+SLEEP=0.6; DELAY_DRAWINFO=1.2; TMOUT=1
+elif test "$lPL_SPEED" -gt 45; then
+SLEEP=0.7; DELAY_DRAWINFO=1.4; TMOUT=1
+elif test "$lPL_SPEED" -gt 40; then
+SLEEP=0.8; DELAY_DRAWINFO=1.6; TMOUT=1
+elif test "$lPL_SPEED" -gt 35; then
+SLEEP=1.0; DELAY_DRAWINFO=2.0; TMOUT=2
+elif test "$lPL_SPEED" -gt 30; then
+SLEEP=1.5; DELAY_DRAWINFO=3.0; TMOUT=2
+elif test "$lPL_SPEED" -gt 25; then
+SlEEP=2.0; DELAY_DRAWINFO=4.0; TMOUT=2
+elif test "$lPL_SPEED" -gt 20; then
+SlEEP=2.5; DELAY_DRAWINFO=5.0; TMOUT=2
+elif test "$lPL_SPEED" -gt 15; then
+SLEEP=3.0; DELAY_DRAWINFO=6.0; TMOUT=2
+elif test "$lPL_SPEED" -gt 10; then
+SLEEP=4.0; DELAY_DRAWINFO=8.0; TMOUT=2
+elif test "$lPL_SPEED" -ge 0;  then
+SLEEP=5.0; DELAY_DRAWINFO=10.0; TMOUT=2
+elif test "$lPL_SPEED" = "";   then
+_draw 3 "WARNING: Could not set player speed. Using defaults."
+else
+_exit 1 "ERROR while processing player speed."
 fi
 
-_draw 5 "Processing Player's speed..."
+_info "Setting SLEEP=$SLEEP ,TMOUT=$TMOUT ,DELAY_DRAWINFO=$DELAY_DRAWINFO"
+}
 
-local ANSWER OLD_ANSWER PL_SPEED
-ANSWER=
-OLD_ANSWER=
+_player_speed_to_human_readable(){
+local lPL_SPEED=${1:-$PL_SPEED}
+test "$lPL_SPEED" || return 254
 
-echo request stat cmbt # only one line
+local PL_SPEED_PRE PL_SPEED_POST
 
-while :; do
-read -t $TMOUT ANSWER
-_log "$REQUEST_LOG" "request stat cmbt:$ANSWER"
-_msg 7 "$ANSWER"
-test "$ANSWER" || break
-test "$ANSWER" = "$OLD_ANSWER" && break
-OLD_ANSWER="$ANSWER"
-sleep 0.1
-done
-
-#PL_SPEED=`awk '{print $7}' <<<"$ANSWER"`    # *** bash
-test "$ANSWER" && PL_SPEED0=`echo "$ANSWER" | awk '{print $7}'` # *** ash + bash
-PL_SPEED=${PL_SPEED0:-$PL_SPEED}
-PL_SPEED=${PL_SPEED:-50000} # 0.50
-
-case ${#PL_SPEED} in
-6) PL_SPEED1="${PL_SPEED:0:4}"; PL_SPEED1=`dc $PL_SPEED1 1000 \/ p`;;
-5) PL_SPEED1="0.${PL_SPEED:0:3}";;
-4) PL_SPEED1="0.0.${PL_SPEED:0:2}";;
+oLC_NUMERIC=$oLC_NUMERIC
+LC_NUMERIC=C
+case ${#lPL_SPEED} in
+7) PL_SPEED1="${lPL_SPEED:0:5}"; PL_SPEED1=`dc $PL_SPEED1 1000 \/ p`;;
+6) PL_SPEED1="${lPL_SPEED:0:4}"; PL_SPEED1=`dc $PL_SPEED1 1000 \/ p`;;
+5) PL_SPEED1="0.${lPL_SPEED:0:3}";;
+4) PL_SPEED1="0.0.${lPL_SPEED:0:2}";;
 *) :;;
 esac
 
 _msg 7 "Player speed is '$PL_SPEED1'"
 
+case $PL_SPEED1 in
+*.*)
 PL_SPEED_PRE="${PL_SPEED1%.*}."
 PL_SPEED_POST="${PL_SPEED1##*.}"
+;;
+*,*)
+PL_SPEED_PRE="${PL_SPEED1%,*},"
+PL_SPEED_POST="${PL_SPEED1##*,}"
+;;
+[0-9]*)
+PL_SPEED_PRE="${PL_SPEED1}."
+PL_SPEED_POST=00
+;;
+esac
+case ${#PL_SPEED_POST} in 1) PL_SPEED_POST="${PL_SPEED_POST}0";; esac
+
 PL_SPEED_POST=`_round_up_and_down $PL_SPEED_POST`
-_msg 7 "Rounded Player speed is '$PL_SPEED_POST'"
+_msg 7 "Rounded Player speed is '${PL_SPEED_PRE}$PL_SPEED_POST'"
 PL_SPEED_POST=${PL_SPEED_POST:0:2}
-_msg 7 "Rounded Player speed is '$PL_SPEED_POST'"
+_msg 7 "Rounded Player speed is '${PL_SPEED_PRE}$PL_SPEED_POST'"
 
 PL_SPEED1="${PL_SPEED_PRE}${PL_SPEED_POST}"
 _msg 7 "Rounded Player speed is '$PL_SPEED1'"
 
-PL_SPEED1=`echo "$PL_SPEED1" | sed 's!\.!!g;s!^0*!!'`
+PL_SPEED1=`echo "$PL_SPEED1" | sed 's!\.!!g;s!\,!!g;s!^0*!!'`
+LC_NUMERIC=$oLC_NUMERIC
+}
+
+__player_speed_to_human_readable(){
+local lPL_SPEED=${1:-$PL_SPEED}
+test "$lPL_SPEED" || return 254
+:  #TODO
+}
+
+__request(){ # for multi-line replies
+test "$*" || return 254
+
+local lANSWER lOLD_ANSWER
+lANSWER=
+lOLD_ANSWER=
+
+echo request $*
+while :; do
+ read -t $TMOUT lANSWER
+ _log "$REQUEST_LOG" "__request $*:$lANSWER"
+ _msg 7 "$lANSWER"
+ #test "$lANSWER" || break
+ #test "$lANSWER" = "$lOLD_ANSWER" && break
+ case $lANSWER in ''|$lOLD_ANSWER|*request*end) break 1;; esac
+ ANSWER="$ANSWER
+$lANSWER"
+lOLD_ANSWER="$lANSWER"
+sleep 0.01
+done
+#ANSWER="$lANSWER"
+ANSWER=`echo "$ANSWER" | sed 'sI^$II'`
+}
+
+_request(){  # for one line replies
+test "$*" || return 254
+
+local lANSWER=''
+
+echo request $*
+read -t $TMOUT lANSWER
+ _log "$REQUEST_LOG" "_request $*:$lANSWER"
+ _msg 7 "$lANSWER"
+
+ANSWER="$lANSWER"
+}
+
+_get_player_speed(){
+_debug "_get_player_speed:$*"
+
+if test "$1" = '-l'; then # loop counter
+ spdcnt=$((spdcnt+1))
+ test "$spdcnt" -ge ${COUNT_CHECK_FOOD:-10} || return 1
+ spdcnt=0
+fi
+
+ __old_req(){
+ local reqANSWER OLD_ANSWER PL_SPEED
+ reqANSWER=
+ OLD_ANSWER=
+
+ while :; do
+ read -t $TMOUT reqANSWER
+ #read -t $TMOUT r s c WC AC DAM PL_SPEED WP_SPEED
+ #reqANSWER="$r $s $c $WC $AC $DAM $PL_SPEED $WP_SPEED"
+ _log "$REQUEST_LOG" "_get_player_speed:__old_req:$reqANSWER"
+ _msg 7 "$reqANSWER"
+ test "$reqANSWER" || break
+ test "$reqANSWER" = "$OLD_ANSWER" && break
+ OLD_ANSWER="$reqANSWER"
+ sleep 0.1
+ done
+
+ #PL_SPEED=`awk '{print $7}' <<<"$reqANSWER"`    # *** bash
+ test "$reqANSWER" && PL_SPEED0=`echo "$reqANSWER" | awk '{print $7}'` # *** ash + bash
+ PL_SPEED=${PL_SPEED0:-$PL_SPEED}
+}
+
+__new_req(){
+read -t $TMOUT r s c WC AC DAM PL_SPEED WP_SPEED
+ _log "$REQUEST_LOG" "_get_player_speed:__new_req:$r $s $c $WC $AC $DAM PL_SPPED=$PL_SPEED $WP_SPEED"
+ _msg 7 "$r $s $c $WC $AC $DAM PL_SPPED=$PL_SPEED $WP_SPEED"
+}
+
+_use_old_funcs(){
+echo request stat cmbt # only one line
+#__old_req
+__new_req
+}
+
+_use_new_funcs(){
+#__request stat cmbt
+ _request stat cmbt
+test "$ANSWER" && PL_SPEED0=`echo "$ANSWER" | awk '{print $7}'`
+PL_SPEED=${PL_SPEED0:-$PL_SPEED}
+}
+
+_draw 5 "Processing Player's speed..."
+#_use_old_funcs
+_use_new_funcs
+
+PL_SPEED=${PL_SPEED:-50000} # 0.50
+
+_player_speed_to_human_readable $PL_SPEED
 _msg 6 "Using player speed '$PL_SPEED1'"
 
 _draw 6 "Done."
 return 0
 }
 
-_prepare_rod_of_recall(){
+_unapply_rod_of_recall(){
 # *** Unreadying rod of word of recall - just in case *** #
-_debug "_prepare_rod_of_recall:$*"
+_debug "_unapply_rod_of_recall:$*"
 
 local RECALL OLD_REPLY REPLY
 
@@ -582,7 +762,7 @@ echo request items actv
 
 while :; do
 read -t $TMOUT
-_log "$REPLY_LOG" "_prepare_rod_of_recall:request items actv:$REPLY"
+_log "$REPLY_LOG" "_unapply_rod_of_recall:request items actv:$REPLY"
 case $REPLY in
 *rod*of*word*of*recall*) RECALL=1;;
 '') break;;
@@ -610,26 +790,28 @@ hpcnt=0
 local REPLY
 
 local currHP currHPMin
-currHP=$1
-currHPMin=$2
-
-currHP=${currHP:-$HP}
-currHPMin=${HP_MIN_DEF:-$((MHP/10))}
+currHP=${1:-$HP}
+currHPMin=${2:-$HP_MIN_DEF}
+currHPMin=${currHPMin:-$((MHP/10))}
 
 _msg 7 "currHP=$currHP currHPMin=$currHPMin"
-if test "$currHP" -le $currHPMin; then
+if test "$currHP" -le ${currHPMin:-20}; then
 
-_empty_message_stream
-_is 1 1 apply -u rod of word of recall
-_is 1 1 apply -a rod of word of recall
-_empty_message_stream
+ __old_recall(){
+ _empty_message_stream
+ _is 1 1 apply -u ${RETURN_ITEM:-'rod of word of recall'}
+ _is 1 1 apply -a ${RETURN_ITEM:-'rod of word of recall'}
+ _empty_message_stream
 
-_is 1 1 fire center ## TODO: check if already applied and in inventory
-_is 1 1 fire_stop
-_empty_message_stream
+ _is 1 1 fire center ## TODO: check if already applied and in inventory
+ _is 1 1 fire_stop
 
-_unwatch $DRAWINFO
-exit
+ _empty_message_stream
+ _unwatch $DRAWINFO
+ exit 5
+ }
+
+_emergency_exit
 fi
 
 unset HP
@@ -643,7 +825,8 @@ _is(){
 
 _drop(){
  _sound 0 drip &
- _is 1 1 drop "$@"
+ #_is 1 1 drop "$@" #01:58 There are only 1 chests.
+  _is 0 0 drop "$@"
 }
 
 ###END###
