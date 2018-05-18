f5ec8bf51e6cc4b72dd68b704821770c88c789aa
KarlGodt <karlreimergodt@yahoo.de>
2018-05-18 17:29:22 -0100

    /git_helper/diffinstalled.sh : Fix start directory, make diff files of files differences.

--- git_helper/diffinstalled.sh
+++ git_helper/diffinstalled.sh
@@ -3,7 +3,11 @@
 . /etc/rc.d/PUPSTATE
 . /etc/rc.d/f4puppy5
 
-DIRS=`find -type d`
+MY_SELF=`realpath "$0"`
+MY_DIR=${MY_SELF%/*}
+cd $MY_DIR/..
+
+DIRS=`find -type d | sort -d`
 
 
 while read onedir
@@ -11,19 +15,24 @@ do
 [ "$onedir" ]    || continue
 [ -d "$onedir" ] || continue
 
-onedirS=`echo "$onedir" | sed 's!^\.!!'`
-echo "$onedir" >>/dev/null
+case $onedir in *woof-code/rootfs-skeleton/*) :;; *) continue;; esac
+
+#onedirS=`echo "$onedir" | sed 's!^\.!!'`
+onedirS=${onedir##*rootfs-skeleton}
+_debug "$onedirS" #>>/dev/null
+
 
  while read oneFILE
  do
- echo "oneFILE=$oneFILE" >>/dev/null
+ _debug "oneFILE=$oneFILE" #>>/dev/null
  [ "$onedir/$oneFILE" ] || continue
  [ -f "$onedir/$oneFILE" ] || continue
  [ -d "$onedir/$oneFILE" ] && continue
  [ -L "$onedir/$oneFILE" ] && continue
 
- echo "onedir/oneFILE=$onedir/$oneFILE" >>/dev/null
-
+ _debug "onedir/oneFILE=$onedir/$oneFILE" #>>/dev/null
+ _debug "onedirS/oneFILE=$onedirS/$oneFILE" #>>/dev/null
+ 
  [ -e "$onedirS/$oneFILE" ] || continue
 
  diff -qs "$onedir/$oneFILE" "$onedirS/$oneFILE" >>/dev/null && continue
@@ -34,6 +43,25 @@ echo "$onedir" >>/dev/null
 EoI
 
 
-done >diff_files.list 2>&1 <<EoI
+done >/tmp/diff_files.list 2>&1 <<EoI
 `echo "$DIRS"`
 EoI
+
+if test -s /tmp/diff_files.list; then 
+
+  rm -rf /tmp/diff.d
+mkdir -p /tmp/diff.d
+
+while read f wooff a sysf rest;
+do
+
+test "$f" || continue
+test -e "$wooff" -a -e "$sysf" || continue
+
+echo $wooff $sysf
+diff -up $wooff $sysf >/tmp/diff.d/${sysf//\//_}.diff
+
+done </tmp/diff_files.list
+
+else [ "$DEBUG" ] || rm -f /tmp/diff_files.list;
+fi
